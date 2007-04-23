9c0357dedbb993ba8d997112336fb7d1a710c99f
Raimond Spekking <raymond@users.mediawiki.org>
2007-04-23 07:07:26 +0000

    * (bug 9664) Update French translations   Patch by Rémi Kaupp

--- CheckUser.i18n.php
+++ CheckUser.i18n.php
@@ -110,28 +110,25 @@ $wgCheckUserMessages['es'] = array(
 	'grouppage-checkuser'    => '{{ns:project}}:verificador del usuarios',
 );
 $wgCheckUserMessages['fr'] = array(
-	'checkuser-summary'		 => 'Cet outil passe en revue les changements récents pour rechercher l\'IPS employée par un utilisateur,
-	montrer toutes les éditions faites par une IP, ou pour  énumèrer les utilisateurs qui ont employé les IPs. Les utilisateurs et les modifications peuvent
-	être trouvés avec une IP XFF s\'il finit avec « /xff ». IPv4 (CIDR 16-32) et IPv6(CIDR 64-128) sont supportés.
-	Employez ceci selon les chaînes de caractères.',
+	'checkuser-summary'		 => 'Cet outil balaye les changements récents à la recherche de l’adresse IP employée par un utilisateur, affiche toutes les éditions d’une adresse IP (même enregistrée), ou liste les comptes utilisés par une adresse IP. Les comptes et modifications peuvent être trouvés avec une IP XFF si elle finit avec « /xff ». Il est possible d’utiliser les protocoles IPv4 (CIDR 16-32) et IPv6 (CIDR 64-128). Veuillez utiliser cet outil dans les limites de la charte d’utilisation.',
 	'checkuser-logcase'		 => 'La recherche dans le Journal est sensible à la casse.',
-	'checkuser'              => 'Vérificateur d\'utilisateur',
-	'group-checkuser'        => 'Vérificateurs d\'utilisateur',
-	'group-checkuser-member' => 'Vérificateur d\'utilisateur',
-	'grouppage-checkuser'    => '{{ns:projet}}:Vérificateur d\'utilisateur',
-	'checkuser-reason'		 => 'Explication ',
-	'checkuser-showlog'		 => 'Montrer la liste obtenue',
-	'checkuser-log'			 => 'Notation de Vérificateur d\'utilisateur',
+	'checkuser'              => 'Vérificateur d’utilisateur',
+	'group-checkuser'        => 'Vérificateurs d’utilisateur',
+	'group-checkuser-member' => 'Vérificateur d’utilisateur',
+	'grouppage-checkuser'    => '{{ns:projet}}:Vérificateur d’utilisateur',
+	'checkuser-reason'		 => 'Motif',
+	'checkuser-showlog'		 => 'Afficher le journal',
+	'checkuser-log'			 => 'Notation de Vérificateur d’utilisateur',
 	'checkuser-query'		 => 'Recherche par les changements récents',
-	'checkuser-target'		 => 'Nom de l\'utilisateur ou IP',
+	'checkuser-target'		 => 'Nom de l’utilisateur ou IP',
 	'checkuser-users'		 => 'Obtenir les utilisateurs',
-	'checkuser-edits'	  	 => 'Obtenir les modifications de l\'IP',
-	'checkuser-ips'	  	 	 => 'Obtenir les IPs',
+	'checkuser-edits'	  	 => 'Obtenir les modifications de l’IP',
+	'checkuser-ips'	  	 	 => 'Obtenir les adresses IP',
 	'checkuser-search'	  	 => 'Recherche',
 	'checkuser-empty'	 	 => 'Le journal ne contient aucun article',
 	'checkuser-nomatch'	  	 => 'Recherches infructueuses.',
 	'checkuser-check'	  	 => 'Recherche',
-	'checkuser-log-fail'	 => 'Incapable d\'ajouter l\'entrée du journal.',
+	'checkuser-log-fail'	 => 'Impossible d’ajouter l’entrée du journal.',
 	'checkuser-nolog'		 => 'Aucune entrée dans le Journal.'
 );
 $wgCheckUserMessages['he'] = array(
