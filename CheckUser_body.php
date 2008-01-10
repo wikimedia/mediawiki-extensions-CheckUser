267628f75648747a9511aa4abf380d73850f7f8c
Aaron Schulz <aaron@users.mediawiki.org>
2008-01-10 03:42:22 +0000

    Add UL tags

--- CheckUser_body.php
+++ CheckUser_body.php
@@ -790,7 +790,7 @@ class CheckUser extends SpecialPage
 		$pager = new CheckUserLogPager( $this, $searchConds );
 		$wgOut->addHTML( 
 			$pager->getNavigationBar() . 
-			$pager->getBody() . 
+			'<ul>' . $pager->getBody() . '</ul>' .
 			$pager->getNavigationBar() );
 	}
 
