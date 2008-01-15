92ccbd7fd9e0d0bf636d939ce343e07564d3f3f4
Raimond Spekking <raymond@users.mediawiki.org>
2008-01-15 10:56:57 +0000

    Localisation updates German

--- CheckUser.i18n.php
+++ CheckUser.i18n.php
@@ -1635,6 +1635,8 @@ $messages['zh-hant'] = array(
 	'checkuser-blocked'      => '已經查封',
 );
 
+# Fallbacks:
+$messages['de-formal'] = $messages['de'];
 $messages['kk'] = $messages['kk-cyrl'];
 $messages['kk-cn'] = $messages['kk-arab'];
 $messages['kk-kz'] = $messages['kk-cyrl'];
