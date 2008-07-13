b4bcab3b1c3a3524f66bd330e2ff6f2648f542f2
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-07-13 13:41:02 +0000

    Localisation updates for extension special pages from Betawiki

--- CheckUser.alias.php
+++ CheckUser.alias.php
@@ -14,11 +14,31 @@ $aliases['en'] = array(
     'CheckUser' => array( 'CheckUser' ),
 );
 
+/** Arabic (العربية) */
+$aliases['ar'] = array(
+	'CheckUser' => array( 'تدقيق مستخدم' ),
+);
+
+/** Egyptian Spoken Arabic (مصرى) */
+$aliases['arz'] = array(
+	'CheckUser' => array( 'تدقيق مستخدم' ),
+);
+
 /** German
  * @author Raimond Spekking
  */
 $aliases['de'] = array(
 	'CheckUser' => array( 'Checkuser' ),
+);
+
+/** Finnish (Suomi) */
+$aliases['fi'] = array(
+	'CheckUser' => array( 'Tarkista käyttäjä' ),
+);
+
+/** French (Français) */
+$aliases['fr'] = array(
+	'CheckUser' => array( 'Vérificateur d’adresse', 'VérificateurAdresse', 'VerificateurAdresse' ),
 );
 
 /** Hebrew (עברית)
@@ -28,9 +48,20 @@ $aliases['he'] = array(
     'CheckUser' => array( 'בדיקת_משתמש' ),
 );
 
+/** Haitian (Kreyòl ayisyen) */
+$aliases['ht'] = array(
+	'CheckUser' => array( 'TchekeItilizatè' ),
+);
+
+/** Dutch (Nederlands) */
+$aliases['nl'] = array(
+	'CheckUser' => array( 'GebruikerControleren' ),
+);
+
 /** Norwegian (Norsk (bokmål))
  * @author Jon Harald Søby
  */
 $aliases['no'] = array(
     'CheckUser' => array( 'IP-sjekk' ),
 );
+
