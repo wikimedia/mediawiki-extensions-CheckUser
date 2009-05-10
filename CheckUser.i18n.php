33a1740d7530484832ed24b6b5c88855344f6c79
Siebrand Mazeland <siebrand@users.mediawiki.org>
2009-05-10 21:20:42 +0000

    Localisation updates for extension messages from translatewiki.net (2009-05-10 21:07 UTC)

--- CheckUser.i18n.php
+++ CheckUser.i18n.php
@@ -2889,6 +2889,7 @@ Gli indirizzi IP saranno bloccati per una settimana solo per gli utenti anonimi 
 
 /** Japanese (日本語)
  * @author Aotake
+ * @author Fievarsty
  * @author Fryed-peach
  * @author Hosiryuhosi
  * @author JtFuruhata
@@ -2907,7 +2908,7 @@ IPv4（16から32ビットのCIDR表記）と IPv6（96から128ビットのCIDR
 	'checkuser-desc' => '特定の権限を付与された利用者に対して、利用者のIPアドレスなどの情報のチェックを可能にする',
 	'checkuser-logcase' => 'ログの検索では大文字と小文字を区別します。',
 	'checkuser' => 'チェックユーザー',
-	'checkuser-contribs' => 'チェックユーザーのIPアドレス',
+	'checkuser-contribs' => '利用者またはIPアドレスを調査',
 	'group-checkuser' => 'チェックユーザー',
 	'group-checkuser-member' => 'チェックユーザー',
 	'right-checkuser' => '利用者のIPアドレスやその他の情報を確認する',
