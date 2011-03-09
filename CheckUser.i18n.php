afb66f2690ae8730e0d0e69d98c692e6559687f0
Raimond Spekking <raymond@users.mediawiki.org>
2011-03-09 22:15:15 +0000

    Localisation updates for core and extension messages from translatewiki.net (2011-03-09 21:26:00 UTC)

--- CheckUser.i18n.php
+++ CheckUser.i18n.php
@@ -3531,6 +3531,7 @@ Di seguito sono indicati gli indirizzi IP utilizzati (fino a un massimo di 5000,
  * @author Marine-Blue
  * @author Muttley
  * @author Suisui
+ * @author Vigorous action
  * @author 青子守歌
  */
 $messages['ja'] = array(
@@ -3575,7 +3576,7 @@ IPv4 (16から32ビットのCIDR表記) と IPv6 (96から128ビットのCIDR表
 	'checkuser-gblocked' => 'グローバルブロックされています',
 	'checkuser-locked' => 'ロックされています',
 	'checkuser-wasblocked' => '過去にブロックの記録あり',
-	'checkuser-localonly' => '統一されません',
+	'checkuser-localonly' => '統一されていません',
 	'checkuser-massblock' => '選択した利用者をブロックする',
 	'checkuser-massblock-text' => '選択した利用者は無期限ブロックされ、同時に自動ブロックが作動しアカウント作成も禁止されます。IPアドレスはIP利用者向けに1週間ブロックされ、アカウント作成が禁止されます。',
 	'checkuser-blocktag' => '利用者ページを以下で置き換える:',
@@ -7253,7 +7254,7 @@ Gamitin ito ayon sa patakaran.',
 	'checkuser-query' => 'Magtanong hinggil sa kamakailang mga pagbabago',
 	'checkuser-target' => 'Adres ng IP o tagagamit:',
 	'checkuser-users' => 'Kunin ang mga tagagamit',
-	'checkuser-edits' => 'Kuhanin ang mga pagbabago mula sa IP',
+	'checkuser-edits' => 'Kunin ang mga pagbabago',
 	'checkuser-ips' => 'Kunin ang mga IP',
 	'checkuser-search' => 'Maghanap',
 	'checkuser-period' => 'Tagal ng panahon:',
