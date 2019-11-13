b5ae51a9d3a5d76df9650dbecc94644d1702b30c
Dayllan Maza <dmaza@wikimedia.org>
2019-11-12 22:42:11 -0500

    Add PreliminaryCheckService and PSR-4 to extension
    PreliminaryCheckService was added with basic functionality to support
    fetching global information about users. The current state of
    the service is mostly to provide some basics to later improve
    upon as part of T236225
    
    Note: In order to support pagination, a different approach should
    be used depending on how the pagination is gonna be handled (T237298)
    
    Bug: T237296
    Change-Id: I533748330b71a7d2b3cbb43849c73284b5ddef93

---
+++ src/PreliminaryCheckService.php
@@ -0,0 +1,130 @@
+<?php
+namespace MediaWiki\CheckUser;
+
+use User;
+use ExtensionRegistry;
+use CentralAuthUser;
+use UserGroupMembership;
+use Wikimedia\Rdbms\ILBFactory;
+use Wikimedia\Rdbms\IDatabase;
+
+class PreliminaryCheckService {
+	/** @var ILBFactory */
+	private $lbFactory;
+
+	/** @var string */
+	private $localWikiId;
+
+	/** @var ExtensionRegistry */
+	private $extensionRegistry;
+
+	/**
+	 * @param ILBFactory $lbFactory
+	 * @param ExtensionRegistry $extensionRegistry
+	 * @param string $localWikiId
+	 */
+	public function __construct( ILBFactory $lbFactory,
+		ExtensionRegistry $extensionRegistry,
+		string $localWikiId
+ ) {
+		$this->lbFactory = $lbFactory;
+		$this->extensionRegistry = $extensionRegistry;
+		$this->localWikiId = $localWikiId;
+	}
+
+	/**
+	 * Gets preliminary data for users to start an investigation
+	 *
+	 * @param User[] $users
+	 * @return array
+	 */
+	public function getPreliminaryData( array $users ): array {
+		$data = [];
+		foreach ( $users as $user ) {
+			$attachedWikis = $this->getAttachedWikis( $user );
+
+			foreach ( $attachedWikis as $wikiId ) {
+				$data[$user->getName()][$wikiId] = $this->getUserData( $user->getName(), $wikiId );
+			}
+		}
+
+		return $data;
+	}
+
+	/**
+	 * @param User $user
+	 * @return array
+	 */
+	private function getAttachedWikis( User $user ): array {
+		$attachedWikis = [ $this->localWikiId ];
+
+		$globalUser = $this->getGlobalUser( $user );
+
+		// $globalUser could be null if CentalAuth is not enabled
+		if ( $globalUser && $globalUser->exists() ) {
+			if ( $globalUser->exists() ) {
+				$attachedWikis = $globalUser->listAttached();
+			}
+		}
+
+		return $attachedWikis;
+	}
+
+	/**
+	 * @param User $user
+	 * @return CentralAuthUser|null
+	 */
+	protected function getGlobalUser( User $user ): ?CentralAuthUser {
+		if ( $this->extensionRegistry->isLoaded( 'CentralAuth' ) ) {
+			// work with central auth
+			$globalUser = CentralAuthUser::getInstance( $user );
+		}
+
+		return $globalUser ?: null;
+	}
+
+	/**
+	 * @param string $username
+	 * @param string $wikiId
+	 * @return array
+	 */
+	private function getUserData( string $username, string $wikiId ): array {
+		$db = $this->lbFactory->getMainLB( $wikiId )->getConnectionRef( DB_REPLICA, [], $wikiId );
+		$fields = [
+			'user_id', 'user_name', 'user_editcount', 'user_registration',
+		];
+		$conds = [ 'user_name' => $username ];
+		$row = $db->selectRow( 'user', $fields, $conds, __METHOD__ );
+
+		return [
+			'id' => $row->user_id,
+			'name' => $row->user_name,
+			'registration' => $row->user_registration,
+			'editcount' => $row->user_editcount,
+			'blocked' => $this->isUserBlocked( $row->user_id, $db ),
+			'groups' => $this->getUserGroups( $row->user_id, $db ),
+		];
+	}
+
+	/**
+	 * @param int $userId
+	 * @param IDatabase $db Database connection
+	 * @return string[]
+	 */
+	protected function getUserGroups( int $userId, IDatabase $db ): array {
+		$groupMembership = UserGroupMembership::getMembershipsForUser( $userId, $db );
+		return array_keys( $groupMembership );
+	}
+
+	/**
+	 * @param int $userId
+	 * @param IDatabase $db Database connection
+	 * @return bool
+	 */
+	protected function isUserBlocked( int $userId, IDatabase $db ): bool {
+		$blocks = $db->selectField(
+			'ipblocks', '1', [ 'ipb_user' => $userId ], __METHOD__, [ 'LIMIT' => 1 ]
+		);
+		return $blocks > 0;
+	}
+}
---
+++ tests/phpunit/PreliminaryCheckServiceTest.php
@@ -0,0 +1,161 @@
+<?php
+
+use Wikimedia\Rdbms\IDatabase;
+use Wikimedia\Rdbms\ILBFactory;
+use Wikimedia\Rdbms\ILoadBalancer;
+use MediaWiki\CheckUser\PreliminaryCheckService;
+
+/**
+ * Test class for PreliminaryCheckService class
+ *
+ * @group CheckUser
+ *
+ * @coversDefaultClass \MediaWiki\CheckUser\PreliminaryCheckService
+ */
+class PreliminaryCheckServiceTest extends MediaWikiTestCase {
+	/**
+	 * @return MockObject|ILoadBalancer
+	 */
+	private function getMockLoadBalancer() {
+		return $this->getMockBuilder( ILoadBalancer::class )
+			->disableOriginalConstructor()->getMock();
+	}
+
+	/**
+	 * @return MockObject|IDatabase
+	 */
+	private function getMockDb() {
+		return $this->getMockBuilder( IDatabase::class )
+			->disableOriginalConstructor()->getMock();
+	}
+
+	/**
+	 * @return MockObject|ILBFactory
+	 */
+	private function getMockLoadBalancerFactory() {
+		return $this->getMockBuilder( ILBFactory::class )
+			->disableOriginalConstructor()->getMock();
+	}
+
+	/**
+	 * @return MockObject|ExtensionRegistry
+	 */
+	private function getMockExtensionRegistry() {
+		return $this->getMockBuilder( ExtensionRegistry::class )
+			->disableOriginalConstructor()->getMock();
+	}
+
+	/**
+	 * @covers ::getPreliminaryData
+	 * @dataProvider preliminaryDataProvider()
+	 */
+	public function testGetPreliminaryData( $user, $options, $expected ) {
+		$dbRef = $this->getMockDb();
+		$dbRef->method( 'selectRow' )
+			->willReturn(
+				(object)[
+					'user_id' => $user['id'],
+					'user_name' => $user['name'],
+					'user_registration' => $user['registration'],
+					'user_editcount' => $user['editcount'],
+				]
+			);
+
+		$lb = $this->getMockLoadBalancer();
+		$lb->method( 'getConnectionRef' )->willReturn( $dbRef );
+		$lbFactory = $this->getMockLoadBalancerFactory();
+		$lbFactory->method( 'getMainLB' )->willReturn( $lb );
+
+		$service = $this->getMockBuilder( PreliminaryCheckService::class )
+			->setConstructorArgs( [ $lbFactory,
+				$this->getMockExtensionRegistry(),
+				$options['localWikiId']
+ ] )
+			->setMethods( [ 'isUserBlocked', 'getUserGroups', 'getGlobalUser' ] )
+			->getMock();
+
+		$service->method( 'isUserBlocked' )
+			->willReturn( $user['blocked'] );
+		$service->method( 'getUserGroups' )
+			->willReturn( $user['groups'] );
+
+		$globalUserMock = $this->getMockBuilder( CentralAuthUser::class )
+			->setMethods( [ 'listAttached', 'exists' ] )
+			->disableOriginalConstructor()
+			->getMock();
+		$globalUserMock->method( 'exists' )
+			->willReturn( isset( $options['attachedWikis'] ) ? true : false );
+
+		if ( $options['isCentralAuthAvailable'] ) {
+			$globalUserMock->expects( $this->once() )
+				->method( 'listAttached' )
+				->willReturn( $options['attachedWikis'] );
+		} else {
+			$globalUserMock
+				->expects( $this->never() )
+				->method( 'listAttached' );
+		}
+
+		$service->method( 'getGlobalUser' )
+			->willReturn( $globalUserMock );
+
+		$users = [ User::newFromName( $user['name'] ) ];
+		$data = $service->getPreliminaryData( $users );
+
+		$this->assertEquals( $data, $expected );
+	}
+
+	public function preliminaryDataProvider() {
+		$userData = [
+			'id' => 1,
+			'name' => 'Test User',
+			'registration' => '20190101010101',
+			'editcount' => 20,
+			'blocked' => true,
+			'groups' => [ 'sysop', 'autoconfirmed' ],
+		];
+
+		return [
+			'User attached to 4 wikis' => [
+				$userData,
+				[
+					'attachedWikis' => [ 'enwiki', 'frwiki', 'testwiki' ],
+					'isCentralAuthAvailable' => true,
+					'localWikiId' => 'testwiki',
+				],
+				[
+					$userData['name'] => [
+						'enwiki'  => $userData,
+						'frwiki'  => $userData,
+						'testwiki'  => $userData,
+					],
+				],
+			],
+			'User with only 1 wiki' => [
+				$userData,
+				[
+					'attachedWikis' => [ 'testwiki' ],
+					'isCentralAuthAvailable' => true,
+					'localWikiId' => 'testwiki',
+				],
+				[
+					$userData['name'] => [
+						'testwiki'  => $userData,
+					],
+				],
+			],
+			'CentralAuth not available' => [
+				$userData,
+				[
+					'isCentralAuthAvailable' => false,
+					'localWikiId' => 'somewiki',
+				],
+				[
+					$userData['name'] => [
+						'somewiki'  => $userData,
+					],
+				],
+			],
+		];
+	}
+}
