8a1a9ee55ea8cb0df337ac88ae2678dcf093731d
Thalia <thalia.e.chan@googlemail.com>
2020-02-13 01:04:54 +0000

    Add style linting to CheckUser extension
    This is in advance of adding styles for Special:Investigate.
    
    Change-Id: I93b9efaba77a48ad592edf4d71adbd7876d88a95

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,6 +9,7 @@ module.exports = function ( grunt ) {
 	var conf = grunt.file.readJSON( 'extension.json' );
 	grunt.loadNpmTasks( 'grunt-eslint' );
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
+	grunt.loadNpmTasks( 'grunt-stylelint' );
 
 	grunt.initConfig( {
 		eslint: {
@@ -21,9 +22,14 @@ module.exports = function ( grunt ) {
 				'modules/**/*.{js,json}'
 			]
 		},
+		stylelint: {
+			all: [
+				'modules/**/*.{css,less}'
+			]
+		},
 		banana: conf.MessagesDirs
 	} );
 
-	grunt.registerTask( 'test', [ 'eslint', 'banana' ] );
+	grunt.registerTask( 'test', [ 'eslint', 'banana', 'stylelint' ] );
 	grunt.registerTask( 'default', 'test' );
 };
