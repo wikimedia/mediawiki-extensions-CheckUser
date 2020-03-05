a7d6aac43ae0b5e2dbe566b8b273d173eb47e79b
Ammar Abdulhamid <ammarpad@yahoo.com>
2020-03-05 19:56:42 +0100

    Replace deprecated User::isAllowed with PermissionManager
    Bug: T220191
    Change-Id: I6654d4dcb8ac5c62e8c72fe00b8cdaf175a95812

--- includes/CheckUserHooks.php
+++ includes/CheckUserHooks.php
@@ -614,7 +614,9 @@ class CheckUserHooks {
 	) {
 		$user = $sp->getUser();
 		$linkRenderer = $sp->getLinkRenderer();
-		if ( $user->isAllowed( 'checkuser' ) ) {
+		$permissionManager = MediaWikiServices::getInstance()->getPermissionManager();
+
+		if ( $permissionManager->userHasRight( $user, 'checkuser' ) ) {
 			$links['checkuser'] = $linkRenderer->makeKnownLink(
 				SpecialPage::getTitleFor( 'CheckUser' ),
 				$sp->msg( 'checkuser-contribs' )->text(),
@@ -622,7 +624,7 @@ class CheckUserHooks {
 				[ 'user' => $nt->getText() ]
 			);
 		}
-		if ( $user->isAllowed( 'checkuser-log' ) ) {
+		if ( $permissionManager->userHasRight( $user, 'checkuser-log' ) ) {
 			$links['checkuser-log'] = $linkRenderer->makeKnownLink(
 				SpecialPage::getTitleFor( 'CheckUserLog' ),
 				$sp->msg( 'checkuser-contribs-log' )->text(),
