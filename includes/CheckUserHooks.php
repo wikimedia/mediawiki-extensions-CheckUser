6a05c2ab8d9753175ae3a6fc3599f6334165523f
Máté Szabó <mszabo@fandom.com>
2020-05-23 22:28:07 +0200

    Trim comment field content prior to insertion
    CheckUser populates the cuc_actiontext, cuc_agent, cuc_xff and cuc_comment
    from various UGC, such as MediaWiki system messages and request headers. These
    fields are all defined as VARCHAR(255) binary in the DB schema, so they can't
    fit more than 255 bytes.
    
    On installs that use MySQL/MariaDB in strict mode, this means that an action
    with a too long comment or too long User-Agent header will cause the INSERT
    into the checkuser table to fail. Worse still, this also prevents a recent
    changes row from being created for the action (e.g. edit) that triggered the
    INSERT!
    
    As a fix, update the logic to truncate these fields before attempting to
    INSERT them; on non-strict mode systems, this should be the same as the
    current behavior, while on strict mode systems, this should prevent the error.
    This fix also does not require a schema change or other migration in WMF
    production.
    
    A limited version of this fix has been applied to 1.19 CheckUser with strict
    mode MySQL 5.7.25.[1][2]
    
    Also swap out some trivial wfGetDB() calls to getConnectionRef(), as this
    patch introduces use of the MediaWiki service container.
    
    ---
    [1] https://github.com/Wikia/app/commit/62c9079665911ea9d561f90f
    [2] https://github.com/Wikia/app/commit/31f511b319b4259a41c1bd71
    
    Bug: T199323
    Change-Id: Ic07c5ac5fc77625b619741fe883391479f8fda61

--- includes/CheckUserHooks.php
+++ includes/CheckUserHooks.php
@@ -9,6 +9,13 @@ use Wikimedia\IPUtils;
 use Wikimedia\Rdbms\IDatabase;
 
 class CheckUserHooks {
+
+	/**
+	 * The maximum number of bytes that fit in CheckUser's text fields
+	 * (cuc_agent,cuc_actiontext,cuc_comment,cuc_xff)
+	 */
+	private const TEXT_FIELD_LENGTH = 255;
+
 	/**
 	 * @param array &$list
 	 * @return bool
@@ -89,7 +96,18 @@ class CheckUserHooks {
 			$actionText = '';
 		}
 
-		$dbw = wfGetDB( DB_MASTER );
+		$comment = $rc->getAttribute( 'rc_comment' );
+
+		$services = MediaWikiServices::getInstance();
+		$contLang = $services->getContentLanguage();
+
+		// (T199323) Truncate text fields prior to database insertion
+		// Attempting to insert too long text will cause an error in MariaDB/MySQL strict mode
+		$actionText = $contLang->truncateForDatabase( $actionText, self::TEXT_FIELD_LENGTH );
+		$agent = $contLang->truncateForDatabase( $agent, self::TEXT_FIELD_LENGTH );
+		$xff = $contLang->truncateForDatabase( $xff, self::TEXT_FIELD_LENGTH );
+		$comment = $contLang->truncateForDatabase( $comment, self::TEXT_FIELD_LENGTH );
+
 		$rcRow = [
 			'cuc_namespace'  => $attribs['rc_namespace'],
 			'cuc_title'      => $attribs['rc_title'],
@@ -97,7 +115,7 @@ class CheckUserHooks {
 			'cuc_user'       => $attribs['rc_user'],
 			'cuc_user_text'  => $attribs['rc_user_text'],
 			'cuc_actiontext' => $actionText,
-			'cuc_comment'    => $rc->getAttribute( 'rc_comment' ),
+			'cuc_comment'    => $comment,
 			'cuc_this_oldid' => $attribs['rc_this_oldid'],
 			'cuc_last_oldid' => $attribs['rc_last_oldid'],
 			'cuc_type'       => $attribs['rc_type'],
@@ -114,6 +132,8 @@ class CheckUserHooks {
 		}
 
 		Hooks::run( 'CheckUserInsertForRecentChange', [ $rc, &$rcRow ] );
+
+		$dbw = $services->getDBLoadBalancer()->getConnectionRef( DB_MASTER );
 		$dbw->insert( 'cu_changes', $rcRow, __METHOD__ );
 
 		return true;
