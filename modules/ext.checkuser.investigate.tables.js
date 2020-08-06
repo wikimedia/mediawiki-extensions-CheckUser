wikimedia/mediawiki-extensions-CheckUser

commit e68cb680529dda90fb1d91c8c5216003a6af7f0e

Customize button labels in Special:Investigate Compare tab
Customize the button label for adding an IP to the investigation,
depending on whether there are more edits from other users on that
IP.

Bug: T251906
Change-Id: I5af19b1ed23aef96056ea124fc20cc730ef83059

modules/ext.checkuser.investigate.tables.js
@@ -112,7 +112,9 @@ module.exports = function setupTables() {
 				classes: [
 					'ext-checkuser-investigate-button-add-user-targets'
 				],
-				label: mw.msg( 'checkuser-investigate-compare-table-button-add-user-targets-label' ),
+				label: $tableCell.data( 'edits' ) === $tableCell.data( 'all-edits' ) ?
+					mw.msg( 'checkuser-investigate-compare-table-button-add-user-targets-log-label' ) :
+					mw.msg( 'checkuser-investigate-compare-table-button-add-user-targets-label' ),
 				data: { type: 'addUsers' }
 			} ) );
 		}

src/ComparePager.php
@@ -130,12 +130,15 @@ class ComparePager extends TablePager {
 						break;
 					}
 				}
+				$ipHex = IPUtils::toHex( $value );
 				$attributes['class'] .= ' ext-checkuser-investigate-table-cell-interactive';
 				$attributes['class'] .= ' ext-checkuser-investigate-table-cell-pinnable';
 				$attributes['class'] .= ' ext-checkuser-compare-table-cell-ip-target';
 				$attributes['data-' . $field] = $value;
 				$attributes['data-target'] = $value;
-				$attributes['data-sort-value'] = IPUtils::toHex( $value );
+				$attributes['data-sort-value'] = $ipHex;
+				$attributes['data-edits'] = $row->total_edits;
+				$attributes['data-all-edits'] = $this->ipTotalEdits[$ipHex];
 				break;
 			case 'cuc_user_text':
 				$attributes['class'] .= ' ext-checkuser-investigate-table-cell-interactive';

commit fa7149d18d8aa340a131a989a175444c51f4d4be

Fix table cell highlighting in Special:Investigate
Compare particular data attributes to check whether table cells
contain the same data, rather than comparing all the data attributes.

Each cell's content is captured in two data attributes:
* data-field, e.g. "cuc_user_text"
* data-value, e.g. "Admin"
The field is needed as well as the value, to avoid cross-column
matches, e.g. if a user agent string was spoofed to match a username.

This fixes a regression introduced in e68cb680529d

Bug: T259106
Change-Id: I35cf6692ba29dbcc870b361c8f603a416e13948e

modules/ext.checkuser.investigate.tables.js
@@ -4,7 +4,6 @@
 module.exports = function setupTables() {
 	// Attributes used for pinnable highlighting
 	var highlightData = mw.storage.session.get( 'checkuser-investigate-highlight' ),
-		dataAttributes = [ 'registration', 'wiki', 'cuc_ip', 'cuc_agent' ],
 		toggleButtons = {};
 
 	// The message 'checkuser-toollinks' was parsed in PHP, since translations
@@ -17,36 +16,34 @@ module.exports = function setupTables() {
 	}
 
 	function getDataKey( $element ) {
-		return JSON.stringify( $element.data() );
+		return JSON.stringify( [
+			$element.data( 'field' ),
+			$element.data( 'value' )
+		] );
 	}
 
 	function updateMatchingElements( $target, value, classSuffix ) {
-		dataAttributes.forEach( function ( dataAttribute ) {
-			var $matches,
-				dataValue = $target.data( dataAttribute ),
-				cellClass = 'ext-checkuser-investigate-table-cell-' + classSuffix,
-				rowClass = 'ext-checkuser-investigate-table-row-' + classSuffix;
+		var $matches,
+			dataField = $target.data( 'field' ),
+			dataValue = $target.data( 'value' ),
+			cellClass = 'ext-checkuser-investigate-table-cell-' + classSuffix,
+			rowClass = 'ext-checkuser-investigate-table-row-' + classSuffix;
 
-			if ( dataValue === undefined ) {
-				return;
-			}
+		$matches = $( 'td[data-field="' + dataField + '"][data-value="' + dataValue + '"]' );
+		// The following messages can be passed here:
+		// * ext-checkuser-investigate-table-cell-hover-data-match
+		// * ext-checkuser-investigate-table-cell-pinned-data-match
+		$matches.toggleClass( cellClass, value );
 
-			$matches = $( 'td[data-' + dataAttribute + '="' + dataValue + '"]' );
+		// Rows should be highlighted iff they contain highlighted cells
+		$matches.closest( 'tr' ).each( function () {
 			// The following messages can be passed here:
-			// * ext-checkuser-investigate-table-cell-hover-data-match
-			// * ext-checkuser-investigate-table-cell-pinned-data-match
-			$matches.toggleClass( cellClass, value );
-
-			// Rows should be highlighted iff they contain highlighted cells
-			$matches.closest( 'tr' ).each( function () {
-				// The following messages can be passed here:
-				// * ext-checkuser-investigate-table-row-hover-data-match
-				// * ext-checkuser-investigate-table-row-pinned-data-match
-				$( this ).toggleClass(
-					rowClass,
-					!!$( this ).find( '.' + cellClass ).length
-				);
-			} );
+			// * ext-checkuser-investigate-table-row-hover-data-match
+			// * ext-checkuser-investigate-table-row-pinned-data-match
+			$( this ).toggleClass(
+				rowClass,
+				!!$( this ).find( '.' + cellClass ).length
+			);
 		} );
 	}
 
@@ -74,7 +71,7 @@ module.exports = function setupTables() {
 
 	function filterValue( $tableCell ) {
 		$( 'textarea[name=exclude-targets]' ).val( function () {
-			return this.value + '\n' + $tableCell.data( 'target' );
+			return this.value + '\n' + $tableCell.data( 'value' );
 		} );
 		$( '.mw-htmlform' ).trigger( 'submit' );
 	}

src/ComparePager.php
@@ -134,8 +134,8 @@ class ComparePager extends TablePager {
 				$attributes['class'] .= ' ext-checkuser-investigate-table-cell-interactive';
 				$attributes['class'] .= ' ext-checkuser-investigate-table-cell-pinnable';
 				$attributes['class'] .= ' ext-checkuser-compare-table-cell-ip-target';
-				$attributes['data-' . $field] = $value;
-				$attributes['data-target'] = $value;
+				$attributes['data-field'] = $field;
+				$attributes['data-value'] = $value;
 				$attributes['data-sort-value'] = $ipHex;
 				$attributes['data-edits'] = $row->total_edits;
 				$attributes['data-all-edits'] = $this->ipTotalEdits[$ipHex];
@@ -147,8 +147,8 @@ class ComparePager extends TablePager {
 					if ( in_array( $value, $this->filteredTargets ) ) {
 						$attributes['class'] .= ' ext-checkuser-compare-table-cell-target';
 					}
-					$attributes['data-' . $field] = $value;
-					$attributes['data-target'] = $value;
+					$attributes['data-field'] = $field;
+					$attributes['data-value'] = $value;
 				}
 				// Store the sort value as an attribute, to avoid using the table cell contents
 				// as the sort value, since UI elements are added to the table cell.
@@ -158,7 +158,8 @@ class ComparePager extends TablePager {
 				$attributes['class'] .= ' ext-checkuser-investigate-table-cell-interactive';
 				$attributes['class'] .= ' ext-checkuser-investigate-table-cell-pinnable';
 				$attributes['class'] .= ' ext-checkuser-compare-table-cell-user-agent';
-				$attributes['data-' . $field] = $value;
+				$attributes['data-field'] = $field;
+				$attributes['data-value'] = $value;
 				// Store the sort value as an attribute, to avoid using the table cell contents
 				// as the sort value, since UI elements are added to the table cell.
 				$attributes['data-sort-value'] = $value;

src/PreliminaryCheckPager.php
@@ -106,7 +106,8 @@ class PreliminaryCheckPager extends TablePager {
 			case 'wiki':
 				$attributes['class'] .= ' ext-checkuser-investigate-table-cell-interactive';
 				$attributes['class'] .= ' ext-checkuser-investigate-table-cell-pinnable';
-				$attributes['data-' . $field] = $value;
+				$attributes['data-field'] = $field;
+				$attributes['data-value'] = $value;
 				break;
 			case 'registration':
 				$attributes['class'] .= ' ext-checkuser-investigate-table-cell-interactive';
@@ -116,7 +117,8 @@ class PreliminaryCheckPager extends TablePager {
 					$this->getUser(),
 					[ 'format' => 'ISO 8601' ]
 				);
-				$attributes['data-' . $field] = $date;
+				$attributes['data-field'] = $field;
+				$attributes['data-value'] = $date;
 				break;
 		}
 