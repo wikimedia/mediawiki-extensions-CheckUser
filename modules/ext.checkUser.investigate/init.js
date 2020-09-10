d29e52527e4ea824c0fb4d4a956b36f38883212a
Timo Tijhof <krinklemail@gmail.com>
2020-09-10 22:24:52 +0100

    resources: Organize frontend files by module directory
    * Easier to find files in most text editors by typing the module
      name (e.g. based on error reports) and then naturally
      seeing which files are part of the same module.
    
    * Less likely to mistakenly reference files that are part of a different module,
      or might not be consistently available as part of the same module.
    
    * Quantifies the module cost more by seeing them as separate directories,
      hopefully making it easier for the next step of consolidating modules
      that don't have a functional requirement for module splitting.
    
    Ref
    <https://mediawiki.org/wiki/Wikimedia_Performance_Team/Page_load_performance#Size_of_scripts>
    <https://mediawiki.org/wiki/ResourceLoader/Developing_with_ResourceLoader#Introduction>
    
    Bug: T193826
    Change-Id: I49d76d6da3a4fce0a4cdf934c36d6a53651e3f7d

--- modules/ext.checkUser.investigate/init.js
+++ modules/ext.checkUser.investigate/init.js
@@ -1,7 +1,7 @@
 ( function () {
-	var addBlockForm = require( './ext.checkuser.investigate.blockform.js' ),
-		setupTables = require( './ext.checkuser.investigate.tables.js' ),
-		addCopyFeature = require( './ext.checkuser.investigate.copy.js' );
+	var addBlockForm = require( './blockform.js' ),
+		setupTables = require( './tables.js' ),
+		addCopyFeature = require( './copy.js' );
 
 	if ( $( '.ext-checkuser-investigate-subtitle-block-button' ).length > 0 ) {
 		addBlockForm();
