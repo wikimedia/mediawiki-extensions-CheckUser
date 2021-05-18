025d552c4ca4968cca8a8717b25129d62147c9a7
Amir Sarabadani <Ladsgroup@gmail.com>
2021-05-18 17:51:43 +0200

    Make cuc_id unsigned
    Wikidata is now on 67% of its maximum value and similar to rc_id (T62962)
    this should also be unsigned otherwise, it'll run out of values really
    soon. cuc_id now has the highest ratio to maximum value in wikidatawiki:
    https://grafana.wikimedia.org/d/79S1Hq9Mz/wikidata-reliability-metrics?viewPanel=29&orgId=1&refresh=1d
    
    This only affects MySQL and not PG nor Sqlite
    
    Bug: T283069
    Change-Id: Ib104c5e587e3fa0ed5986d5c2d5e1af66777da48

--- includes/CheckUserHooks.php
+++ includes/CheckUserHooks.php
@@ -558,6 +558,11 @@ class CheckUserHooks {
 				'cuc_private',
 				"$base/archives/patch-cu_changes_privatedata.sql"
 			);
+			$updater->modifyExtensionField(
+				'cu_changes',
+				'cuc_id',
+				"$base/archives/patch-cu_changes-cuc_id-unsigned.sql"
+			);
 		} elseif ( $dbType === 'postgres' ) {
 			$updater->addExtensionUpdate(
 				[ 'addPgField', 'cu_changes', 'cuc_private', 'BYTEA' ]
