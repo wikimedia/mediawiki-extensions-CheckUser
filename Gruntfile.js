d87c19e065b7a1a155d74f0c1519b8e85ed4fc76
Ed Sanders <esanders@wikimedia.org>
2022-03-13 17:11:21 +0000

    build: Update devDependencies
    Change-Id: I8c57d60565bdeee5433461044120b6964372794e

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,6 @@
-/* eslint-env node */
+'use strict';
 module.exports = function ( grunt ) {
-	var conf = grunt.file.readJSON( 'extension.json' );
+	const conf = grunt.file.readJSON( 'extension.json' );
 	grunt.loadNpmTasks( 'grunt-eslint' );
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
 	grunt.loadNpmTasks( 'grunt-stylelint' );
