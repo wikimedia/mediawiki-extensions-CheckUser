105e706197e88ada61a56f17f1f1dfd7fa6d3c85
dreamyjazz <dreamyjazzwikipedia@gmail.com>
2022-08-01 14:00:17 +0100

    Split the MultiLock links if the length exceeds 2000 characters
    Split the links to the MultiLock tool if the length of the URL
    exceeds 2000 characters. This involves making a unordered list of
    the URLs and then having multiple URLs for the user to click.
    
    Bug: T314243
    Change-Id: I428f5cc90f1aa63e129f362a8dcc4cbe0a66761d

--- modules/ext.checkUser/checkuser/caMultiLock.js
+++ modules/ext.checkUser/checkuser/caMultiLock.js
@@ -12,9 +12,10 @@
 	}
 
 	// Initialize the link
-	$( '#checkuserblock fieldset' ).append(
+	var $checkUserBlockFieldset = $( '#checkuserblock fieldset' );
+	$checkUserBlockFieldset.append(
 		$( '<a>' ).attr( {
-			id: 'cacu-multilock-link',
+			class: 'mw-checkuser-multilock-link',
 			href: centralURL
 		} ).text( mw.msg( 'checkuser-centralauth-multilock' ) )
 	);
@@ -22,7 +23,9 @@
 	// Change the URL of the link when a checkbox's state is changed
 	$userCheckboxes = $( '#checkuserresults li [type=checkbox]' );
 	$userCheckboxes.on( 'change', function () {
+		$( '.mw-checkuser-multilock-link, .mw-checkuser-multilock-link-header, .mw-checkuser-multilock-link-list' ).remove();
 		var names = [];
+		var urls = [];
 		$userCheckboxes.serializeArray().forEach( function ( obj ) {
 			if ( obj.name && obj.name === 'users[]' ) {
 				// Only registered accounts (not IPs) can be locked
@@ -32,11 +35,57 @@
 			}
 		} );
 
+		// Split the names up into batches of username length of a
+		// maximum of 2,000 including the centralURL + other parts
+		// of the GET parameters
+		var i = 0;
+		while ( i < names.length ) {
+			var url = centralURL + '?wpTarget=';
+			var firstUsername = true;
+			while ( i < names.length ) {
+				var urlComponent = names[ i ];
+				if ( !firstUsername ) {
+					urlComponent = '\n' + urlComponent;
+				} else {
+					firstUsername = false;
+				}
+				urlComponent = encodeURIComponent( urlComponent );
+				if ( urlComponent.length + url.length >= 2000 ) {
+					break;
+				}
+				url += urlComponent;
+				i = i + 1;
+			}
+			urls.push( url );
+		}
+
 		// Update the href of the link with the latest change
-		$( '#cacu-multilock-link' ).prop(
-			'href',
-			centralURL + '?wpTarget=' + encodeURIComponent( names.join( '\n' ) )
-		);
+		if ( urls.length > 1 ) {
+			$checkUserBlockFieldset.append(
+				$( '<span>' ).attr( {
+					class: 'mw-checkuser-multilock-link-header'
+				} ).text( mw.msg( 'checkuser-centralauth-multilock-list' ) )
+			);
+			var links = '';
+			urls.forEach( function ( urlToAdd, index ) {
+				var $li = $( '<li>' );
+				var $a = $( '<a>' );
+				$a.attr( 'href', urlToAdd )
+					.text( mw.msg( 'checkuser-centralauth-multilock-list-item', index + 1 ) );
+				$li.append( $a );
+				links += $li[ 0 ].outerHTML;
+			} );
+			$checkUserBlockFieldset.append(
+				$( '<ul>' ).attr( { class: 'mw-checkuser-multilock-link-list' } ).append( links )
+			);
+		} else {
+			$checkUserBlockFieldset.append(
+				$( '<a>' ).attr( {
+					class: 'mw-checkuser-multilock-link',
+					href: urls[ 0 ]
+				} ).text( mw.msg( 'checkuser-centralauth-multilock' ) )
+			);
+		}
 	} );
 
 }() );
