74dbd314ec15ddca5b8f23fd11b34429be04c7f6
Dreamy Jazz <dreamyjazzwikipedia@gmail.com>
2023-01-14 15:27:11 +0000

    Add maintenance script to move cu_changes log entries to new table
    Add maintenance script that moves log entries from cu_changes to
    the new cu_private_event table. This maintenance script is not
    added to update.php yet as the extension is not yet ready to read
    from these new tables.
    
    Bug: T324907
    Change-Id: Ic4e225374f60038052ad6ef71a5d724b7f2de5f2

---
+++ maintenance/moveLogEntriesFromCuChanges.php
@@ -0,0 +1,174 @@
+<?php
+
+namespace MediaWiki\CheckUser\Maintenance;
+
+use LogEntryBase;
+use LoggedUpdateMaintenance;
+use MediaWiki\MediaWikiServices;
+use MediaWiki\User\UserIdentityValue;
+
+$IP = getenv( 'MW_INSTALL_PATH' );
+if ( $IP === false ) {
+	$IP = __DIR__ . '/../../..';
+}
+require_once "$IP/maintenance/Maintenance.php";
+
+/**
+ * Move log entries from cu_changes to cu_private_event. This removes
+ * the log entries from cu_changes which means that this maintenance script
+ * should only be run when reading and writing to the new tables.
+ *
+ * Based on parts of multiple other maintenance scripts in this extension.
+ */
+class MoveLogEntriesFromCuChanges extends LoggedUpdateMaintenance {
+	public function __construct() {
+		parent::__construct();
+		$this->addDescription( 'Move log entries from cu_changes to cu_private_event' );
+		$this->setBatchSize( 100 );
+
+		$this->requireExtension( 'CheckUser' );
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	protected function getUpdateKey() {
+		return __CLASS__;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	protected function doDBUpdates() {
+		$dbw = $this->getDB( DB_PRIMARY );
+
+		$services = MediaWikiServices::getInstance();
+
+		$eventTableMigrationStage = $services->getMainConfig()->get( 'CheckUserEventTablesMigrationStage' );
+		if ( $eventTableMigrationStage !== SCHEMA_COMPAT_NEW ) {
+			$this->output(
+				"Event table migration config must be set to write and read new otherwise moved entries would " .
+				"not be displayed in check results. Change the migration config and try again."
+			);
+			return false;
+		}
+
+		// Check if the table is empty
+		$cuChangesRows = $dbw->newSelectQueryBuilder()
+			->table( 'cu_changes' )
+			->caller( __METHOD__ )
+			->fetchRowCount();
+		if ( !$cuChangesRows ) {
+			$this->output( "cu_changes is empty; nothing to move.\n" );
+			return true;
+		}
+
+		$start = (int)$dbw->newSelectQueryBuilder()
+			->field( 'MIN(cuc_id)' )
+			->table( 'cu_changes' )
+			->caller( __METHOD__ )
+			->fetchField();
+		$end = (int)$dbw->newSelectQueryBuilder()
+			->field( 'MAX(cuc_id)' )
+			->table( 'cu_changes' )
+			->caller( __METHOD__ )
+			->fetchField();
+		// Do remaining chunk
+		$end += $this->mBatchSize - 1;
+		$blockStart = $start;
+		$blockEnd = $start + $this->mBatchSize - 1;
+
+		$this->output(
+			"Moving log entries from cu_changes to cu_private_event with cuc_id from $start to $end\n"
+		);
+
+		$lbFactory = $services->getDBLoadBalancerFactory();
+
+		$actorMigrationStage = $services->getMainConfig()->get( 'CheckUserActorMigrationStage' );
+		$commentMigrationStage = $services->getMainConfig()->get( 'CheckUserCommentMigrationStage' );
+
+		$commentStore = $services->getCommentStore();
+
+		while ( $blockStart <= $end ) {
+			$this->output( "...checking and moving log entries with cuc_id from $blockStart to $blockEnd\n" );
+			$cond = "cuc_id BETWEEN $blockStart AND $blockEnd";
+			$res = $dbw->newSelectQueryBuilder()
+				->fields( [
+					'cuc_id',
+					'cuc_namespace',
+					'cuc_title',
+					'cuc_user',
+					'cuc_user_text',
+					'cuc_actor',
+					'cuc_actiontext',
+					'cuc_comment',
+					'cuc_comment_id',
+					'cuc_page_id',
+					'cuc_timestamp',
+					'cuc_ip',
+					'cuc_ip_hex',
+					'cuc_xff',
+					'cuc_xff_hex',
+					'cuc_agent',
+					'cuc_private'
+				] )
+				->table( 'cu_changes' )
+				->conds( $cond )
+				->where( [ 'cuc_type' => RC_LOG ] )
+				->caller( __METHOD__ )
+				->fetchResultSet();
+			$batch = [];
+			$removeBatch = [];
+			foreach ( $res as $row ) {
+				$entry = [
+					'cupe_timestamp' => $row->cuc_timestamp,
+					'cupe_namespace' => $row->cuc_namespace,
+					'cupe_title' => $row->cuc_title,
+					'cupe_page' => $row->cuc_page_id,
+					'cupe_log_action' => 'migrated-cu_changes-log-event',
+					'cupe_log_type' => 'checkuser-private-event',
+					'cupe_params' => LogEntryBase::makeParamBlob( [ '4::actiontext' => $row->cuc_actiontext ] ),
+					'cupe_ip' => $row->cuc_ip,
+					'cupe_ip_hex' => $row->cuc_ip_hex,
+					'cupe_xff' => $row->cuc_ip,
+					'cupe_xff_hex' => $row->cuc_ip_hex,
+					'cupe_agent' => $row->cuc_agent,
+					'cupe_private' => $row->cuc_private
+				];
+
+				if ( $actorMigrationStage & SCHEMA_COMPAT_WRITE_NEW ) {
+					$entry['cupe_actor'] = $row->cuc_actor;
+				} else {
+					$entry['cupe_actor'] = $services->getActorStore()->acquireActorId(
+						new UserIdentityValue( $row->cuc_user ?? 0, $row->cuc_user_text ), $dbw
+					);
+				}
+
+				if ( $commentMigrationStage & SCHEMA_COMPAT_WRITE_NEW ) {
+					$entry['cupe_comment_id'] = $row->cuc_comment_id;
+				} else {
+					$entry['cupe_comment_id'] = $commentStore->createComment( $dbw, $row->cuc_comment )->id;
+				}
+
+				$batch[] = $entry;
+				$removeBatch[] = $row->cuc_id;
+			}
+			if ( count( $batch ) ) {
+				$dbw->insert( 'cu_private_event', $batch, __METHOD__ );
+			}
+			if ( count( $removeBatch ) ) {
+				$dbw->delete( 'cu_changes', [ 'cuc_id' => $removeBatch ], __METHOD__ );
+			}
+			$blockStart += $this->mBatchSize - 1;
+			$blockEnd += $this->mBatchSize - 1;
+			$lbFactory->waitForReplication( [ 'ifWritesSince' => 5 ] );
+			$lbFactory->autoReconfigure();
+		}
+
+		$this->output( "...all log entries in cu_changes have been moved to cu_private_event table.\n" );
+		return true;
+	}
+}
+
+$maintClass = MoveLogEntriesFromCuChanges::class;
+require_once RUN_MAINTENANCE_IF_MAIN;
---
+++ tests/phpunit/integration/maintenance/MoveLogEntriesFromCuChangesTest.php
@@ -0,0 +1,174 @@
+<?php
+
+namespace MediaWiki\CheckUser\Tests\Integration\Maintenance;
+
+use MediaWiki\CheckUser\Maintenance\MoveLogEntriesFromCuChanges;
+use MediaWiki\CheckUser\Test\Integration\CheckUserCommonTraitTest;
+use MediaWiki\Tests\Maintenance\MaintenanceBaseTestCase;
+use Wikimedia\TestingAccessWrapper;
+
+/**
+ * @group CheckUser
+ * @group Database
+ * @coversDefaultClass \MediaWiki\CheckUser\Maintenance\MoveLogEntriesFromCuChanges
+ */
+class MoveLogEntriesFromCuChangesTest extends MaintenanceBaseTestCase {
+
+	use CheckUserCommonTraitTest;
+
+	/** @inheritDoc */
+	public function setUp(): void {
+		parent::setUp();
+
+		$this->tablesUsed = [
+			'cu_changes',
+			'cu_private_event',
+			'cu_log_event',
+			'recentchanges',
+			'updatelog'
+		];
+	}
+
+	/** @inheritDoc */
+	protected function getMaintenanceClass() {
+		return MoveLogEntriesFromCuChanges::class;
+	}
+
+	protected function commonTestNoMove( $expectedCuChangesCount, $expectedCuPrivateRowCount = 0 ) {
+		$this->assertRowCount(
+			$expectedCuPrivateRowCount, 'cu_private_event', 'cupe_id',
+			'Rows were moved to cu_private_event when they should not have been moved.'
+		);
+		$this->assertRowCount(
+			$expectedCuChangesCount, 'cu_changes', 'cuc_id',
+			'Rows were removed from cu_changes even though there was no move.'
+		);
+	}
+
+	protected function commonTestMoved(
+		$originalCuChangesRowCount, $expectedCuChangesRowCount, $expectedCuPrivateRowCount
+	) {
+		$this->assertRowCount(
+			$expectedCuPrivateRowCount, 'cu_private_event', 'cupe_id',
+			'Rows were moved to cu_private_event when they should not have been moved.'
+		);
+		$this->assertRowCount(
+			$expectedCuChangesRowCount, 'cu_changes', 'cuc_id',
+			'Rows were removed from cu_changes when they should not have been.'
+		);
+		$this->assertSame(
+			$originalCuChangesRowCount,
+			(
+				$this->db->newSelectQueryBuilder()
+					->table( 'cu_changes' )
+					->fetchRowCount() +
+				$this->db->newSelectQueryBuilder()
+					->table( 'cu_private_event' )
+					->fetchRowCount()
+			),
+			'Rows went missing or were added when moving entries.'
+		);
+		$this->assertRowCount(
+			0, 'cu_changes', 'cuc_id',
+			'Log entries are still left in cu_changes',
+			[ 'cuc_type' => RC_LOG ]
+		);
+	}
+
+	/**
+	 * @covers ::doDBUpdates
+	 * @dataProvider provideSchemaNoMoveValues
+	 */
+	public function testNoMoveWhenWrongSchemaStage( $schemaStage, $cuChangesRowCount, $cuPrivateEventCount ) {
+		$this->setMwGlobals( 'wgCheckUserEventTablesMigrationStage', $schemaStage );
+		// Set up cu_changes
+		$expectedRow = [];
+		$this->commonTestsUpdateCheckUserData(
+			array_merge( $this->getDefaultRecentChangeAttribs(), [ 'rc_type' => RC_LOG, 'rc_log_type' => '' ] ),
+			[],
+			$expectedRow
+		);
+		// Run the script
+		$this->assertFalse(
+			$this->maintenance->execute(),
+			'execute() should have returned false so it can be run again as it failed.'
+		);
+		// Test cu_changes was untouched
+		$this->commonTestNoMove( $cuChangesRowCount, $cuPrivateEventCount );
+	}
+
+	public function provideSchemaNoMoveValues() {
+		return [
+			'Read and write old' => [ SCHEMA_COMPAT_OLD, 1, 0 ],
+			'Read and write old, read new' => [ SCHEMA_COMPAT_OLD | SCHEMA_COMPAT_READ_NEW, 1, 0 ],
+			'Read and write old, write new' => [ SCHEMA_COMPAT_OLD | SCHEMA_COMPAT_WRITE_NEW, 0, 1 ],
+			'Read and write both old and new' => [ SCHEMA_COMPAT_READ_BOTH | SCHEMA_COMPAT_WRITE_BOTH, 0, 1 ]
+		];
+	}
+
+	/**
+	 * @covers ::doDBUpdates
+	 */
+	public function testNoMoveIfCuChangesEmpty() {
+		$this->setMwGlobals( 'wgCheckUserEventTablesMigrationStage', SCHEMA_COMPAT_NEW );
+		// Run the script
+		$this->assertTrue(
+			$this->maintenance->execute(),
+			'execute() should have returned true so that the script can be skipped in the future.'
+		);
+		// Test no moving happened
+		$this->commonTestNoMove( 0 );
+	}
+
+	/**
+	 * @covers ::doDBUpdates
+	 * @dataProvider provideBatchSize
+	 */
+	public function testBatchSize( $numberOfRows, $batchSize ) {
+		$this->setMwGlobals( 'wgCheckUserEventTablesMigrationStage', SCHEMA_COMPAT_OLD );
+		// Set up cu_changes
+		$expectedRow = [];
+		for ( $i = 0; $i < $numberOfRows / 2; $i++ ) {
+			$this->commonTestsUpdateCheckUserData(
+				array_merge( $this->getDefaultRecentChangeAttribs(), [ 'rc_type' => RC_EDIT ] ),
+				[],
+				$expectedRow
+			);
+			$this->commonTestsUpdateCheckUserData(
+				array_merge( $this->getDefaultRecentChangeAttribs(), [ 'rc_type' => RC_LOG, 'rc_log_type' => '' ] ),
+				[],
+				$expectedRow
+			);
+		}
+		$this->assertRowCount(
+			$numberOfRows, 'cu_changes', 'cuc_id',
+			'Database not set up correctly for the test'
+		);
+		$this->setMwGlobals( 'wgCheckUserEventTablesMigrationStage', SCHEMA_COMPAT_NEW );
+		// Run the script
+		/** @var TestingAccessWrapper $maintenance */
+		// Make a copy to prevent syntax error warnings for accessing protected method setBatchSize.
+		$maintenance = $this->maintenance;
+		$maintenance->setBatchSize( $batchSize );
+		$this->assertTrue(
+			$maintenance->execute(),
+			'execute() should have returned true as moving entries should have completed successfully.'
+		);
+		// Test entries were moved
+		$this->commonTestMoved( $numberOfRows, $numberOfRows / 2, $numberOfRows / 2 );
+	}
+
+	public function provideBatchSize() {
+		return [
+			'cu_changes row count 3 and batch size 1' => [
+				6, 4
+			],
+			'cu_changes row count 10 and batch size 5' => [
+				10, 5
+			],
+			'cu_changes row count 10 and batch size 100' => [
+				10, 100
+			],
+		];
+	}
+}
