d676bd1fcbf337df38f122ace5d933dd9a6e957b
STran <stran@wikimedia.org>
2023-01-16 03:45:34 -0800

    Add preference to view temp account IPs
    Bug: T326736
    Change-Id: I776dfa55bcb9935d5ab493210aaf02a061b8ec0a

--- src/HookHandler/Preferences.php
+++ src/HookHandler/Preferences.php
@@ -2,6 +2,7 @@
 
 namespace MediaWiki\CheckUser\HookHandler;
 
+use MediaWiki\Permissions\PermissionManager;
 use MediaWiki\Preferences\Hook\GetPreferencesHook;
 
 class Preferences implements GetPreferencesHook {
@@ -11,6 +12,18 @@ class Preferences implements GetPreferencesHook {
 
 	/** @var string */
 	public const INVESTIGATE_FORM_TOUR_SEEN = 'checkuser-investigate-form-tour-seen';
+
+	/** @var PermissionManager */
+	private $permissionManager;
+
+	/**
+	 * @param PermissionManager $permissionManager
+	 */
+	public function __construct(
+		PermissionManager $permissionManager
+	) {
+		$this->permissionManager = $permissionManager;
+	}
 
 	/**
 	 * @inheritDoc
@@ -23,6 +36,16 @@ class Preferences implements GetPreferencesHook {
 		$preferences[self::INVESTIGATE_FORM_TOUR_SEEN] = [
 			'type' => 'api',
 		];
+
+		if ( $this->permissionManager->userHasRight( $user, 'checkuser-temporary-account' ) ) {
+			// TODO: Users with the 'checkuser' right who haven't set a preference
+			// should have the option checked by default. See T327061
+			$preferences['checkuser-temporary-account-enable'] = [
+				'type' => 'toggle',
+				'label-message' => 'checkuser-tempaccount-enable-preference',
+				'section' => 'personal/checkuser-tempaccount',
+			];
+		}
 	}
 
 }
