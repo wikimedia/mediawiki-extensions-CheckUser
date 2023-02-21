63ea3369035f4aeef292d36ccb00e38a6c0c75df
AnaïsGueyte <agueyte@wikimedia.org>
2023-02-21 11:20:36 -0500

    Fix limit handling in TemporaryAccountHandler query
    Bug: T328894
    Change-Id: I861471128bff6cc627a52e7e6e9063eb609740c8

--- src/Api/Rest/Handler/TemporaryAccountHandler.php
+++ src/Api/Rest/Handler/TemporaryAccountHandler.php
@@ -3,6 +3,7 @@
 namespace MediaWiki\CheckUser\Api\Rest\Handler;
 
 use Wikimedia\ParamValidator\ParamValidator;
+use Wikimedia\ParamValidator\TypeDef\IntegerDef;
 use Wikimedia\Rdbms\IDatabase;
 use Wikimedia\Rdbms\SelectQueryBuilder;
 
@@ -11,42 +12,23 @@ class TemporaryAccountHandler extends AbstractTemporaryAccountHandler {
 	 * @inheritDoc
 	 */
 	protected function getData( int $actorId, IDatabase $dbr ): array {
-		$paramLimit = $this->getValidatedParams()['limit'];
-		$configLimit = $this->config->get( 'CheckUserMaximumRowCount' );
-
-		if ( isset( $paramLimit ) && $paramLimit < $configLimit ) {
-			$limit = $paramLimit;
-		} else {
-			$limit = $configLimit;
-		}
-
 		$result = $this->loadBalancer->getConnection( DB_REPLICA )
 			->newSelectQueryBuilder()
-			// T327906: 'cuc_timestamp' is selected
-			// only to satisfy Postgres requirement where all ORDER BY
-			// fields must be present in SELECT list.
-			->select( [ 'cuc_ip', 'cuc_timestamp' ] )
+			->select( [ 'cuc_ip' ] )
 			->from( 'cu_changes' )
 			->where( [
 				'cuc_actor' => $actorId
 			] )
-			->limit( $limit )
+			->groupBy( 'cuc_ip' )
 			->orderby(
-				[ 'cuc_timestamp' ],
+				[ 'Max(cuc_timestamp)' ],
 				SelectQueryBuilder::SORT_DESC
 			)
+			->limit( $this->getValidatedParams()['limit'] )
 			->caller( __METHOD__ )
-			->fetchResultSet();
+			->fetchFieldValues();
 
-		$ips = [];
-
-		foreach ( $result as $row ) {
-			$ips[] = $row->cuc_ip;
-		}
-
-		$unique_ips = array_values( array_unique( $ips ) );
-
-		return [ 'ips' => $unique_ips ];
+		return [ 'ips' => $result ];
 	}
 
 	/**
@@ -58,6 +40,9 @@ class TemporaryAccountHandler extends AbstractTemporaryAccountHandler {
 			self::PARAM_SOURCE => 'query',
 			ParamValidator::PARAM_TYPE => 'integer',
 			ParamValidator::PARAM_REQUIRED => false,
+			ParamValidator::PARAM_DEFAULT => $this->config->get( 'CheckUserMaximumRowCount' ),
+			IntegerDef::PARAM_MAX => $this->config->get( 'CheckUserMaximumRowCount' ),
+			IntegerDef::PARAM_MIN => 1
 		];
 		return $settings;
 	}
