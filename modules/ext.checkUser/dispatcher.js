d7081d9f007ef4368c1bff9d1916f0edba4980f8
Thalia <thalia.e.chan@googlemail.com>
2023-02-23 17:44:14 +0000

    Show reveal IP buttons on Special:CheckUserLog
    Also refactor dispatcher.js to clarify intention behind resource
    loading on Special Pages.
    
    Change-Id: I10abf478be478142d0a15a7a2d3ea2626bfcc62d

--- modules/ext.checkUser/dispatcher.js
+++ modules/ext.checkUser/dispatcher.js
@@ -1,26 +1,35 @@
 ( function () {
-	if ( mw.config.get( 'wgCanonicalSpecialPageName' ) === 'Investigate' ) {
-		require( './investigate/init.js' );
-	} else if ( mw.config.get( 'wgCanonicalSpecialPageName' ) === 'InvestigateBlock' ) {
-		require( './investigateblock/investigateblock.js' );
-	} else if ( mw.config.get( 'wgCanonicalSpecialPageName' ) === 'CheckUser' ) {
-		require( './checkuser/cidr.js' );
-		require( './checkuser/caMultiLock.js' );
-		require( './checkuser/checkUserHelper.js' );
-	} else if ( mw.config.get( 'wgCanonicalSpecialPageName' ) === 'CheckUserLog' ) {
-		require( './checkuserlog/highlightScroll.js' );
-	}
-
-	var excludePages = [ 'Investigate', 'InvestigateBlock', 'CheckUser', 'CheckUserLog' ];
+	// Include resources for specific special pages
 	switch ( mw.config.get( 'wgCanonicalSpecialPageName' ) ) {
+		case 'Investigate':
+			require( './investigate/init.js' );
+			break;
+		case 'InvestigateBlock':
+			require( './investigateblock/investigateblock.js' );
+			break;
+		case 'CheckUser':
+			require( './checkuser/cidr.js' );
+			require( './checkuser/caMultiLock.js' );
+			require( './checkuser/checkUserHelper.js' );
+			break;
+		case 'CheckUserLog':
+			require( './checkuserlog/highlightScroll.js' );
+			break;
 		case 'Contributions':
 			if ( mw.util.isTemporaryUser( mw.config.get( 'wgRelevantUserName' ) ) ) {
 				require( './temporaryaccount/SpecialContributions.js' );
 			}
 			break;
-		default:
-			if ( excludePages.indexOf( mw.config.get( 'wgCanonicalSpecialPageName' ) ) === -1 ) {
-				require( './temporaryaccount/init.js' );
-			}
+	}
+
+	// Include resources for all but a few specific special pages
+	var excludePages = [
+		'Investigate',
+		'InvestigateBlock',
+		'CheckUser',
+		'Contributions'
+	];
+	if ( excludePages.indexOf( mw.config.get( 'wgCanonicalSpecialPageName' ) ) === -1 ) {
+		require( './temporaryaccount/init.js' );
 	}
 }() );
