db5f9f97ac286a3fac9f49eaa8f5429a1d395e9b
tsepothoabala <tthoabala@wikimedia.org>
2023-03-20 10:08:00 +0200

    In Special:Block, hide IP addresses associated with a temporary account, if there are too many
    Bug: T324719
    Change-Id: Ie06d9df7b7bb2f8bb67fccd589dfa16f167a73e6

--- modules/ext.checkUser/temporaryaccount/SpecialBlock.js
+++ modules/ext.checkUser/temporaryaccount/SpecialBlock.js
@@ -1,6 +1,5 @@
 var $blockTargetWidget = $( '#mw-bi-target' );
 var blockTargetWidget;
-
 // This code is also loaded on the "block succeeded" page where there is no form,
 // so check for block target widget; if it exists, the form is present
 if ( $blockTargetWidget.length ) {
@@ -16,24 +15,75 @@ function updateIPs() {
 	if ( blockTarget.length === 0 ) {
 		$( '.ext-checkuser-tempaccount-specialblock-ips' ).empty();
 	}
+
 	if ( isTemporaryUser ) {
 		$.get(
 			mw.config.get( 'wgScriptPath' ) +
 			'/rest.php/checkuser/v0/temporaryaccount/' + blockTarget
 		).then( function ( response ) {
-			$( '#mw-htmlform-target' ).after(
-				$( '<div>' )
-					.addClass( 'ext-checkuser-tempaccount-specialblock-ips' )
-					.append(
-						new OO.ui.LabelWidget( {
-							label: mw.message(
-								'checkuser-tempaccount-specialblock-ips',
-								response.ips.length,
-								mw.language.listToText( response.ips )
-							).text()
-						} ).$element
-					)
-			);
+			var showLink = response.ips.length > 3;
+			var messageData, messageText;
+			if ( showLink ) {
+				messageData = [ response.ips[ 0 ], response.ips[ 1 ] ];
+				messageText = new OO.ui.HtmlSnippet(
+					mw.message(
+						'checkuser-tempaccount-specialblock-ips',
+						response.ips.length,
+						messageData.join( mw.msg( 'comma-separator' ) )
+					).text()
+				);
+
+				var button = new OO.ui.ButtonWidget( {
+					label: mw.message(
+						'checkuser-tempaccount-specialblock-see-more-ips',
+						response.ips.length - 2
+					).text(),
+					framed: false,
+					flags: [
+						'progressive'
+					],
+					classes: [ 'ext-checkuser-tempaccount-specialblock-ips-link' ]
+				} );
+
+				$( '#mw-htmlform-target' ).after(
+					$( '<div>' )
+						.addClass( 'ext-checkuser-tempaccount-specialblock-ips' )
+						.append( new OO.ui.LabelWidget( {
+							label: messageText
+						} ).$element )
+						.append( button.$element )
+				);
+
+				button.once( 'click', function () {
+					$( '.ext-checkuser-tempaccount-specialblock-ips' ).empty();
+					messageText = mw.message(
+						'checkuser-tempaccount-specialblock-ips',
+						response.ips.length,
+						mw.language.listToText( response.ips )
+					).text();
+					$( '#mw-htmlform-target' ).after(
+						$( '<div>' )
+							.addClass( 'ext-checkuser-tempaccount-specialblock-ips' )
+							.append( new OO.ui.LabelWidget( {
+								label: messageText
+							} ).$element )
+					);
+				} );
+
+			} else {
+				messageText = mw.message(
+					'checkuser-tempaccount-specialblock-ips',
+					response.ips.length,
+					mw.language.listToText( response.ips )
+				).text();
+				$( '#mw-htmlform-target' ).after(
+					$( '<div>' )
+						.addClass( 'ext-checkuser-tempaccount-specialblock-ips' )
+						.append( new OO.ui.LabelWidget( {
+							label: messageText
+						} ).$element )
+				);
+			}
 		} );
 	}
 }
