8e8aa1457dc35e8a3f76d0096802fb4ea0644dff
Thalia <thalia.e.chan@googlemail.com>
2023-04-13 18:12:43 +0100

    Improve readability of SpecialBlock.js
    Change-Id: I38922cfca45b0e883f0b50cc123b4363795c1131

--- modules/ext.checkUser/temporaryaccount/SpecialBlock.js
+++ modules/ext.checkUser/temporaryaccount/SpecialBlock.js
@@ -1,5 +1,6 @@
 var $blockTargetWidget = $( '#mw-bi-target' );
 var blockTargetWidget;
+
 // This code is also loaded on the "block succeeded" page where there is no form,
 // so check for block target widget; if it exists, the form is present
 if ( $blockTargetWidget.length ) {
@@ -10,33 +11,38 @@ if ( $blockTargetWidget.length ) {
 
 function updateIPs() {
 	var blockTarget = blockTargetWidget.getValue().toString().trim();
-	var isTemporaryUser = mw.util.isTemporaryUser( blockTarget );
 
 	if ( blockTarget.length === 0 ) {
 		$( '.ext-checkuser-tempaccount-specialblock-ips' ).empty();
 	}
 
-	if ( isTemporaryUser ) {
+	if ( mw.util.isTemporaryUser( blockTarget ) ) {
 		$.get(
 			mw.config.get( 'wgScriptPath' ) +
 			'/rest.php/checkuser/v0/temporaryaccount/' + blockTarget
 		).then( function ( response ) {
-			var showLink = response.ips.length > 3;
-			var messageData, messageText;
-			if ( showLink ) {
-				messageData = [ response.ips[ 0 ], response.ips[ 1 ] ];
-				messageText = new OO.ui.HtmlSnippet(
-					mw.message(
-						'checkuser-tempaccount-specialblock-ips',
-						response.ips.length,
-						messageData.join( mw.msg( 'comma-separator' ) )
-					).text()
-				);
+			var maxDisplayWithoutButton = 3;
+			var maxDisplayWithButton = maxDisplayWithoutButton - 1;
 
+			function displayWithoutButton() {
+				$( '#mw-htmlform-target' ).after(
+					$( '<div>' )
+						.addClass( 'ext-checkuser-tempaccount-specialblock-ips' )
+						.append( new OO.ui.LabelWidget( {
+							label: mw.message(
+								'checkuser-tempaccount-specialblock-ips',
+								response.ips.length,
+								mw.language.listToText( response.ips )
+							).text()
+						} ).$element )
+				);
+			}
+
+			function displayWithButton() {
 				var button = new OO.ui.ButtonWidget( {
 					label: mw.message(
 						'checkuser-tempaccount-specialblock-see-more-ips',
-						response.ips.length - 2
+						response.ips.length - ( maxDisplayWithButton )
 					).text(),
 					framed: false,
 					flags: [
@@ -44,7 +50,19 @@ function updateIPs() {
 					],
 					classes: [ 'ext-checkuser-tempaccount-specialblock-ips-link' ]
 				} );
+				button.once( 'click', function () {
+					$( '.ext-checkuser-tempaccount-specialblock-ips' ).empty();
+					displayWithoutButton();
+				} );
 
+				var messageData = response.ips.slice( 0, maxDisplayWithButton );
+				var messageText = new OO.ui.HtmlSnippet(
+					mw.message(
+						'checkuser-tempaccount-specialblock-ips',
+						response.ips.length,
+						messageData.join( mw.msg( 'comma-separator' ) )
+					).text()
+				);
 				$( '#mw-htmlform-target' ).after(
 					$( '<div>' )
 						.addClass( 'ext-checkuser-tempaccount-specialblock-ips' )
@@ -53,36 +71,12 @@ function updateIPs() {
 						} ).$element )
 						.append( button.$element )
 				);
+			}
 
-				button.once( 'click', function () {
-					$( '.ext-checkuser-tempaccount-specialblock-ips' ).empty();
-					messageText = mw.message(
-						'checkuser-tempaccount-specialblock-ips',
-						response.ips.length,
-						mw.language.listToText( response.ips )
-					).text();
-					$( '#mw-htmlform-target' ).after(
-						$( '<div>' )
-							.addClass( 'ext-checkuser-tempaccount-specialblock-ips' )
-							.append( new OO.ui.LabelWidget( {
-								label: messageText
-							} ).$element )
-					);
-				} );
-
+			if ( response.ips.length > maxDisplayWithoutButton ) {
+				displayWithButton();
 			} else {
-				messageText = mw.message(
-					'checkuser-tempaccount-specialblock-ips',
-					response.ips.length,
-					mw.language.listToText( response.ips )
-				).text();
-				$( '#mw-htmlform-target' ).after(
-					$( '<div>' )
-						.addClass( 'ext-checkuser-tempaccount-specialblock-ips' )
-						.append( new OO.ui.LabelWidget( {
-							label: messageText
-						} ).$element )
-				);
+				displayWithoutButton();
 			}
 		} );
 	}
