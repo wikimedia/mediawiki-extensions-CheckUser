0afbbead72c4e4070688e4c6dea46e27abdd8218
Dreamy Jazz <dreamyjazzwikipedia@gmail.com>
2023-05-20 18:13:08 +0300

    Add tests for the CIDR calculator widget
    Add tests for the CIDR calculator widget. This is not needed to
    split the code, so these tests are in a seperate change to make
    the first change smaller. It is also not needed for other parent
    tasks to Ifd3936b0c0e58450c034a8f69a69b1d8ea82bd65.
    
    Bug: T313291
    Change-Id: Ieb8928295f2d757456fa643307ea44b111297886

---
+++ tests/phpunit/unit/CheckUser/Widgets/CIDRCalculatorResultBoxTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace MediaWiki\CheckUser\Tests\Unit\CheckUser\Widgets;
+
+use MediaWiki\CheckUser\CheckUser\Widgets\CIDRCalculatorResultBox;
+use MediaWikiUnitTestCase;
+use Wikimedia\TestingAccessWrapper;
+
+/**
+ * @group CheckUser
+ * @coversDefaultClass \MediaWiki\CheckUser\CheckUser\Widgets\CIDRCalculatorResultBox
+ */
+class CIDRCalculatorResultBoxTest extends MediaWikiUnitTestCase {
+
+	/**
+	 * @covers ::__construct
+	 * @dataProvider provideIsAlwaysDisabled
+	 */
+	public function testIsAlwaysDisabled( $config ) {
+		$resultBox = TestingAccessWrapper::newFromObject( new CIDRCalculatorResultBox( $config ) );
+		$this->assertSame(
+			'disabled',
+			$resultBox->input->getAttribute( 'disabled' ),
+			'The input should always have the disabled attribute set.'
+		);
+	}
+
+	public function provideIsAlwaysDisabled() {
+		return [
+			'Disabled is not set in the caller\'s config' => [
+				[]
+			],
+			'Disabled is set in the caller\'s config' => [
+				[ 'disabled' => true ]
+			]
+		];
+	}
+
+	/**
+	 * @covers ::setDisabled
+	 */
+	public function testSetDisabled() {
+		$resultBox = TestingAccessWrapper::newFromObject( new CIDRCalculatorResultBox( [] ) );
+		$this->assertSame(
+			$resultBox->object,
+			$resultBox->setDisabled( false ),
+			'setDisabled should return the result box object'
+		);
+		$this->assertSame(
+			'disabled',
+			$resultBox->input->getAttribute( 'disabled' ),
+			'The input should not have been un-disabled by the setDisabled call.'
+		);
+	}
+}
