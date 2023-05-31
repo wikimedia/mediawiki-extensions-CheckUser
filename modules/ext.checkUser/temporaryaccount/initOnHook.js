15c0e5c124b4ab56b992b4d8dffc68f6547abbc7
AnaïsGueyte <agueyte@wikimedia.org>
2023-05-31 13:59:02 -0230

    Fix IP reveal persit on all pages
    Bug: T336307
    Change-Id: Ib50f2251566610885941a0763fdf725f0d43e8fc

--- modules/ext.checkUser/temporaryaccount/initOnHook.js
+++ modules/ext.checkUser/temporaryaccount/initOnHook.js
@@ -1,7 +1,20 @@
 var ipReveal = require( './ipReveal.js' );
+var ipRevealUtils = require( './ipRevealUtils.js' );
 
 mw.hook( 'wikipage.content' ).add( function ( $content ) {
 	ipReveal.addButton( $content );
 } );
 
 ipReveal.enableMultiReveal( $( document ) );
+
+// Check which users have been revealed recently
+var recentUsers = [];
+$( '.mw-tempuserlink' ).each( function () {
+	var target = $( this ).text();
+	if ( ipRevealUtils.getRevealedStatus( target ) && recentUsers.indexOf( target ) < 0 ) {
+		recentUsers.push( target );
+	}
+} );
+recentUsers.forEach( function ( user ) {
+	$( document ).trigger( 'userRevealed', user );
+} );
--- modules/ext.checkUser/temporaryaccount/initOnLoad.js
+++ modules/ext.checkUser/temporaryaccount/initOnLoad.js
@@ -1,5 +1,18 @@
 var ipReveal = require( './ipReveal.js' );
+var ipRevealUtils = require( './ipRevealUtils.js' );
 
 ipReveal.addButton( $( '#bodyContent' ) );
 
 ipReveal.enableMultiReveal( $( document ) );
+
+// Check which users have been revealed recently
+var recentUsers = [];
+$( '.mw-tempuserlink' ).each( function () {
+	var target = $( this ).text();
+	if ( ipRevealUtils.getRevealedStatus( target ) && recentUsers.indexOf( target ) < 0 ) {
+		recentUsers.push( target );
+	}
+} );
+recentUsers.forEach( function ( user ) {
+	$( document ).trigger( 'userRevealed', user );
+} );
