82f7e120817c999dac29ed8bdeefa5517a1c22f9
Martin Urbanec <martin.urbanec@wikimedia.cz>
2023-06-07 01:36:08 +0200

    SECURITY: Close a DoS vector by an index hint
    Bug: T338276
    Change-Id: I10a9273c542576b3f7bb38de68dcd2aa41cfb1b0

--- src/Hooks.php
+++ src/Hooks.php
@@ -1205,7 +1205,9 @@ class Hooks implements
 		}
 
 		$res = $dbr->newSelectQueryBuilder()
-			->tables( [ 'cu_changes', 'actor' ] )
+			->table( 'cu_changes' )
+			->useIndex( 'cuc_actor_ip_time' )
+			->table( 'actor' )
 			->field( 'cuc_ip' )
 			->conds( [ 'actor_user' => $user->getId( $block->getWikiId() ) ] )
 			->joinConds( [ 'actor' => [ 'JOIN', 'actor_id=cuc_actor' ] ] )
