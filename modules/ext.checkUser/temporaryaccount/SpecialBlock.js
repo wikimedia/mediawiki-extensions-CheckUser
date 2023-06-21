521651ea00ff75fe5eb095a6824d6e74701a9084
Thalia <thalia.e.chan@googlemail.com>
2023-06-21 15:16:52 +0100

    Check temp user exists before adding IP lookup button to Special:Block
    Bug: T338154
    Change-Id: I8e58eab16c2cdda17869fb0b6da2819652a52a99

--- modules/ext.checkUser/temporaryaccount/SpecialBlock.js
+++ modules/ext.checkUser/temporaryaccount/SpecialBlock.js
@@ -1,13 +1,16 @@
 var $blockTargetWidget = $( '#mw-bi-target' );
-var blockTargetWidget, lastRequest;
+var blockTargetWidget, lastUserRequest, lastIpRequest;
 
 // This code is also loaded on the "block succeeded" page where there is no form,
 // so check for block target widget; if it exists, the form is present
 if ( $blockTargetWidget.length ) {
 	blockTargetWidget = OO.ui.infuse( $blockTargetWidget );
 	blockTargetWidget.on( 'change', function ( blockTarget ) {
-		if ( lastRequest ) {
-			lastRequest.abort();
+		if ( lastUserRequest ) {
+			lastUserRequest.abort();
+		}
+		if ( lastIpRequest ) {
+			lastIpRequest.abort();
 		}
 		onTargetChange( blockTarget );
 	} );
@@ -28,42 +31,51 @@ function onTargetChange( blockTarget ) {
 	if ( !mw.util.isTemporaryUser( blockTarget ) ) {
 		return;
 	}
+	var api = new mw.Api();
+	lastUserRequest = api.get( {
+		action: 'query',
+		list: 'users',
+		ususers: blockTargetWidget.getValue()
+	} );
+	lastUserRequest.done( function ( data ) {
+		if ( data.query.users[ 0 ].userid ) {
+			var revealButton = createButton(
+				mw.msg( 'checkuser-tempaccount-reveal-ip-button-label' )
+			);
+			var $container = $( '<div>' )
+				.addClass( 'ext-checkuser-tempaccount-specialblock-ips' )
+				.append( revealButton.$element );
+			$( '#mw-htmlform-target' ).after( $container );
 
-	var revealButton = createButton(
-		mw.msg( 'checkuser-tempaccount-reveal-ip-button-label' )
-	);
-	var $container = $( '<div>' )
-		.addClass( 'ext-checkuser-tempaccount-specialblock-ips' )
-		.append( revealButton.$element );
-	$( '#mw-htmlform-target' ).after( $container );
+			revealButton.once( 'click', function () {
+				$container.empty();
 
-	revealButton.once( 'click', function () {
-		$container.empty();
-
-		lastRequest = $.get(
-			mw.config.get( 'wgScriptPath' ) +
-			'/rest.php/checkuser/v0/temporaryaccount/' + blockTarget
-		);
-		lastRequest.then( function ( response ) {
-			$container.empty()
-				.append( new OO.ui.LabelWidget( {
-					label: response.ips.length ?
-						mw.message(
-							'checkuser-tempaccount-specialblock-ips',
-							response.ips.length,
-							mw.language.listToText( response.ips )
-						).text() :
-						mw.message(
-							'checkuser-tempaccount-no-ip-results',
-							Math.round( mw.config.get( 'wgCUDMaxAge' ) / 86400 )
-						).text()
-				} ).$element );
-		} ).fail( function () {
-			$container.empty()
-				.addClass( 'ext-checkuser-tempaccount-reveal-ip' )
-				.append( new OO.ui.LabelWidget( {
-					label: mw.message( 'checkuser-tempaccount-reveal-ip-error' ).text()
-				} ).$element );
-		} );
+				lastIpRequest = $.get(
+					mw.config.get( 'wgScriptPath' ) +
+					'/rest.php/checkuser/v0/temporaryaccount/' + blockTarget
+				);
+				lastIpRequest.then( function ( response ) {
+					$container.empty()
+						.append( new OO.ui.LabelWidget( {
+							label: response.ips.length ?
+								mw.message(
+									'checkuser-tempaccount-specialblock-ips',
+									response.ips.length,
+									mw.language.listToText( response.ips )
+								).text() :
+								mw.message(
+									'checkuser-tempaccount-no-ip-results',
+									Math.round( mw.config.get( 'wgCUDMaxAge' ) / 86400 )
+								).text()
+						} ).$element );
+				} ).fail( function () {
+					$container.empty()
+						.addClass( 'ext-checkuser-tempaccount-reveal-ip' )
+						.append( new OO.ui.LabelWidget( {
+							label: mw.message( 'checkuser-tempaccount-reveal-ip-error' ).text()
+						} ).$element );
+				} );
+			} );
+		}
 	} );
 }
