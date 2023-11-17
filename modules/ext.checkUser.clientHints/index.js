529c88f0bfdda1a47c5a789e924a54f8acaa0944
Kosta Harlan <kharlan@wikimedia.org>
2023-11-17 14:57:01 +0100

    clienthints: Ensure getHighEntropyValues() exists
    Why:
    
    - Some browsers have implemented navigator.userAgentData but not
      navigator.getHighEntropyValues
    
    What:
    
    - Make sure navigator.userAgentData.getHighEntropyValues() exists
      before calling it
    - Lightly refactor the clientHints/index.js file to support unit
      testing
    - Add basic tests for verifying if we are setting up the
      listener code
      - Some day, we may want to spy on mw.Rest() and make assertions about
        its invocation after firing the postEdit hook, but this is
    	complicated by the fact that we'd need to use async assertions, and
    	is maybe more trouble than it's worth at the moment
    
    Bug: T350318
    Change-Id: Ic53340aaa1aa17d615750726be735186edf662a2

--- modules/ext.checkUser.clientHints/index.js
+++ modules/ext.checkUser.clientHints/index.js
@@ -1,54 +1,70 @@
 ( function () {
-	if ( !navigator.userAgentData ) {
-		// The browser doesn't support navigator.userAgentData
-		return;
-	}
-
-	var wgCheckUserClientHintsHeadersJsApi =
-		mw.config.get( 'wgCheckUserClientHintsHeadersJsApi' );
-
 	/**
-	 * POST an object with user-agent client hint data to a CheckUser REST endpoint.
+	 * Set up the listener for the postEdit hook, if client hints are supported by the browser.
 	 *
-	 * @param {Object} clientHintData Data structured returned by
-	 *  navigator.userAgentData.getHighEntropyValues()
-	 * @return {jQuery.Promise} A promise that resolves after the POST is complete.
+	 * @param {Navigator|Object} navigatorData
+	 * @return {boolean} true if client hints integration has been set up on postEdit hook,
+	 *   false otherwise.
 	 */
-	function postClientHintData( clientHintData ) {
-		return new mw.Rest().post(
-			'/checkuser/v0/useragent-clienthints/revision/' + mw.config.get( 'wgCurRevisionId' ),
-			clientHintData
-		).fail( function ( _err, errObject ) {
-			mw.log.error( errObject );
-			var errMessage = errObject.exception;
-			if ( errObject.xhr &&
+	function init( navigatorData ) {
+		var hasHighEntropyValuesMethod = navigatorData.userAgentData &&
+			navigatorData.userAgentData.getHighEntropyValues;
+		if ( !hasHighEntropyValuesMethod ) {
+			// The browser doesn't support navigator.userAgentData.getHighEntropyValues. Used
+			// for tests.
+			return false;
+		}
+
+		var wgCheckUserClientHintsHeadersJsApi = mw.config.get( 'wgCheckUserClientHintsHeadersJsApi' );
+
+		/**
+		 * POST an object with user-agent client hint data to a CheckUser REST endpoint.
+		 *
+		 * @param {Object} clientHintData Data structured returned by
+		 *  navigator.userAgentData.getHighEntropyValues()
+		 * @return {jQuery.Promise} A promise that resolves after the POST is complete.
+		 */
+		function postClientHintData( clientHintData ) {
+			return new mw.Rest().post(
+				'/checkuser/v0/useragent-clienthints/revision/' + mw.config.get( 'wgCurRevisionId' ),
+				clientHintData
+			).fail( function ( _err, errObject ) {
+				mw.log.error( errObject );
+				var errMessage = errObject.exception;
+				if ( errObject.xhr &&
 				errObject.xhr.responseJSON &&
 				errObject.xhr.responseJSON.messageTranslations ) {
-				errMessage = errObject.xhr.responseJSON.messageTranslations.en;
-			}
-			mw.errorLogger.logError( new Error( errMessage ), 'error.checkuser' );
-		} );
-	}
+					errMessage = errObject.xhr.responseJSON.messageTranslations.en;
+				}
+				mw.errorLogger.logError( new Error( errMessage ), 'error.checkuser' );
+			} );
+		}
 
-	/**
-	 * Respond to postEdit hook, fired by MediaWiki core, VisualEditor and DiscussionTools.
-	 *
-	 * Note that CheckUser only adds this code to article page views if
-	 * CheckUserClientHintsEnabled is set to true.
-	 */
-	mw.hook( 'postEdit' ).add( function () {
-		try {
-			// eslint-disable-next-line compat/compat
-			navigator.userAgentData
-				.getHighEntropyValues(
+		/**
+		 * Respond to postEdit hook, fired by MediaWiki core, VisualEditor and DiscussionTools.
+		 *
+		 * Note that CheckUser only adds this code to article page views if
+		 * CheckUserClientHintsEnabled is set to true.
+		 */
+		mw.hook( 'postEdit' ).add( function () {
+			try {
+				navigatorData.userAgentData.getHighEntropyValues(
 					wgCheckUserClientHintsHeadersJsApi
 				).then( function ( userAgentHighEntropyValues ) {
 					return postClientHintData( userAgentHighEntropyValues );
 				} );
-		} catch ( err ) {
-			// Handle NotAllowedError, if the browser throws it.
-			mw.log.error( err );
-			mw.errorLogger.logError( new Error( err ), 'error.checkuser' );
-		}
-	} );
+			} catch ( err ) {
+				// Handle NotAllowedError, if the browser throws it.
+				mw.log.error( err );
+				mw.errorLogger.logError( new Error( err ), 'error.checkuser' );
+			}
+		} );
+		return true;
+	}
+
+	init( navigator );
+
+	module.exports = {
+		init: init
+	};
 }() );
--- tests/qunit/index.js
+++ tests/qunit/index.js
@@ -7,3 +7,4 @@ require( './ext.checkUser/checkUserHelper/utils.test.js' );
 require( './ext.checkUser/checkUserHelper/createTableText.test.js' );
 require( './ext.checkUser/checkUserHelper/createTable.test.js' );
 require( './ext.checkUser/checkUserHelper/generateData.test.js' );
+require( './ext.checkUser.clientHints/index.test.js' );
