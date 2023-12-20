f29400e27ecad345850f589974d9735bd8cc64ff
Dreamy Jazz <wpgbrown@wikimedia.org>
2023-12-20 14:32:18 +0000

    Partially test CheckUserLogPager::getQueryInfo
    Why:
    * Unit tests for I0e5e35b89cdf5e4f881f5e68f56ac8cde4b9f8a8 were
      unable to be written initially because of a use of a static method
      that called MediaWikiServices.
    * With the refactoring in Iea0ec97ba5d3226e55d608f5cf234dac462fd162,
      these static methods are now in the CheckUserLogService and no
      longer static, so can be mocked in a unit test.
    * This means that tests can be added for the original change.
    
    What:
    * Add a test for CheckUserLogPager::getQueryInfo that checks that
      the USE INDEX statement is added correctly for a variety of
      targets.
    
    Bug: T342639
    Change-Id: I47d7d7d5d22c91d8c51eb57699b864f639df115e

--- tests/phpunit/unit/CheckUser/Pagers/CheckUserLogPagerTest.php
+++ tests/phpunit/unit/CheckUser/Pagers/CheckUserLogPagerTest.php
@@ -3,6 +3,8 @@
 namespace MediaWiki\CheckUser\Tests\Unit\CheckUser\Pagers;
 
 use MediaWiki\CheckUser\CheckUser\Pagers\CheckUserLogPager;
+use MediaWiki\CheckUser\Services\CheckUserLogService;
+use MediaWiki\CommentStore\CommentStore;
 use MediaWiki\User\ActorStore;
 use MediaWikiUnitTestCase;
 use Wikimedia\Rdbms\IReadableDatabase;
@@ -57,6 +59,69 @@ class CheckUserLogPagerTest extends MediaWikiUnitTestCase {
 		return [
 			'IP' => [ '127.0.0.1' ],
 			'User' => [ 'TestAccount' ]
+		];
+	}
+
+	/** @dataProvider provideGetQueryInfo */
+	public function testGetQueryInfo( $opts, $expectedOptions ) {
+		$objectUnderTest = $this->getMockBuilder( CheckUserLogPager::class )
+			->disableOriginalConstructor()
+			->onlyMethods( [] )
+			->getMock();
+		// Use a mock CommentStore in the test that returns an array of empty
+		// arrays for the call to ::getJoin
+		$mockCommentStore = $this->createMock( CommentStore::class );
+		$mockCommentStore->method( 'getJoin' )
+			->willReturn( [
+				'tables' => [],
+				'fields' => [],
+				'joins' => [],
+			] );
+		// Use a mock CheckUserLogService in the test that a mock ::getTargetSearchConds result
+		$mockCheckUserLogService = $this->createMock( CheckUserLogService::class );
+		$mockCheckUserLogService->method( 'getTargetSearchConds' )
+			->willReturn( [] );
+		// Assign the mock services to the object under test
+		$objectUnderTest = TestingAccessWrapper::newFromObject( $objectUnderTest );
+		$objectUnderTest->checkUserLogService = $mockCheckUserLogService;
+		$objectUnderTest->commentStore = $mockCommentStore;
+		// Assign $opts to the object under test
+		$objectUnderTest->opts = $opts;
+		// Call the method under test
+		$actualQueryInfo = $objectUnderTest->getQueryInfo();
+		$this->assertArrayEquals(
+			$objectUnderTest->selectFields(),
+			$actualQueryInfo['fields'],
+			'::getQueryInfo should use the fields from ::selectFields.'
+		);
+		$this->assertArrayEquals(
+			[ 'cu_log', 'cu_log_actor' => 'actor' ],
+			$actualQueryInfo['tables'],
+			'::getQueryInfo did not return the expected tables.'
+		);
+		$this->assertArrayEquals(
+			$expectedOptions,
+			$actualQueryInfo['options'],
+			'::getQueryInfo did not return the expected options array.'
+		);
+	}
+
+	public static function provideGetQueryInfo() {
+		return [
+			'IP specified as the target' => [
+				// $objectUnderTest->opts value for the test.
+				[ 'target' => '1.2.3.4', 'initiator' => '', 'reason' => '' ],
+				// The expected array for the 'options' array returned by ::getQueryInfo
+				[ 'USE INDEX' => [ 'cu_log' => 'cul_target_hex' ] ]
+			],
+			'IP range specified as the target' => [
+				[ 'target' => '1.2.3.4/22', 'initiator' => '', 'reason' => '' ],
+				[ 'USE INDEX' => [ 'cu_log' => 'cul_target_hex' ] ]
+			],
+			'User specified as the target' => [
+				[ 'target' => 'Testinguser', 'initiator' => '', 'reason' => '' ],
+				[]
+			],
 		];
 	}
 }
