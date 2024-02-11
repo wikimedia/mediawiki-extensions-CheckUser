42cea5bdf75d83fc0f73a20200b3b4b54fa4f1d6
Taavi Väänänen <hi@taavi.wtf>
2024-02-11 12:49:36 +0200

    build: Update eslint-config-wikimedia to 0.26.0
    Change-Id: Idfb16f9d7f0eee26aae0ade4fc3264867f2a1172

--- tests/selenium/pageobjects/investigate.page.js
+++ tests/selenium/pageobjects/investigate.page.js
@@ -3,10 +3,21 @@
 const Page = require( 'wdio-mediawiki/Page' );
 
 class InvestigatePage extends Page {
-	get hasPermissionErrors() { return $( '.permissions-errors' ); }
-	get targetsInput() { return $( '#targets' ); }
-	get durationSelector() { return $( '#investigate-duration' ); }
-	get reasonInput() { return $( '#investigate-reason' ); }
+	get hasPermissionErrors() {
+		return $( '.permissions-errors' );
+	}
+
+	get targetsInput() {
+		return $( '#targets' );
+	}
+
+	get durationSelector() {
+		return $( '#investigate-duration' );
+	}
+
+	get reasonInput() {
+		return $( '#investigate-reason' );
+	}
 
 	async open() {
 		await super.openTitle( 'Special:Investigate' );
