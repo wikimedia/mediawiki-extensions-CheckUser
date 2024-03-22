5026ab2392be6c4f98171d1be99643e650d947bb
Dreamy Jazz <wpgbrown@wikimedia.org>
2024-03-22 02:25:21 +0000

    Add ipusers and userips response classes the for CheckUser API
    Why:
    * The ApiQueryCheckUserAbstractResponse class was created in
      previous commits, and was intended to for extending by other
      classes that implemented the response to a particular request.
    * This commit adds support for the ipusers and userips request
      types, including adding them to the factory.
    * These classes are currently not tested, but will be in a future
      commit where the ApiQueryCheckUser class is converted to use
      these response classes.
    
    What:
    * Create ApiQueryCheckUserIpUsersResponse and
      ApiQueryCheckUserUserIpsResponse.
    * Add support for these response classes to the factory service.
    
    Bug: T360963
    Bug: T360962
    Change-Id: I71e835622b8b9f29ac7f92de8e5c6a48b8b1aea0

---
+++ src/Api/CheckUser/ApiQueryCheckUserIpUsersResponse.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace MediaWiki\CheckUser\Api\CheckUser;
+
+use ApiResult;
+use Wikimedia\Rdbms\IExpression;
+use Wikimedia\Rdbms\SelectQueryBuilder;
+use Wikimedia\Timestamp\ConvertibleTimestamp;
+
+class ApiQueryCheckUserIpUsersResponse extends ApiQueryCheckUserAbstractResponse {
+
+	/** @inheritDoc */
+	public function getRequestType(): string {
+		return 'ipusers';
+	}
+
+	/** @inheritDoc */
+	public function getResponseData(): array {
+		$res = $this->performQuery( __METHOD__ );
+
+		$users = [];
+		foreach ( $res as $row ) {
+			$user = $row->user_text;
+			$ip = $row->ip;
+			$agent = $row->agent;
+
+			if ( !isset( $users[$user] ) ) {
+				$users[$user] = [
+					'end' => ConvertibleTimestamp::convert( TS_ISO_8601, $row->timestamp ),
+					'editcount' => 1,
+					'ips' => [ $ip ],
+					'agents' => [ $agent ]
+				];
+			} else {
+				$users[$user]['start'] = ConvertibleTimestamp::convert( TS_ISO_8601, $row->timestamp );
+				$users[$user]['editcount']++;
+				if ( !in_array( $ip, $users[$user]['ips'] ) ) {
+					$users[$user]['ips'][] = $ip;
+				}
+				if ( !in_array( $agent, $users[$user]['agents'] ) ) {
+					$users[$user]['agents'][] = $agent;
+				}
+			}
+		}
+
+		$resultUsers = [];
+		foreach ( $users as $userName => $userData ) {
+			$userData['name'] = $userName;
+			ApiResult::setIndexedTagName( $userData['ips'], 'ip' );
+			ApiResult::setIndexedTagName( $userData['agents'], 'agent' );
+
+			$resultUsers[] = $userData;
+		}
+
+		$logType = 'ipusers';
+		if ( $this->xff === true ) {
+			$logType .= '-xff';
+		}
+		$this->checkUserLogService->addLogEntry(
+			$this->module->getUser(), $logType, 'ip', $this->target, $this->reason
+		);
+		return $resultUsers;
+	}
+
+	/** @inheritDoc */
+	protected function validateTargetAndGenerateTargetConditions( string $table ): IExpression {
+		$ipExpr = $this->checkUserLookupUtils->getIPTargetExpr( $this->target, $this->xff ?? false, $table );
+		if ( $ipExpr === null ) {
+			$this->module->dieWithError( 'apierror-badip', 'invalidip' );
+		}
+		return $ipExpr;
+	}
+
+	/** @inheritDoc */
+	protected function getPartialQueryBuilderForCuChanges(): SelectQueryBuilder {
+		$queryBuilder = $this->dbr->newSelectQueryBuilder()
+			->select( [
+				'timestamp' => 'cuc_timestamp',
+				'ip' => 'cuc_ip',
+				'agent' => 'cuc_agent',
+				'user_text' => 'actor_name',
+				'actor' => 'cuc_actor',
+			] )
+			->from( 'cu_changes' )
+			->join( 'actor', null, 'actor_id=cuc_actor' )
+			->where( $this->dbr->expr( 'cuc_timestamp', '>', $this->timeCutoff ) );
+		// When reading new, only select results from cu_changes that are
+		// for read new (defined as those with cuc_only_for_read_old set to 0).
+		if ( $this->eventTableReadNew ) {
+			$queryBuilder->andWhere( [ 'cuc_only_for_read_old' => 0 ] );
+		}
+		return $queryBuilder;
+	}
+
+	/** @inheritDoc */
+	protected function getPartialQueryBuilderForCuLogEvent(): SelectQueryBuilder {
+		return $this->dbr->newSelectQueryBuilder()
+			->select( [
+				'timestamp' => 'cule_timestamp',
+				'ip' => 'cule_ip',
+				'agent' => 'cule_agent',
+				'user_text' => 'actor_name',
+				'actor' => 'cule_actor',
+			] )
+			->from( 'cu_log_event' )
+			->join( 'actor', null, 'actor_id=cule_actor' )
+			->where( $this->dbr->expr( 'cule_timestamp', '>', $this->timeCutoff ) );
+	}
+
+	/** @inheritDoc */
+	protected function getPartialQueryBuilderForCuPrivateEvent(): SelectQueryBuilder {
+		return $this->dbr->newSelectQueryBuilder()
+			->select( [
+				'timestamp' => 'cupe_timestamp',
+				'ip' => 'cupe_ip',
+				'agent' => 'cupe_agent',
+				'user_text' => 'actor_name',
+				'actor' => 'cupe_actor',
+			] )
+			->from( 'cu_private_event' )
+			->leftJoin( 'actor', null, 'actor_id=cupe_actor' )
+			->where( $this->dbr->expr( 'cupe_timestamp', '>', $this->timeCutoff ) );
+	}
+}
