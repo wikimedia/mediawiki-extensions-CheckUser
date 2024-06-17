59921f58ad93bdc23097fad4e445c7cb18f2d310
Thalia <thalia.e.chan@googlemail.com>
2024-06-17 18:11:20 +0100

    IPContributionsPager: Improve revision query performance
    Why:
    
    * IPContributionsPager fetches information about revisions made
      by temporary users, given an IP address or range.
    * The query is similar to the query used by ContribsPager, but
      must use the `cu_changes` table to look up the IPs of the users.
    * To make the query efficient, indexes on `cu_changes` are used.
    * The query joins on several other tables, in order to display
      lots of metadata to users.
    * Before this patch, `cu_changes` table joins on `revision`, and
      all the other joins are done on `revision`. This is expected to
      be inefficient, and all joins could instead be done on
      `cu_changes`.
    
    What:
    
    * Join to other tables via `cu_changes` instead of `revision`.
    * Ensure the fields are still named in a way that RevisionStore
      can recognize.
    
    Bug: T367091
    Change-Id: I341dbcbe74f27e6d3da26ae9e8981afaae9a067f

--- src/IPContributions/IPContributionsPager.php
+++ src/IPContributions/IPContributionsPager.php
@@ -2,6 +2,7 @@
 
 namespace MediaWiki\CheckUser\IPContributions;
 
+use LogicException;
 use MediaWiki\Cache\LinkBatchFactory;
 use MediaWiki\CheckUser\Services\CheckUserLookupUtils;
 use MediaWiki\CommentFormatter\CommentFormatter;
@@ -15,6 +16,7 @@ use MediaWiki\User\TempUser\TempUserConfig;
 use MediaWiki\User\UserFactory;
 use MediaWiki\User\UserIdentity;
 use Wikimedia\Rdbms\IExpression;
+use Wikimedia\Rdbms\SelectQueryBuilder;
 
 class IPContributionsPager extends ContributionsPager {
 	private TempUserConfig $tempUserConfig;
@@ -32,7 +34,7 @@ class IPContributionsPager extends ContributionsPager {
 	 * @param CheckUserLookupUtils $checkUserLookupUtils
 	 * @param IContextSource $context
 	 * @param array $options
-	 * @param ?UserIdentity $target
+	 * @param ?UserIdentity $target IP address for temporary user contributions lookup
 	 */
 	public function __construct(
 		LinkRenderer $linkRenderer,
@@ -68,33 +70,55 @@ class IPContributionsPager extends ContributionsPager {
 	 * @inheritDoc
 	 */
 	protected function getRevisionQuery() {
-		$revQuery = $this->revisionStore->getQueryInfo( [ 'page', 'user' ] );
-		$queryInfo = [
-			'tables' => $revQuery['tables'],
-			'fields' => array_merge( $revQuery['fields'], [ 'page_is_new' ] ),
-			'conds' => [],
-			'options' => [],
-			'join_conds' => $revQuery['joins'],
-		];
+		$queryBuilder = new SelectQueryBuilder( $this->getDatabase() );
+		$queryBuilder
+			->select( [
+				// The fields prefixed with rev_ must be named that way so that
+				// RevisionStore::isRevisionRow can recognize the row.
+				'rev_id',
+				'rev_page',
+				'rev_actor',
+				'rev_user' => 'cu_changes_actor.actor_user',
+				'rev_user_text' => 'cu_changes_actor.actor_name',
+				'rev_timestamp',
+				'rev_minor_edit',
+				'rev_deleted',
+				'rev_len',
+				'rev_parent_id',
+				'rev_sha1',
+				'rev_comment_text' => 'cu_changes_comment.comment_text',
+				'rev_comment_data' => 'cu_changes_comment.comment_data',
+				'rev_comment_cid' => 'cu_changes_comment.comment_id',
+				'page_latest',
+				'page_is_new',
+				'page_namespace',
+				'page_title',
+			] )
+			->from( 'cu_changes' )
+			->join( 'actor', 'cu_changes_actor', 'actor_id=cuc_actor' )
+			->join( 'revision', 'cu_changes_revision', 'rev_id=cuc_this_oldid' )
+			->join( 'page', 'cu_changes_page', 'page_id=cuc_page_id' )
+			->join( 'comment', 'cu_changes_comment', 'comment_id=cuc_comment_id' );
 
-		array_unshift( $queryInfo['tables'], 'cu_changes' );
-		$queryInfo['join_conds']['revision'] = [
-			'JOIN', [ 'rev_id = cuc_this_oldid' ]
-		];
-
-		$queryInfo['conds'][] = $this->checkUserLookupUtils->getIPTargetExpr(
+		$ipConditions = $this->checkUserLookupUtils->getIPTargetExpr(
 			$this->target,
 			false,
 			'cu_changes'
 		);
+		if ( $ipConditions === null ) {
+			throw new LogicException( "Attempted IP contributions lookup with non-IP target: $this->target" );
+		}
 
-		$queryInfo['conds'][] = $this->tempUserConfig->getMatchCondition(
+		$tempUserConditions = $this->tempUserConfig->getMatchCondition(
 			$this->getDatabase(),
 			'actor_name',
 			IExpression::LIKE
 		);
 
-		return $queryInfo;
+		$queryBuilder->where( $ipConditions );
+		$queryBuilder->where( $tempUserConditions );
+
+		return $queryBuilder->getQueryInfo();
 	}
 
 	/**
