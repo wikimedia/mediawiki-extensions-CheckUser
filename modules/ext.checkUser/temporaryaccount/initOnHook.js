8fe1fdd9f2b146fd55f9c738099c4bfc4f0adb2a
STran <stran@wikimedia.org>
2024-06-20 11:04:47 +0100

    Check for recent IP reveals on wikipage content refresh
    When content was reloaded without reloading the page, the ip reveal
    feature wasn't doing a check to automatically reveal recently revealed
    IPs, resulting in a "Show IP" button when the user would expect the IP.
    
    - Add the check and reveal on the `wikipage.content` hook
    
    Bug: T345574
    Change-Id: I9943327e0751d42f91541149aa99a50afe59c689

--- modules/ext.checkUser/temporaryaccount/initOnHook.js
+++ modules/ext.checkUser/temporaryaccount/initOnHook.js
@@ -1,20 +1,24 @@
 const ipReveal = require( './ipReveal.js' );
 const ipRevealUtils = require( './ipRevealUtils.js' );
 
+// Check which users have been revealed recently
+function checkRecentlyRevealedUsers() {
+	const recentUsers = [];
+	$( '.mw-tempuserlink' ).each( function () {
+		const target = $( this ).text();
+		if ( ipRevealUtils.getRevealedStatus( target ) && recentUsers.indexOf( target ) < 0 ) {
+			recentUsers.push( target );
+		}
+	} );
+	recentUsers.forEach( ( user ) => {
+		$( document ).trigger( 'userRevealed', user );
+	} );
+}
+
 mw.hook( 'wikipage.content' ).add( ( $content ) => {
 	ipReveal.addButton( $content );
+	checkRecentlyRevealedUsers();
 } );
 
 ipReveal.enableMultiReveal( $( document ) );
-
-// Check which users have been revealed recently
-const recentUsers = [];
-$( '.mw-tempuserlink' ).each( function () {
-	const target = $( this ).text();
-	if ( ipRevealUtils.getRevealedStatus( target ) && recentUsers.indexOf( target ) < 0 ) {
-		recentUsers.push( target );
-	}
-} );
-recentUsers.forEach( ( user ) => {
-	$( document ).trigger( 'userRevealed', user );
-} );
+checkRecentlyRevealedUsers();
