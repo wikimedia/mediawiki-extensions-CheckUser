4643b6bb2cdbdc469647d6b0911e8ca3fbe30ef7
Dreamy Jazz <wpgbrown@wikimedia.org>
2024-08-21 20:41:56 +0100

    Use CheckUserDataPruner in the purgeOldData.php maintenance script
    Why:
    * The purgeOldData.php maintenance script contains a fair bit
      of duplicated code to the PruneCheckUserDataJob.
    * Using the new CheckUserDataPurger service to de-duplicate this,
      along with a maintenance script added to core in
      Id32780c00b053d7fec94a00812f8d40a6bf2e40c will allow reduction
      of code duplication along with the ability to reduce
      duplicated tests in later commits.
    
    What:
    * Update the purgeOldData.php maintenance script to purge data
      from the CheckUser result tables using the CheckUserDataPurger
      service.
    ** For the recentchanges purge, make use of the maintenance script
       added to MediaWiki core in
       Id32780c00b053d7fec94a00812f8d40a6bf2e40c to do this purge.
    * Update the CheckUserDataPurger::purgeDataFromLocalTable method
      to support being used by the maintenance script in the following
      ways:
    ** Generate the Client Hints reference field from the table name
       to remove an argument from the method
    ** Allow the caller to specify how many rows to purge
    ** Make the return value be the number of purged rows
    * Update PruneCheckUserData to reflect the new argument structure
      for the CheckUserDataPurger::purgeDataFromLocalTable method,
      and while doing it simplify the code by using a for loop.
    * No tests are modified in this commit so that they can be used
      to verify that no functionality changes were made in this change.
    
    Depends-On: Id32780c00b053d7fec94a00812f8d40a6bf2e40c
    Bug: T359560
    Change-Id: I20631de296fb04cb74a482293a64d64b4088b35d

--- src/Services/CheckUserDataPurger.php
+++ src/Services/CheckUserDataPurger.php
@@ -2,6 +2,7 @@
 
 namespace MediaWiki\CheckUser\Services;
 
+use MediaWiki\CheckUser\CheckUserQueryInterface;
 use MediaWiki\CheckUser\ClientHints\ClientHintsReferenceIds;
 use Wikimedia\Rdbms\IDatabase;
 
@@ -11,38 +12,42 @@ use Wikimedia\Rdbms\IDatabase;
  *
  * @internal For use by PruneCheckUserDataJob and purgeOldData.php only
  */
-class CheckUserDataPurger {
+class CheckUserDataPurger implements CheckUserQueryInterface {
 
 	/**
-	 * Purge 500 rows from the given local CheckUser result table.
+	 * Purge rows from the given local CheckUser result table.
 	 *
 	 * @param IDatabase $dbw A primary database connection for the database we are purging rows from. This is provided
 	 *   via the arguments in case the connection has an exclusive lock for the purging of rows.
 	 * @param string $table The relevant CheckUser result table (e.g. cu_changes)
-	 * @param string $idField The ID field for the $table (e.g. cuc_id)
-	 * @param string $timestampField The timestamp field for the $table (e.g. cuc_timestamp)
 	 * @param string $cutoff The timestamp used as a "cutoff", where rows which have a timestamp before the given
 	 *   cutoff are eligible to be purged from the database
-	 * @param int $clientHintMapTypeIdentifier The UserAgentClientHintsManager::IDENTIFIER_* constant for the given
-	 *   $table
 	 * @param ClientHintsReferenceIds $deletedReferenceIds A {@link ClientHintsReferenceIds} instance used to collect
 	 *   the reference IDs associated with rows that were purged by the call to this method. The caller is
 	 *   responsible for purging the Client Hints data for these reference IDs.
+	 * @param string $fname The name of the calling method / function, used for the SQL comments
+	 * @param int $totalRowsToPurge The maximum number of rows to purge, default 500
+	 * @return int The number of rows that were purged
 	 */
 	public function purgeDataFromLocalTable(
-		IDatabase $dbw, string $table, string $idField, string $timestampField, string $cutoff,
-		int $clientHintMapTypeIdentifier, ClientHintsReferenceIds $deletedReferenceIds
-	) {
-		// Get at most 500 rows to purge from the given $table, selecting the row ID and associated Client Hints
-		// data reference ID
+		IDatabase $dbw, string $table, string $cutoff, ClientHintsReferenceIds $deletedReferenceIds,
+		string $fname, int $totalRowsToPurge = 500
+	): int {
+		// Get the Client Hints reference field for the given table
+		$clientHintMapTypeIdentifier = array_flip( UserAgentClientHintsManager::IDENTIFIER_TO_TABLE_NAME_MAP )[$table];
 		$clientHintReferenceField =
 			UserAgentClientHintsManager::IDENTIFIER_TO_COLUMN_NAME_MAP[$clientHintMapTypeIdentifier];
+		// Get the timestamp and ID columns for the given $table
+		$idField = self::RESULT_TABLE_TO_PREFIX[$table] . 'id';
+		$timestampField = self::RESULT_TABLE_TO_PREFIX[$table] . 'timestamp';
+		// Get at most 500 rows to purge from the given $table, selecting the row ID and associated Client Hints
+		// data reference ID
 		$idQueryBuilder = $dbw->newSelectQueryBuilder()
 			->field( $idField )
 			->table( $table )
 			->conds( $dbw->expr( $timestampField, '<', $cutoff ) )
-			->limit( 500 )
-			->caller( __METHOD__ );
+			->limit( $totalRowsToPurge )
+			->caller( $fname );
 		if ( $clientHintReferenceField !== $idField ) {
 			$idQueryBuilder->field( $clientHintReferenceField );
 		}
@@ -59,8 +64,12 @@ class CheckUserDataPurger {
 			$dbw->newDeleteQueryBuilder()
 				->table( $table )
 				->where( [ $idField => $ids ] )
-				->caller( __METHOD__ )
+				->caller( $fname )
 				->execute();
 		}
+		// The number of IDs found will be the number of rows purged, as the delete statement above will have purged
+		// them. This being accurate (i.e. some rows were not deleted) is not important, as it is used only
+		// for stats purposes in purgeOldData.php.
+		return count( $ids );
 	}
 }
