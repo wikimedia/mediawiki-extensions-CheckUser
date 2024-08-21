a306001c69ede13b8bdc14c02b2a409e5768117a
Dreamy Jazz <wpgbrown@wikimedia.org>
2024-08-21 14:13:10 +0100

    Clean up code in PruneCheckUserDataJob
    Why:
    * The code in PruneCheckUserDataJob is hard to read currently,
      and doing some cleanup should make it easier to read.
    ** Moving the static function code into a separate method should
       also help, but this is done in a later commit to make it
       easier to verify no changes were made when copying the code.
    
    What:
    * Instead of returning the reference IDs from the static function,
      add them directly to the ClientHintsReferenceIds object.
    ** This reduces the nested calls in lines 77 onwards.
    
    Bug: T359560
    Change-Id: Ib8e4ac63757017a4ceec38f11df287f0c9003ca1

--- src/Jobs/PruneCheckUserDataJob.php
+++ src/Jobs/PruneCheckUserDataJob.php
@@ -38,9 +38,11 @@ class PruneCheckUserDataJob extends Job {
 			ConvertibleTimestamp::time() - $services->getMainConfig()->get( 'CUDMaxAge' )
 		);
 
+		$deletedReferenceIds = new ClientHintsReferenceIds();
+
 		$deleteOperation = static function (
 			$table, $idField, $timestampField, $clientHintMapTypeIdentifier
-		) use ( $dbw, $cutoff, $fname ) {
+		) use ( $dbw, $cutoff, $fname, $deletedReferenceIds ) {
 			// Get at most 500 rows to purge from the given $table, selecting the row ID and associated Client Hints
 			// data reference ID
 			$clientHintReferenceField =
@@ -55,12 +57,12 @@ class PruneCheckUserDataJob extends Job {
 				$idQueryBuilder->field( $clientHintReferenceField );
 			}
 			$result = $idQueryBuilder->fetchResultSet();
-			// Group the row IDs and Client Hints data reference IDs into two arrays
+			// Group the row IDs into an array so that we can process them shortly. While doing this, also
+			// add the reference IDs for these rows for purging to the ClientHintsReferenceIds object.
 			$ids = [];
-			$referenceIds = [];
 			foreach ( $result as $row ) {
 				$ids[] = $row->$idField;
-				$referenceIds[] = $row->$clientHintReferenceField;
+				$deletedReferenceIds->addReferenceIds( $row->$clientHintReferenceField, $clientHintMapTypeIdentifier );
 			}
 			// Perform the purging of the rows with IDs in $ids
 			if ( $ids ) {
@@ -70,34 +72,20 @@ class PruneCheckUserDataJob extends Job {
 					->caller( $fname )
 					->execute();
 			}
-			// Return the Client Hints reference IDs for the now deleted rows for purging by
-			// UserAgentClientHintsManager::deleteMappingRows
-			return $referenceIds;
 		};
 
-		$deletedReferenceIds = new ClientHintsReferenceIds();
-
-		$deletedReferenceIds->addReferenceIds(
-			$deleteOperation(
-				'cu_changes', 'cuc_id', 'cuc_timestamp',
-				UserAgentClientHintsManager::IDENTIFIER_CU_CHANGES
-			),
+		$deleteOperation(
+			'cu_changes', 'cuc_id', 'cuc_timestamp',
 			UserAgentClientHintsManager::IDENTIFIER_CU_CHANGES
 		);
 
-		$deletedReferenceIds->addReferenceIds(
-			$deleteOperation(
-				'cu_private_event', 'cupe_id', 'cupe_timestamp',
-				UserAgentClientHintsManager::IDENTIFIER_CU_PRIVATE_EVENT
-			),
+		$deleteOperation(
+			'cu_private_event', 'cupe_id', 'cupe_timestamp',
 			UserAgentClientHintsManager::IDENTIFIER_CU_PRIVATE_EVENT
 		);
 
-		$deletedReferenceIds->addReferenceIds(
-			$deleteOperation(
-				'cu_log_event', 'cule_id', 'cule_timestamp',
-				UserAgentClientHintsManager::IDENTIFIER_CU_LOG_EVENT
-			),
+		$deleteOperation(
+			'cu_log_event', 'cule_id', 'cule_timestamp',
 			UserAgentClientHintsManager::IDENTIFIER_CU_LOG_EVENT
 		);
 
