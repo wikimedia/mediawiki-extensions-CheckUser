e739605ecc90007122c3abe5cd0321b8debb627d
Dreamy Jazz <wpgbrown@wikimedia.org>
2024-08-21 14:28:53 +0100

    PruneCheckUserDataJob: Move static function into a private method
    Why:
    * PruneCheckUserDataJob::run contains a static method that is
      used to actually perform the pruning from a given local
      CheckUser table.
    * Moving this to a private method will make it easier to read
      the code, and will allow this to be moved to a new service
      in a future commit
    ** This later move will be so that the purgeOldData.php maintenance
       script can make use of this method to reduce duplication.
    
    What:
    * Move the code from the static method into a private method
      without modification except in the following ways:
    ** Use __METHOD__ instead of $fname as we can use the private
       method name instead.
    * Document this new private method
    * Use this new private method in ::run.
    
    Bug: T359560
    Change-Id: I8a9bcfdea36b99286319a8b4df8ece2b15eb8264

--- src/Jobs/PruneCheckUserDataJob.php
+++ src/Jobs/PruneCheckUserDataJob.php
@@ -6,6 +6,7 @@ use Job;
 use MediaWiki\CheckUser\ClientHints\ClientHintsReferenceIds;
 use MediaWiki\CheckUser\Services\UserAgentClientHintsManager;
 use MediaWiki\MediaWikiServices;
+use Wikimedia\Rdbms\IDatabase;
 use Wikimedia\Rdbms\ILoadBalancer;
 use Wikimedia\Timestamp\ConvertibleTimestamp;
 
@@ -40,53 +41,19 @@ class PruneCheckUserDataJob extends Job {
 
 		$deletedReferenceIds = new ClientHintsReferenceIds();
 
-		$deleteOperation = static function (
-			$table, $idField, $timestampField, $clientHintMapTypeIdentifier
-		) use ( $dbw, $cutoff, $fname, $deletedReferenceIds ) {
-			// Get at most 500 rows to purge from the given $table, selecting the row ID and associated Client Hints
-			// data reference ID
-			$clientHintReferenceField =
-				UserAgentClientHintsManager::IDENTIFIER_TO_COLUMN_NAME_MAP[$clientHintMapTypeIdentifier];
-			$idQueryBuilder = $dbw->newSelectQueryBuilder()
-				->field( $idField )
-				->table( $table )
-				->conds( $dbw->expr( $timestampField, '<', $cutoff ) )
-				->limit( 500 )
-				->caller( $fname );
-			if ( $clientHintReferenceField !== $idField ) {
-				$idQueryBuilder->field( $clientHintReferenceField );
-			}
-			$result = $idQueryBuilder->fetchResultSet();
-			// Group the row IDs into an array so that we can process them shortly. While doing this, also
-			// add the reference IDs for these rows for purging to the ClientHintsReferenceIds object.
-			$ids = [];
-			foreach ( $result as $row ) {
-				$ids[] = $row->$idField;
-				$deletedReferenceIds->addReferenceIds( $row->$clientHintReferenceField, $clientHintMapTypeIdentifier );
-			}
-			// Perform the purging of the rows with IDs in $ids
-			if ( $ids ) {
-				$dbw->newDeleteQueryBuilder()
-					->table( $table )
-					->where( [ $idField => $ids ] )
-					->caller( $fname )
-					->execute();
-			}
-		};
-
-		$deleteOperation(
-			'cu_changes', 'cuc_id', 'cuc_timestamp',
-			UserAgentClientHintsManager::IDENTIFIER_CU_CHANGES
+		$this->purgeDataFromLocalTable(
+			$dbw, 'cu_changes', 'cuc_id', 'cuc_timestamp', $cutoff,
+			UserAgentClientHintsManager::IDENTIFIER_CU_CHANGES, $deletedReferenceIds
 		);
 
-		$deleteOperation(
-			'cu_private_event', 'cupe_id', 'cupe_timestamp',
-			UserAgentClientHintsManager::IDENTIFIER_CU_PRIVATE_EVENT
+		$this->purgeDataFromLocalTable(
+			$dbw, 'cu_private_event', 'cupe_id', 'cupe_timestamp', $cutoff,
+			UserAgentClientHintsManager::IDENTIFIER_CU_PRIVATE_EVENT, $deletedReferenceIds
 		);
 
-		$deleteOperation(
-			'cu_log_event', 'cule_id', 'cule_timestamp',
-			UserAgentClientHintsManager::IDENTIFIER_CU_LOG_EVENT
+		$this->purgeDataFromLocalTable(
+			$dbw, 'cu_log_event', 'cule_id', 'cule_timestamp', $cutoff,
+			UserAgentClientHintsManager::IDENTIFIER_CU_LOG_EVENT, $deletedReferenceIds
 		);
 
 		/** @var UserAgentClientHintsManager $userAgentClientHintsManager */
@@ -94,5 +61,55 @@ class PruneCheckUserDataJob extends Job {
 		$userAgentClientHintsManager->deleteMappingRows( $deletedReferenceIds );
 
 		return true;
+	}
+
+	/**
+	 * Purge 500 rows from the given local CheckUser result table.
+	 *
+	 * @param IDatabase $dbw A primary database connection for the database we are purging rows from
+	 * @param string $table The relevant CheckUser result table (e.g. cu_changes)
+	 * @param string $idField The ID field for the $table (e.g. cuc_id)
+	 * @param string $timestampField The timestamp field for the $table (e.g. cuc_timestamp)
+	 * @param string $cutoff The timestamp used as a "cutoff", where rows which have a timestamp before the given
+	 *   cutoff are eligible to be purged from the database
+	 * @param int $clientHintMapTypeIdentifier The UserAgentClientHintsManager::IDENTIFIER_* constant for the given
+	 *   $table
+	 * @param ClientHintsReferenceIds $deletedReferenceIds A {@link ClientHintsReferenceIds} instance used to collect
+	 *   the reference IDs associated with rows that were purged by the call to this method. The caller is
+	 *   responsible for purging the Client Hints data for these reference IDs.
+	 */
+	private function purgeDataFromLocalTable(
+		IDatabase $dbw, string $table, string $idField, string $timestampField, string $cutoff,
+		int $clientHintMapTypeIdentifier, ClientHintsReferenceIds $deletedReferenceIds
+	) {
+		// Get at most 500 rows to purge from the given $table, selecting the row ID and associated Client Hints
+		// data reference ID
+		$clientHintReferenceField =
+			UserAgentClientHintsManager::IDENTIFIER_TO_COLUMN_NAME_MAP[$clientHintMapTypeIdentifier];
+		$idQueryBuilder = $dbw->newSelectQueryBuilder()
+			->field( $idField )
+			->table( $table )
+			->conds( $dbw->expr( $timestampField, '<', $cutoff ) )
+			->limit( 500 )
+			->caller( __METHOD__ );
+		if ( $clientHintReferenceField !== $idField ) {
+			$idQueryBuilder->field( $clientHintReferenceField );
+		}
+		$result = $idQueryBuilder->fetchResultSet();
+		// Group the row IDs into an array so that we can process them shortly. While doing this, also
+		// add the reference IDs for these rows for purging to the ClientHintsReferenceIds object.
+		$ids = [];
+		foreach ( $result as $row ) {
+			$ids[] = $row->$idField;
+			$deletedReferenceIds->addReferenceIds( $row->$clientHintReferenceField, $clientHintMapTypeIdentifier );
+		}
+		// Perform the purging of the rows with IDs in $ids
+		if ( $ids ) {
+			$dbw->newDeleteQueryBuilder()
+				->table( $table )
+				->where( [ $idField => $ids ] )
+				->caller( __METHOD__ )
+				->execute();
+		}
 	}
 }
