4f60d46706a38075e70c949af5966b47e55490ec
Kosta Harlan <kharlan@wikimedia.org>
2024-10-28 17:01:08 +0100

    GlobalContributionsPager: Use Special:PermanentLink to construct link
    Why:
    
    - We need to redirect to the correct page which can vary based on
      namespace. We do not yet have a way to fetch namespace info across
      wikis.
    
    What:
    
    - As a temporary measure, construct the link to the contribution using
      Special:PermanentLink on the local wiki, which will handle redirecting
      to the correct namespace
    
    Bug: T378155
    Change-Id: Ibd0db039a31db6ed3c7ba6b185f31a05db3cb728

--- src/GlobalContributions/GlobalContributionsPager.php
+++ src/GlobalContributions/GlobalContributionsPager.php
@@ -308,7 +308,7 @@ class GlobalContributionsPager extends ContributionsPager implements CheckUserQu
 		$link = $this->getLinkRenderer()->makeExternalLink(
 			$this->getForeignURL(
 				$row->sourcewiki,
-				$row->{$this->pageTitleField}
+				'Special:PermanentLink/' . $row->rev_id
 			),
 			// The page is only used for its title and namespace,
 			// so this is safe.
