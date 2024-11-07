3e58ff3c23fdde78097bf8843589d6c9243fa8fb
Dreamy Jazz <wpgbrown@wikimedia.org>
2024-11-07 13:13:07 +0000

    e2e test that Client Hints data is collected for edits
    Why:
    * Client Hints data is collected for edits using a JS API that
      collects the edit and sends it to an API.
    * However, the JS module is hard to test using qunit because it
      requires mocking the call to get the Client Hints data and also
      involves mocking the response of API calls.
    * In this case, it makes sense to create a e2e test that checks
      the Client Hints data sent for a test edit appears in
      Special:CheckUser for display.
    
    What:
    * Create specs/clienthints.js which tests that the Client Hints
      data sent for a test edit appears in Special:CheckUser.
    ** This does not check the values of the Client Hints data, just
       that data has been sent and is displayed. Doing this makes the
       usually long to run e2e tests as simple as possible while still
       testing the code effectively.
    * Create pageobjects/edit.page.js which is a copy of the
      edit.page.js from MediaWiki core, but with the ability to define
      an edit summary.
    
    Bug: T379252
    Change-Id: I14d9602ac8e69d6afc248cce31bca4280a38d924

---
+++ tests/selenium/specs/clienthints.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const assert = require( 'assert' ),
+	LoginAsCheckUser = require( '../checkuserlogin' ),
+	CheckUserPage = require( '../pageobjects/checkuser.page' ),
+	EditPage = require( '../pageobjects/edit.page' );
+
+const supportedBrowsers = [ 'chrome', 'chromium', 'msedge' ];
+
+function checkIfBrowserSupportsClientHints( browserName ) {
+	return supportedBrowsers.includes( browserName );
+}
+
+describe( 'Client Hints', () => {
+	before( async () => {
+		await LoginAsCheckUser.loginAsCheckUser();
+		// Skip the tests if we are using a browser which does not support client hints, because the tests
+		// need data to be sent to the API to e2e test.
+		if ( !checkIfBrowserSupportsClientHints( driver.requestedCapabilities.browserName ) ) {
+			return;
+		}
+		await EditPage.edit( 'Testing', 'testing1234', 'test-edit-to-test-client-hints' );
+	} );
+	it( 'Verify edit sends Client Hints data', async () => {
+		// Skip the tests if we are using a browser which does not support client hints, because the tests
+		// need data to be sent to the API to e2e test.
+		if ( !checkIfBrowserSupportsClientHints( driver.requestedCapabilities.browserName ) ) {
+			return;
+		}
+		await CheckUserPage.open();
+		await CheckUserPage.getActionsCheckTypeRadio.click();
+		await CheckUserPage.checkTarget.setValue( process.env.MEDIAWIKI_USER );
+		await CheckUserPage.checkReasonInput.setValue( 'Selenium browser testing' );
+		await CheckUserPage.submit.click();
+		browser.waitUntil( () => {
+			browser.execute( () => browser.document.readyState === 'complete' );
+		}, { timeout: 10 * 1000, timeoutMsg: 'Page failed to load in a reasonable time.' } );
+		assert( await CheckUserPage.getActionsResults.isExisting() );
+		// Check that Client Hints data exists for the edit, by checking if the Client Hints element class is present
+		// for the edit.
+		const $relevantResultLine = $( 'li:contains(test-edit-to-test-client-hints)', CheckUserPage.getActionsResults );
+		assert( $relevantResultLine.find( 'mw-checkuser-client-hints' ).length !== 0 );
+	} );
+} );
