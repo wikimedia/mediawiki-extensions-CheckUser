864e694fca44caa044d8f1951b0ee8aa1e36ee7d
Héctor Arroyo <hector.arroyo@wikimedia.org>
2025-02-17 17:10:23 +0100

    preferences: Make sure IP reveal is contained in the modified options
    Why:
    - When resetting all options via Special:Preferences/reset, the
      controller calls UserOptionsManager::resetAllOptions(), which in turn
      calls OptionsManager::setOption() passing NULL as the new value for
      every option the used has previously enabled.
    - Calls to setOption() passing NULL as the option value makes it try to
      get a default value to use by calling DefaultOptionsLookup, but
      ultimately falls back to NULL if a default value is not found.
    - Extensions may provide default values for options in extension.json by
      placing them in the DefaultUserOptions section, but CheckUser does not
      provide a default there.
    
    What:
    - As a safeguard measure, make the Preferences hook check if the IP
      reveal option is being disabled by checking its presence using
      array_key_exists() instead of isset(), since the later returns false
      also when the option is present in $modifiedOptions but has a NULL
      value.
    - Provide a default value for the option in extension.json.
    
    Bug: T382010
    Change-Id: Iaf0f959e05dc8d4e0e5492fbc1a2f6f83aa068da

--- tests/phpunit/integration/HookHandler/PreferencesTest.php
+++ tests/phpunit/integration/HookHandler/PreferencesTest.php
@@ -3,26 +3,56 @@
 namespace MediaWiki\CheckUser\Tests\Integration\HookHandler;
 
 use MediaWiki\CheckUser\HookHandler\Preferences;
+use MediaWiki\CheckUser\Logging\TemporaryAccountLogger;
 use MediaWiki\CheckUser\Logging\TemporaryAccountLoggerFactory;
 use MediaWiki\Context\RequestContext;
 use MediaWiki\Permissions\PermissionManager;
 use MediaWiki\User\User;
+use MediaWikiIntegrationTestCase;
+use PHPUnit\Framework\MockObject\MockObject;
 
 /**
  * @group CheckUser
  * @covers \MediaWiki\CheckUser\HookHandler\Preferences
  */
-class PreferencesTest extends \MediaWikiIntegrationTestCase {
+class PreferencesTest extends MediaWikiIntegrationTestCase {
+	/** @var (PermissionManager&MockObject) */
+	private PermissionManager $permissionManager;
+
+	/** @var (TemporaryAccountLoggerFactory&MockObject) */
+	private TemporaryAccountLoggerFactory $loggerFactory;
+
+	/** @var (User&MockObject) */
+	private User $user;
+
+	/** @var (Preferences&MockObject) */
+	private Preferences $sut;
+
+	public function setUp(): void {
+		parent::setUp();
+
+		$this->user = $this->createMock( User::class );
+		$this->permissionManager = $this->createMock(
+			PermissionManager::class
+		);
+		$this->loggerFactory = $this->createMock(
+			TemporaryAccountLoggerFactory::class
+		);
+
+		$this->sut = new Preferences(
+			$this->permissionManager,
+			$this->loggerFactory,
+			$this->getServiceContainer()->getMainConfig()
+		);
+	}
 
 	/**
 	 * @dataProvider provideOnGetPreferencesTemporaryAccount
 	 */
 	public function testOnGetPreferencesTemporaryAccount( $options ) {
-		$user = $this->createMock( User::class );
 		$prefs = [];
 
-		$permissionManager = $this->createMock( PermissionManager::class );
-		$permissionManager->method( 'userHasRight' )
+		$this->permissionManager->method( 'userHasRight' )
 			->willReturnCallback( static function ( $user, $right ) use ( $options ) {
 				if ( $right === 'checkuser-temporary-account' ) {
 					return $options['hasRight'];
@@ -36,13 +66,7 @@ class PreferencesTest extends \MediaWikiIntegrationTestCase {
 				return true;
 			} );
 
-		$loggerFactory = $this->createMock( TemporaryAccountLoggerFactory::class );
-
-		( new Preferences(
-			$permissionManager,
-			$loggerFactory,
-			$this->getServiceContainer()->getMainConfig()
-		) )->onGetPreferences( $user, $prefs );
+		$this->sut->onGetPreferences( $this->user, $prefs );
 
 		// Always expect that the 'temporary accounts onboarding dialog seen' preference is added and that
 		// it is a hidden preference.
@@ -91,23 +115,15 @@ class PreferencesTest extends \MediaWikiIntegrationTestCase {
 	/** @dataProvider provideOnGetPreferencesForCheckUserRight */
 	public function testGetOnPreferencesForCheckUserRight( $siteConfigValue, $expectedSiteConfigValue ) {
 		$this->overrideConfigValue( 'CheckUserCollapseCheckUserHelperByDefault', $siteConfigValue );
-		$user = $this->createMock( User::class );
 		$prefs = [];
 
-		$permissionManager = $this->createMock( PermissionManager::class );
-		$permissionManager->method( 'userHasRight' )
+		$this->permissionManager->method( 'userHasRight' )
 			->willReturnCallback( static function ( $user, $right ) {
 				return $right === 'checkuser';
 			} );
 
-		$loggerFactory = $this->createMock( TemporaryAccountLoggerFactory::class );
-
 		$this->setUserLang( 'qqx' );
-		( new Preferences(
-			$permissionManager,
-			$loggerFactory,
-			$this->getServiceContainer()->getMainConfig()
-		) )->onGetPreferences( $user, $prefs );
+		$this->sut->onGetPreferences( $this->user, $prefs );
 
 		$this->assertArrayHasKey( 'checkuser-helper-table-collapse-by-default', $prefs );
 		$actualOptions = $prefs['checkuser-helper-table-collapse-by-default']['options'];
@@ -145,6 +161,225 @@ class PreferencesTest extends \MediaWikiIntegrationTestCase {
 			'Site config set to false' => [ false, '(checkuser-helper-table-collapse-by-default-preference-never)' ],
 			'Site config set to true' => [ true, '(checkuser-helper-table-collapse-by-default-preference-always)' ],
 			'Site config set to 200' => [ 200, '200' ],
+		];
+	}
+
+	/**
+	 * @dataProvider onSaveUserOptionsDataProvider
+	 */
+	public function testOnSaveUserOptions(
+		array $modifiedOptions,
+		array $originalOptions,
+		bool $logAccessEnabled,
+		bool $logAccessDisabled
+	): void {
+		$logger = $this->createMock(
+			TemporaryAccountLogger::class
+		);
+
+		$this->loggerFactory
+			->method( 'getLogger' )
+			->willReturn( $logger );
+
+		if ( $logAccessEnabled || $logAccessDisabled ) {
+			$logger
+				->expects( $this->once() )
+				->method(
+					$logAccessEnabled ?
+						'logAccessEnabled' :
+						'logAccessDisabled'
+				)
+				->with( $this->user );
+			$logger
+				->expects( $this->never() )
+				->method(
+					$logAccessEnabled ?
+						'logAccessDisabled' :
+						'logAccessEnabled'
+				);
+		} else {
+			$logger
+				->expects( $this->never() )
+				->method( 'logAccessEnabled' );
+			$logger
+				->expects( $this->never() )
+				->method( 'logAccessDisabled' );
+		}
+
+		$formerOptions = $modifiedOptions;
+
+		$this->sut->onSaveUserOptions(
+			$this->user,
+			$modifiedOptions,
+			$originalOptions
+		);
+
+		// Assert $modifiedOptions (passed by reference) is kept intact
+		$this->assertEquals( $formerOptions, $modifiedOptions );
+	}
+
+	public function onSaveUserOptionsDataProvider(): array {
+		return [
+			'When the option is kept enabled' => [
+				'modifiedOptions' => [
+					Preferences::ENABLE_IP_REVEAL => 1,
+				],
+				'originalOptions' => [
+					Preferences::ENABLE_IP_REVEAL => 1,
+				],
+				'logAccessEnabled' => false,
+				'logAccessDisabled' => false
+			],
+			'When the option is kept disabled' => [
+				'modifiedOptions' => [
+					Preferences::ENABLE_IP_REVEAL => 0,
+				],
+				'originalOptions' => [
+					Preferences::ENABLE_IP_REVEAL => 0,
+				],
+				'logAccessEnabled' => false,
+				'logAccessDisabled' => false
+			],
+			'When the option is not provided' => [
+				'modifiedOptions' => [
+					'anotheroption' => 0,
+				],
+				'originalOptions' => [
+					'anotheroption' => 0,
+				],
+				'logAccessEnabled' => false,
+				'logAccessDisabled' => false
+			],
+			'When the option is kept enabled while other is changed' => [
+				'modifiedOptions' => [
+					Preferences::ENABLE_IP_REVEAL => 1,
+					'anotheroption' => 0,
+				],
+				'originalOptions' => [
+					Preferences::ENABLE_IP_REVEAL => 1,
+					'anotheroption' => 1,
+				],
+				'logAccessEnabled' => false,
+				'logAccessDisabled' => false
+			],
+			'When the option is kept disabled while other is changed' => [
+				'modifiedOptions' => [
+					Preferences::ENABLE_IP_REVEAL => 0,
+					'anotheroption' => 0,
+				],
+				'originalOptions' => [
+					Preferences::ENABLE_IP_REVEAL => 0,
+					'anotheroption' => 1,
+				],
+				'logAccessEnabled' => false,
+				'logAccessDisabled' => false
+			],
+			'When the option is switched to enabled, single change' => [
+				'modifiedOptions' => [
+					Preferences::ENABLE_IP_REVEAL => 1,
+				],
+				'originalOptions' => [
+					Preferences::ENABLE_IP_REVEAL => 0,
+				],
+				'logAccessEnabled' => true,
+				'logAccessDisabled' => false
+			],
+			'When the option is switched to disabled, single change' => [
+				'modifiedOptions' => [
+					Preferences::ENABLE_IP_REVEAL => 0,
+				],
+				'originalOptions' => [
+					Preferences::ENABLE_IP_REVEAL => 1,
+				],
+				'logAccessEnabled' => false,
+				'logAccessDisabled' => true
+			],
+			'When the option is switched to enabled, multiple changes' => [
+				'modifiedOptions' => [
+					Preferences::ENABLE_IP_REVEAL => 1,
+					'anotheroption' => 0,
+				],
+				'originalOptions' => [
+					Preferences::ENABLE_IP_REVEAL => 0,
+					'anotheroption' => 1,
+				],
+				'logAccessEnabled' => true,
+				'logAccessDisabled' => false
+			],
+			'When the option is switched to disabled, multiple changes' => [
+				'modifiedOptions' => [
+					Preferences::ENABLE_IP_REVEAL => 0,
+					'anotheroption' => 0,
+				],
+				'originalOptions' => [
+					Preferences::ENABLE_IP_REVEAL => 1,
+					'anotheroption' => 1,
+				],
+				'logAccessEnabled' => false,
+				'logAccessDisabled' => true
+			],
+			'When the new value is NULL and the option was previously set' => [
+				// T382010
+				'modifiedOptions' => [
+					Preferences::ENABLE_IP_REVEAL => null,
+				],
+				'originalOptions' => [
+					Preferences::ENABLE_IP_REVEAL => 1,
+				],
+				'logAccessEnabled' => false,
+				'logAccessDisabled' => true
+			],
+			'When the new value is NULL and the option was previously unset' => [
+				// T382010
+				'modifiedOptions' => [
+					Preferences::ENABLE_IP_REVEAL => null,
+				],
+				'originalOptions' => [
+					Preferences::ENABLE_IP_REVEAL => 0,
+				],
+				'logAccessEnabled' => false,
+				'logAccessDisabled' => false
+			],
+			'When the new value is false and the option was previously set' => [
+				'modifiedOptions' => [
+					Preferences::ENABLE_IP_REVEAL => false,
+				],
+				'originalOptions' => [
+					Preferences::ENABLE_IP_REVEAL => 1,
+				],
+				'logAccessEnabled' => false,
+				'logAccessDisabled' => true
+			],
+			'When the new value is false and the option was previously unset' => [
+				'modifiedOptions' => [
+					Preferences::ENABLE_IP_REVEAL => false,
+				],
+				'originalOptions' => [
+					Preferences::ENABLE_IP_REVEAL => 0,
+				],
+				'logAccessEnabled' => false,
+				'logAccessDisabled' => false
+			],
+			'When the new value is true and the option was previously set' => [
+				'modifiedOptions' => [
+					Preferences::ENABLE_IP_REVEAL => true,
+				],
+				'originalOptions' => [
+					Preferences::ENABLE_IP_REVEAL => 1,
+				],
+				'logAccessEnabled' => false,
+				'logAccessDisabled' => false
+			],
+			'When the new value is true and the option was previously unset' => [
+				'modifiedOptions' => [
+					Preferences::ENABLE_IP_REVEAL => true,
+				],
+				'originalOptions' => [
+					Preferences::ENABLE_IP_REVEAL => 0,
+				],
+				'logAccessEnabled' => true,
+				'logAccessDisabled' => false
+			]
 		];
 	}
 }
