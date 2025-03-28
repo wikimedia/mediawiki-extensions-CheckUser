825517a34566293e1c45cbd477ebe71f6355dc89
Thalia <thalia.e.chan@googlemail.com>
2025-03-28 18:45:01 +0000

    SpecialContributions.js: Move logic into ipReveal.js
    Why:
    
    * SpecialContributions.js is an initialization script for IP reveal
      that runs on some contributions pages. For IP auto-reveal it is
      helpful to be able to re-use this logic when auto-reveal mode is
      switched on and off.
    
    What:
    
    * Move the logic in the initialization script to ipReveal.js, where
      it can be re-used.
    
    Bug: T386498
    Change-Id: I7733277c5796f5fceb90735a3913e7d311adc568

--- modules/ext.checkUser.tempAccounts/SpecialContributions.js
+++ modules/ext.checkUser.tempAccounts/SpecialContributions.js
@@ -1,15 +1,8 @@
-const ipRevealUtils = require( './ipRevealUtils.js' );
 const ipReveal = require( './ipReveal.js' );
 
 /**
  * Add IP reveal functionality to contributions pages that show contributions made by a single
- * temporary user. There are no user links on these pages.
- *
- * This is similar to initOnLoad except with the following customizations:
- * - Since there are no user links, add the buttons for revealing IPs to a specified place
- *   within each revision line.
- * - Use simpler, customized logic for enabling multi-reveal and automatically revealing users,
- *   since the page does not have multiple users, and all buttons are related to revisions.
+ * temporary user. See ipReveal#enableIpRevealForContributionsPage for details.
  *
  * @param {string|*} documentRoot A Document or selector to use as the root of the
  *   search for elements
@@ -18,55 +11,5 @@ const ipReveal = require( './ipReveal.js' );
  *   as they have different guaranteed existing elements.
  */
 module.exports = function ( documentRoot, pageTitle ) {
-	if ( !documentRoot ) {
-		documentRoot = document;
-	}
-
-	// Define the class name of the element that the "Show IP" button should be appended after.
-	// This can't point to the element yet as it'll be the child of a container revision line.
-	let revAppendAfter;
-	if ( pageTitle === 'Contributions' ) {
-		revAppendAfter = '.mw-diff-bytes';
-	} else if ( pageTitle === 'DeletedContributions' ) {
-		revAppendAfter = '.mw-deletedcontribs-tools';
-	}
-
-	const target = mw.config.get( 'wgRelevantUserName' );
-	const revIds = [];
-
-	const $userLinks = $( '#bodyContent', documentRoot ).find( '.mw-contributions-list [data-mw-revid]' );
-	$userLinks.each( function () {
-		const revId = ipReveal.getRevisionId( $( this ) );
-		revIds.push( revId );
-	} );
-	$userLinks.each( function () {
-		const revId = ipReveal.getRevisionId( $( this ) );
-		$( this ).find( revAppendAfter ).after( () => {
-			const ids = {
-				targetId: revId,
-				allIds: revIds
-			};
-			return [
-				' ',
-				$( '<span>' ).addClass( 'mw-changeslist-separator' ),
-				ipReveal.makeButton( target, ids, undefined, documentRoot )
-			];
-		} );
-	} );
-
-	$( documentRoot ).on( 'userRevealed', ( _e, userLookup, ips ) => {
-		$( '.ext-checkuser-tempaccount-reveal-ip-button' ).each( function () {
-			const id = $( this ).closest( '[data-mw-revid]' ).data( 'mw-revid' );
-			const ip = ( ips && ips[ id ] ) ? ips[ id ] : false;
-			ipReveal.replaceButton( $( this ), ip, true );
-		} );
-	} );
-
-	// If the user has been revealed lately, trigger a lookup from the first button
-	if (
-		ipRevealUtils.getAutoRevealStatus() ||
-		ipRevealUtils.getRevealedStatus( mw.config.get( 'wgRelevantUserName' ) )
-	) {
-		$( '.ext-checkuser-tempaccount-reveal-ip-button' ).first().trigger( 'revealIp' );
-	}
+	ipReveal.enableIpRevealForContributionsPage( documentRoot, pageTitle );
 };
--- modules/ext.checkUser.tempAccounts/ipReveal.js
+++ modules/ext.checkUser.tempAccounts/ipReveal.js
@@ -459,6 +459,76 @@ function getLogId( $element ) {
 	return id;
 }
 
+/**
+ * Add IP reveal functionality to contributions pages that show contributions made by a single
+ * temporary user. There are no user links on these pages.
+ *
+ * This is similar to initOnLoad except with the following customizations:
+ * - Since there are no user links, add the buttons for revealing IPs to a specified place
+ *   within each revision line.
+ * - Use simpler, customized logic for enabling multi-reveal and automatically revealing users,
+ *   since the page does not have multiple users, and all buttons are related to revisions.
+ *
+ * @param {string|*} documentRoot A Document or selector to use as the root of the
+ *   search for elements
+ * @param {string} pageTitle Declare what page this is being run on.
+ *   This is for compatibility across Special:Contributions and Special:DeletedContributions,
+ *   as they have different guaranteed existing elements.
+ */
+function enableIpRevealForContributionsPage( documentRoot, pageTitle ) {
+	if ( !documentRoot ) {
+		documentRoot = document;
+	}
+
+	// Define the class name of the element that the "Show IP" button should be appended after.
+	// This can't point to the element yet as it'll be the child of a container revision line.
+	let revAppendAfter;
+	if ( pageTitle === 'Contributions' ) {
+		revAppendAfter = '.mw-diff-bytes';
+	} else if ( pageTitle === 'DeletedContributions' ) {
+		revAppendAfter = '.mw-deletedcontribs-tools';
+	}
+
+	const target = mw.config.get( 'wgRelevantUserName' );
+	const revIds = [];
+
+	const $userLinks = $( '#bodyContent', documentRoot ).find( '.mw-contributions-list [data-mw-revid]' );
+	$userLinks.each( function () {
+		const revId = getRevisionId( $( this ) );
+		revIds.push( revId );
+	} );
+	$userLinks.each( function () {
+		const revId = getRevisionId( $( this ) );
+		$( this ).find( revAppendAfter ).after( () => {
+			const ids = {
+				targetId: revId,
+				allIds: revIds
+			};
+			return [
+				' ',
+				$( '<span>' ).addClass( 'mw-changeslist-separator' ),
+				makeButton( target, ids, undefined, documentRoot )
+			];
+		} );
+	} );
+
+	$( documentRoot ).on( 'userRevealed', ( _e, _userLookup, ips ) => {
+		$( '.ext-checkuser-tempaccount-reveal-ip-button' ).each( function () {
+			const id = $( this ).closest( '[data-mw-revid]' ).data( 'mw-revid' );
+			const ip = ( ips && ips[ id ] ) ? ips[ id ] : false;
+			replaceButton( $( this ), ip, true );
+		} );
+	} );
+
+	// If the user has been revealed lately, trigger a lookup from the first button
+	if (
+		ipRevealUtils.getAutoRevealStatus() ||
+		ipRevealUtils.getRevealedStatus( mw.config.get( 'wgRelevantUserName' ) )
+	) {
+		$( '.ext-checkuser-tempaccount-reveal-ip-button' ).first().trigger( 'revealIp' );
+	}
+}
+
 module.exports = {
 	makeButton: makeButton,
 	addIpRevealButtons: addIpRevealButtons,
@@ -467,5 +537,6 @@ module.exports = {
 	automaticallyRevealUsers: automaticallyRevealUsers,
 	batchRevealIps: batchRevealIps,
 	getRevisionId: getRevisionId,
-	getLogId: getLogId
+	getLogId: getLogId,
+	enableIpRevealForContributionsPage: enableIpRevealForContributionsPage
 };
