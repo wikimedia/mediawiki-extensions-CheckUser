414d4965b5fe9440cc2d6b3097f2ba623a66c258
mimurawil <mimurawil@gmail.com>
2025-05-06 12:23:17 -0700

    Add initial popover Vue component
    Why:
    - Adding this initial popover component will allow us to parallelize
    development of the remaining components inside the user info card
    
    What:
    - Add initial popover Vue component using Codex components and icons
    
    Bug: T393417
    Change-Id: I1ab54c9954cff7eda610ab0b033d7f7c4304350b

--- modules/ext.checkUser.userInfoCard/components/App.vue
+++ modules/ext.checkUser.userInfoCard/components/App.vue
@@ -1,16 +1,34 @@
 <template>
-	<span></span>
+	<cdx-popover
+		v-model:open="store.isOpen"
+		:anchor="store.currentTrigger"
+		placement="bottom-start"
+		:render-in-place="true"
+		:use-close-button="true"
+	>
+		User ID: {{ store.userCard.userId }}
+	</cdx-popover>
 </template>
 
 <script>
+const { CdxPopover } = require( '@wikimedia/codex' );
+const useUserInfoCardPopoverStore = require( '../stores/UserInfoCardPopover.js' );
 
 // @vue/component
 module.exports = exports = {
 	name: 'App',
-	compilerOptions: {
-		whitespace: 'condense'
+	components: {
+		CdxPopover
 	},
-	components: {},
-	setup() {}
+	setup() {
+		const store = useUserInfoCardPopoverStore();
+
+		return {
+			store
+		};
+	},
+	expose: [
+		'store'
+	]
 };
 </script>
--- modules/ext.checkUser.userInfoCard/init.js
+++ modules/ext.checkUser.userInfoCard/init.js
@@ -1,6 +1,35 @@
 'use strict';
 
 $( () => {
-	// TODO: Bind to clicks to ext-checkuser-userinfocard-button
-	// and pass the user ID from ext-checkuser-userinfocard-id-$wikiId:$userId to the App
+	const Vue = require( 'vue' );
+	const App = require( './components/App.vue' );
+	const Pinia = require( 'pinia' );
+	const pinia = Pinia.createPinia();
+
+	const popover = document.createElement( 'div' );
+	popover.id = 'ext-checkuser-userinfocard-popover';
+	popover.classList.add( 'ext-checkuser-userinfocard-popover' );
+	document.body.appendChild( popover );
+	const popoverApp = Vue.createApp( App ).use( pinia ).mount( popover );
+
+	const buttons = document.querySelectorAll( '.ext-checkuser-userinfocard-button' );
+	buttons.forEach( ( button ) => {
+		$( button ).on( 'click', ( event ) => {
+			event.preventDefault();
+
+			// fetch wikiId and userId from the classname ext-checkuser-userinfocard-id-$wikiId:$userId
+			const idClass = Array.from( button.classList ).find(
+				( cls ) => cls.startsWith( 'ext-checkuser-userinfocard-id-' )
+			);
+			if ( idClass ) {
+				const [ wikiId, userId ] = idClass.replace(
+					'ext-checkuser-userinfocard-id-', ''
+				).split( ':' );
+				if ( wikiId && userId ) {
+					popoverApp.store.setUserCard( { userId, wikiId } );
+					popoverApp.store.open( event.target );
+				}
+			}
+		} );
+	} );
 } );
