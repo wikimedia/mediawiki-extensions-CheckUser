caafe155b142e2626198ccb784f5d547ec246661
mimurawil <mimurawil@gmail.com>
2025-05-12 17:07:31 -0700

    Add menu button and items to the user info card header
    This implements menu button and items from Codex components.
    
    Bug: T393804
    Change-Id: I820bbac5c91d00d8eeec658c30b68e54b1e65b6a

---
+++ modules/ext.checkUser.userInfoCard/components/UserCardMenu.vue
@@ -0,0 +1,85 @@
+<template>
+	<cdx-menu-button
+		v-model:selected="selection"
+		:aria-label="ariaLabel"
+		:menu-items="menuItems"
+		:class="`ext-checkuser-userinfocard-menu-${ userId }`"
+		@update:selected="onMenuSelect"
+	>
+		<cdx-icon :icon="cdxIconEllipsis"></cdx-icon>
+	</cdx-menu-button>
+</template>
+
+<script>
+const { ref } = require( 'vue' );
+const { CdxMenuButton, CdxIcon } = require( '@wikimedia/codex' );
+const { cdxIconEllipsis } = require( './icons.json' );
+
+// @vue/component
+module.exports = exports = {
+	name: 'UserCardMenu',
+	components: { CdxMenuButton, CdxIcon },
+	props: {
+		userId: {
+			type: Number,
+			required: true
+		},
+		ariaLabel: {
+			type: String,
+			default: () => mw.msg( 'checkuser-userinfocard-open-menu-aria-label' )
+		}
+	},
+	setup() {
+		const selection = ref( null );
+		const menuItems = [
+			// TODO: T393946 Add proper links
+			{
+				label: mw.msg( 'checkuser-userinfocard-menu-view-contributions' ),
+				value: 'view-contributions',
+				link: '#'
+			},
+			{
+				label: mw.msg( 'checkuser-userinfocard-menu-view-global-account' ),
+				value: 'view-global-account',
+				link: '#'
+			},
+			// TODO: T393981 Implement proper add/remove user page to watchlist
+			{
+				label: mw.msg( 'checkuser-userinfocard-menu-add-to-watchlist' ),
+				value: 'add-watchlist',
+				link: '#'
+			},
+			{
+				label: mw.msg( 'checkuser-userinfocard-menu-check-ip' ),
+				value: 'check-ip',
+				link: '#'
+			},
+			{
+				label: mw.msg( 'checkuser-userinfocard-menu-block-user' ),
+				value: 'block-user',
+				link: '#'
+			},
+			{
+				label: mw.msg( 'checkuser-userinfocard-menu-turn-off' ),
+				value: 'turn-off',
+				link: mw.util.getUrl( 'Special:Preferences' ) + '#mw-prefsection-rendering'
+			}
+		];
+
+		function onMenuSelect( value ) {
+			const selectedItem = menuItems.find( ( item ) => item.value === value );
+			if ( selectedItem && selectedItem.link ) {
+				window.location.assign( selectedItem.link );
+			}
+			selection.value = null;
+		}
+
+		return {
+			selection,
+			menuItems,
+			onMenuSelect,
+			cdxIconEllipsis
+		};
+	}
+};
+</script>
