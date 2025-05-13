9ac0524ccfc14f31952fa07f6610db4952f7626e
Kosta Harlan <kharlan@wikimedia.org>
2025-05-13 09:51:28 +0200

    UserInfoCard: Add entrypoint for button and scaffolding for Vue app
    Why:
    
    - We need a mechanism to add the user info button in the contributions
      line for usernames
    - It is a better UX to render the button server-side than via
      client-side JS
    
    What:
    
    - Implement the UserLinkRendererUserLink and add the UserInfo button as
      a prefix to the rendered HTML, if a user has the UserInfoCard feature
      enabled
    - Add an init.js file for loading the Vue app and App.vue scaffolding
    
    Depends-On: I0c154865711fdc3266e4e7ddc5dbb7aab0d98f7e
    Bug: T386437
    Bug: T393573
    Change-Id: Ie1b7756d5709b5b2970368f5b8b1a5e98ffd9a14

---
+++ modules/ext.checkUser.userInfoCard/init.js
@@ -0,0 +1,6 @@
+'use strict';
+
+$( () => {
+	// TODO: Bind to clicks to ext-checkuser-userinfocard-button
+	// and pass the user ID from ext-checkuser-userinfocard-id-$wikiId:$userId to the App
+} );
