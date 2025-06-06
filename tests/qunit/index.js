d21a9340c7bc5c74327b567a0e11bc9ebc993809
mimurawil <mimurawil@gmail.com>
2025-06-06 13:35:11 -0700

    Add unit tests for userInfoCard: UserCardHeader
    Why:
    - Improve test coverage for the CheckUser extension
    
    What:
    - Add QUnit tests for UserCardHeader.vue
    
    Bug: T394084
    Change-Id: I83e41c348aaaee01f1a4ce7f3da78afdc6d7e4d8

--- tests/qunit/index.js
+++ tests/qunit/index.js
@@ -20,5 +20,6 @@ require( './ext.checkUser.tempAccounts/rest.test.js' );
 require( './ext.checkUser.tempAccounts/SpecialBlock.test.js' );
 require( './ext.checkUser.tempAccounts/SpecialContributions.test.js' );
 require( './ext.checkUser.tempAccounts/BlockDetailsPopupButtonWidget.test.js' );
+require( './ext.checkUser.userInfoCard/components/UserCardHeader.test.js' );
 require( './ext.checkUser.userInfoCard/components/UserCardMenu.test.js' );
 require( './ext.checkUser.userInfoCard/composables/useWatchList.test.js' );
