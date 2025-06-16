b9f99728a50de2d646dea007f824d6b72839fa8a
Kosta Harlan <kharlan@wikimedia.org>
2025-06-16 08:22:31 +0200

    UserInfoCard: Update link for "Provide feedback"
    Per T395211#10914339
    
    Bug: T395211
    Change-Id: I638ec4303ad832dc6f4a02860cedf3c1aaa611ef

--- modules/ext.checkUser.userInfoCard/components/UserCardMenu.vue
+++ modules/ext.checkUser.userInfoCard/components/UserCardMenu.vue
@@ -98,7 +98,7 @@ module.exports = exports = {
 				{
 					label: mw.msg( 'checkuser-userinfocard-menu-provide-feedback' ),
 					value: 'provide-feedback',
-					link: 'https://www.mediawiki.org/w/index.php?title=Help_talk:Extension:CheckUser'
+					link: 'https://www.mediawiki.org/wiki/Talk:Trust_and_Safety_Product/Anti-abuse_signals'
 				},
 				{
 					label: mw.msg( 'checkuser-userinfocard-menu-turn-off' ),
