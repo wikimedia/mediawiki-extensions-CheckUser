f2876864dc746eb158b436f0d3586baa33077997
Simone Cuomo <simone@thisdot.co>
2025-06-24 08:45:05 +0100

    Display how many actions were undertaken on the sparkline
    - [x] Add Pointer on Chart on hover
    - [x] Add Tooltip on chart on hover
    
    Bug: T397307
    Change-Id: I92305d9d102668cb7ce72a9887fac81990a4c350

--- modules/vue-components/CSparkline.vue
+++ modules/vue-components/CSparkline.vue
@@ -1,19 +1,34 @@
 <template>
-	<svg
-		:id="`sparkline-${ id }`"
-		xmlns="http://www.w3.org/2000/svg"
-		class="ext-checkuser-CSparkline"
-	>
-		<title>
-			{{ title }}
-		</title>
-	</svg>
+	<div class="ext-checkuser-CSparkline-container">
+		<svg
+			:id="`sparkline-${ id }`"
+			xmlns="http://www.w3.org/2000/svg"
+			class="ext-checkuser-CSparkline"
+			@mousemove="handleMouseMove"
+			@mouseleave="handleMouseLeave"
+		>
+			<title>
+				{{ title }}
+			</title>
+		</svg>
+		<div
+			v-if="tooltipData.visible"
+			class="ext-checkuser-CSparkline__tooltip"
+			:style="{
+				left: tooltipData.x + 'px',
+				top: tooltipData.y + 'px'
+			}"
+		>
+			{{ tooltipLabel }}
+		</div>
+	</div>
 </template>
 
 <script>
-const { onMounted } = require( 'vue' );
+const { onMounted, reactive, computed } = require( 'vue' );
 const d3 = require( '../lib/d3/d3.min.js' );
-let chart, sparkline, area = null;
+let chart, sparkline, area, hoverDot = null;
+let xScale, yScale = null;
 
 // @vue/component
 module.exports = exports = {
@@ -45,14 +60,99 @@ module.exports = exports = {
 		}
 	},
 	setup( props ) {
+		const tooltipData = reactive( {
+			visible: false,
+			date: '',
+			edits: 0,
+			x: 0,
+			y: 0
+		} );
+		const tooltipLabel = computed( () => mw.msg(
+			'checkuser-userinfocard-chart-tooltip-label',
+			tooltipData.date,
+			tooltipData.edits
+		) );
+		// Create accessor functions from property names
+		const getXValue = ( d ) => d[ props.xAccessor ];
+		const getYValue = ( d ) => d[ props.yAccessor ];
+
+		/**
+			* Find the closest data point to the given x-value
+			*
+			* @param {Object|null} d0 - Previous data point
+			* @param {Object|null} d1 - Next data point
+			* @param {number} xValue - Target x-value to find closest point to
+			* @param {Function} xValueAccessor - Function to extract x-value from data point
+			* @return {Object|null} The closest data point or null if none available
+			*/
+		const findClosestDataPoint = ( d0, d1, xValue, xValueAccessor ) => {
+			// If only one data point exists, return it
+			if ( !d0 && d1 ) {
+				return d1;
+			}
+			if ( d0 && !d1 ) {
+				return d0;
+			}
+
+			// Calculate distances to both points
+			const distanceToD0 = Math.abs( xValue - xValueAccessor( d0 ) );
+			const distanceToD1 = Math.abs( xValueAccessor( d1 ) - xValue );
+
+			// Return the closest point
+			return distanceToD1 < distanceToD0 ? d1 : d0;
+		};
+
+		const handleMouseMove = ( event ) => {
+			const [ mouseX ] = d3.pointer( event );
+			// Find the closest data point
+			const xValue = xScale.invert( mouseX );
+			const bisector = d3.bisector( getXValue ).left;
+			const index = bisector( props.data, xValue, 1 );
+			const d0 = props.data[ index - 1 ];
+			const d1 = props.data[ index ];
+
+			if ( !d0 && !d1 ) {
+				return;
+			}
+
+			const d = findClosestDataPoint( d0, d1, xValue, getXValue );
+
+			// Position the hover dot
+			const x = xScale( getXValue( d ) );
+			const y = yScale( getYValue( d ) );
+
+			hoverDot
+				.attr( 'cx', x )
+				.attr( 'cy', y )
+				.style( 'display', 'block' );
+
+			// Update tooltip data and position (relative to container)
+			tooltipData.visible = true;
+			tooltipData.date = getXValue( d ).toLocaleDateString();
+			tooltipData.edits = getYValue( d );
+
+			// Convert SVG coordinates to screen coordinates dynamically
+			const svgElement = chart.node();
+			const containerRect = svgElement.getBoundingClientRect();
+
+			// Calculate the actual position relative to the container
+			const svgToScreenX = ( x / props.dimensions.width ) * containerRect.width;
+			const svgToScreenY = ( y / props.dimensions.height ) * containerRect.height;
+
+			// Position tooltip relative to container with proper offsets
+			tooltipData.x = svgToScreenX - 60; // Center the tooltip horizontally
+			tooltipData.y = svgToScreenY - 34; // Position tooltip above the point
+		};
+
+		const handleMouseLeave = () => {
+			hoverDot.style( 'display', 'none' );
+			tooltipData.visible = false;
+		};
 		const plot = () => {
 			chart.attr( 'viewBox', `0 0 ${ props.dimensions.width } ${ props.dimensions.height }` );
-			// Create accessor functions from property names
-			const getXValue = ( d ) => d[ props.xAccessor ];
-			const getYValue = ( d ) => d[ props.yAccessor ];
 
 			const xDomain = d3.extent( props.data, getXValue );
-			const xScale = d3.scaleTime()
+			xScale = d3.scaleTime()
 				.domain( xDomain )
 				.range( [ 0, props.dimensions.width ] );
 			// Get the maximum value from the Y data, or use 10 if all values are 0
@@ -63,7 +163,7 @@ module.exports = exports = {
 			// This issue happens in Safari
 			const yDomain = [ -0.1 * maxY, maxY ];
 
-			const yScale = d3.scaleLinear()
+			yScale = d3.scaleLinear()
 				.domain( yDomain )
 				// Flip svg Y-axis coordinate system and add a pixel on top to avoid cutting
 				// off anti-aliasing pixels. Do not add a pixel on the bottom, that would make the
@@ -95,10 +195,22 @@ module.exports = exports = {
 			// Append order is relevant. Render the line over the area
 			area = chart.append( 'path' ).attr( 'class', 'ext-checkuser-CSparkline__area' );
 			sparkline = chart.append( 'path' ).attr( 'class', 'ext-checkuser-CSparkline__line' );
+
+			// Add hover dot (initially hidden)
+			hoverDot = chart.append( 'circle' )
+				.attr( 'class', 'ext-checkuser-CSparkline__hover-dot' )
+				.attr( 'r', 4 )
+				.style( 'display', 'none' );
+
 			plot();
 		} );
 
-		return {};
+		return {
+			tooltipData,
+			tooltipLabel,
+			handleMouseMove,
+			handleMouseLeave
+		};
 	}
 
 };
@@ -107,8 +219,13 @@ module.exports = exports = {
 <style lang="less">
 @import 'mediawiki.skin.variables.less';
 
+.ext-checkuser-CSparkline-container {
+	position: relative;
+}
+
 .ext-checkuser-CSparkline {
 	padding: @spacing-0 @spacing-50;
+	overflow: visible;
 
 	&__line {
 		stroke: @background-color-progressive--focus;
@@ -117,5 +234,30 @@ module.exports = exports = {
 	&__area {
 		fill: @background-color-progressive-subtle;
 	}
+
+	&__hover-dot {
+		fill: @background-color-progressive--focus;
+		stroke: @background-color-progressive--focus;
+		stroke-width: 1px;
+	}
+
+	// The following CSS was copied from Codex (https://gerrit.wikimedia.org/r/plugins/gitiles/design/codex/+/refs/heads/main/packages/codex/src/components/tooltip/Tooltip.less)
+	&__tooltip {
+		pointer-events: none;
+		white-space: nowrap;
+		background-color: @background-color-inverted;
+		color: @color-inverted;
+		position: absolute;
+		z-index: @z-index-tooltip;
+		width: @size-content-max;
+		max-width: @size-1600;
+		border-radius: @border-radius-base;
+		padding: @spacing-12 @spacing-35;
+		font-family: @font-family-system-sans;
+		font-size: @font-size-small;
+		font-weight: @font-weight-normal;
+		line-height: @line-height-small;
+	}
 }
+
 </style>
