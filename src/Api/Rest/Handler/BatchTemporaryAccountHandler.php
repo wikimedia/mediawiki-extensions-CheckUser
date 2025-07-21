f6924d9000a35456d783c696a99218f00924631d
Dreamy Jazz <wpgbrown@wikimedia.org>
2025-07-21 22:01:02 +0100

    Update BatchTemporaryAccountHandler to support Special:AbuseLog
    Why:
    * We want to support IP reveal on Special:AbuseLog, however, do
      not currently have an API that returns IPs for abuse_filter_log
      afl_id IDs.
    * We can extend the batch-temporaryaccount REST API endpoint to
      optionally accept a list of afl_id values and then return
      the IP addresses associated with them.
    * We added the AbuseFilterLogDetailsLookup service to AbuseFilter
      in I1d4f6cc0f54837778c495105112a861edf72dd41 which allows us
      to use the service instead of queries to AbuseFilter database
      tables.
    
    What:
    * Update BatchTemporaryAccountHandler to optionally accept
      a 'abuseLogIds' list for each user if the AbuseFilter
      extension is installed.
    ** When provided, the API will return the list of IPs associated
       with the provided afl_id values.
    ** afl_id values are ignored if the user that triggered the
       abuse_filter_log is not the user that is indicated in the
       request body (so that we correctly create logs for users
       who had their IP revealed).
    * Update and add PHPUnit tests to ensure consistent test coverage.
    
    Depends-On: I1d4f6cc0f54837778c495105112a861edf72dd41
    Bug: T400042
    Change-Id: I96eea90bc2eaafaf56826b7de0cf6c39b4d61fb0

--- src/Api/Rest/Handler/BatchTemporaryAccountHandler.php
+++ src/Api/Rest/Handler/BatchTemporaryAccountHandler.php
@@ -9,9 +9,11 @@ use MediaWiki\CheckUser\Logging\TemporaryAccountLoggerFactory;
 use MediaWiki\CheckUser\Services\CheckUserPermissionManager;
 use MediaWiki\CheckUser\Services\CheckUserTemporaryAccountAutoRevealLookup;
 use MediaWiki\Config\Config;
+use MediaWiki\Extension\AbuseFilter\AbuseFilterServices;
 use MediaWiki\JobQueue\JobQueueGroup;
 use MediaWiki\ParamValidator\TypeDef\ArrayDef;
 use MediaWiki\Permissions\PermissionManager;
+use MediaWiki\Registration\ExtensionRegistry;
 use MediaWiki\Rest\Response;
 use MediaWiki\Revision\RevisionStore;
 use MediaWiki\User\ActorStore;
@@ -43,7 +45,8 @@ class BatchTemporaryAccountHandler extends AbstractTemporaryAccountHandler {
 		CheckUserPermissionManager $checkUserPermissionsManager,
 		CheckUserTemporaryAccountAutoRevealLookup $autoRevealLookup,
 		TemporaryAccountLoggerFactory $loggerFactory,
-		ReadOnlyMode $readOnlyMode
+		ReadOnlyMode $readOnlyMode,
+		private readonly ExtensionRegistry $extensionRegistry
 	) {
 		parent::__construct(
 			$config,
@@ -107,13 +110,21 @@ class BatchTemporaryAccountHandler extends AbstractTemporaryAccountHandler {
 		$dbr = $this->getConnectionProvider()->getReplicaDatabase();
 		$body = $this->getValidatedBody();
 
+		if ( $this->extensionRegistry->isLoaded( 'Abuse Filter' ) ) {
+			$body = $this->filterAbuseFilterLogIds( $body );
+		}
+
 		foreach ( $body['users'] ?? [] as $username => $params ) {
-			$results[$username] = $this->getData( [
-				$this->getTemporaryAccountActorId( $username ),
-				$params['revIds'] ?? [],
-				$params['logIds'] ?? [],
-				$params['lastUsedIp'] ?? false,
-			], $dbr );
+			$identifier = [
+				'actorId' => $this->getTemporaryAccountActorId( $username ),
+				'revIds' => $params['revIds'] ?? [],
+				'logIds' => $params['logIds'] ?? [],
+				'lastUsedIp' => $params['lastUsedIp'] ?? false,
+			];
+			if ( $this->extensionRegistry->isLoaded( 'Abuse Filter' ) ) {
+				$identifier['abuseLogIds'] = $params['abuseLogIds'] ?? [];
+			}
+			$results[$username] = $this->getData( $identifier, $dbr );
 		}
 
 		if ( $this->autoRevealLookup->isAutoRevealAvailable() ) {
@@ -129,9 +140,9 @@ class BatchTemporaryAccountHandler extends AbstractTemporaryAccountHandler {
 	 * @inheritDoc
 	 */
 	protected function getData( $identifier, IReadableDatabase $dbr ): array {
-		[ $actorId, $revIds, $logIds, $lastUsedIp ] = $identifier;
+		[ 'actorId' => $actorId, 'revIds' => $revIds, 'logIds' => $logIds, 'lastUsedIp' => $lastUsedIp ] = $identifier;
 
-		return [
+		$data = [
 			'revIps' => count( $revIds ) > 0
 				? $this->getRevisionsIps( $actorId, $revIds, $dbr )
 				: null,
@@ -142,24 +153,88 @@ class BatchTemporaryAccountHandler extends AbstractTemporaryAccountHandler {
 				? ( $this->getActorIps( $actorId, 1, $dbr )[0] ?? null )
 				: null,
 		];
+
+		if ( $this->extensionRegistry->isLoaded( 'Abuse Filter' ) ) {
+			$data['abuseLogIps'] = count( $identifier['abuseLogIds'] ) > 0
+				? $this->getAbuseFilterLogIPs( $identifier['abuseLogIds'] )
+				: null;
+		}
+
+		return $data;
+	}
+
+	/**
+	 * Returns the IPs associated with a given set of abuse_filter_log IDs.
+	 *
+	 * This method assumes that the AbuseFilter extension is installed.
+	 */
+	private function getAbuseFilterLogIPs( array $abuseFilterLogIds ): array {
+		if ( count( $abuseFilterLogIds ) === 0 ) {
+			return [];
+		}
+
+		$abuseFilterPrivateLogDetailsLookup = AbuseFilterServices::getLogDetailsLookup();
+		$abuseLogIps = $abuseFilterPrivateLogDetailsLookup->getIPsForAbuseFilterLogs(
+			$this->getAuthority(), $abuseFilterLogIds
+		);
+
+		// Remove any IPs which are false (meaning the user could not see the abuse_filter_log row) and then
+		// return the list.
+		return array_filter( $abuseLogIps, static fn ( $value ) => $value !== false );
+	}
+
+	/**
+	 * Filters out afl_id values from the request body that were not performed by the username that they
+	 * were associated with in the request body.
+	 *
+	 * This is done to ensure that the IP reveal logs correctly indicate all the users that were revealed.
+	 * Otherwise a user could specify an afl_id for a different temporary account and avoid creating a log.
+	 *
+	 * @param array $body The data returned by {@link self::getValidatedBody}
+	 * @return array The data to use as the value of {@link self::getValidatedBody} from now on
+	 */
+	private function filterAbuseFilterLogIds( array $body ): array {
+		$abuseLogIds = [];
+		foreach ( $body['users'] ?? [] as $params ) {
+			$abuseLogIds = array_merge( $params['abuseLogIds'] ?? [], $abuseLogIds );
+		}
+
+		if ( count( $abuseLogIds ) !== 0 ) {
+			$abuseFilterPrivateLogDetailsLookup = AbuseFilterServices::getLogDetailsLookup();
+			$groupedAbuseFilterLogIds = $abuseFilterPrivateLogDetailsLookup->groupAbuseFilterLogIdsByPerformer(
+				$abuseLogIds
+			);
+
+			foreach ( $body['users'] as $username => &$params ) {
+				$params['abuseLogIds'] = $groupedAbuseFilterLogIds[$username] ?? [];
+			}
+		}
+
+		return $body;
 	}
 
 	/**
 	 * @inheritDoc
 	 */
 	public function getBodyParamSettings(): array {
+		$optionalUserProperties = [];
+		if ( $this->extensionRegistry->isLoaded( 'Abuse Filter' ) ) {
+			$optionalUserProperties['abuseLogIds'] = ArrayDef::makeListSchema( 'string' );
+		}
+
 		return [
 			'users' => [
 				self::PARAM_SOURCE => 'body',
 				ParamValidator::PARAM_TYPE => 'array',
 				ParamValidator::PARAM_REQUIRED => true,
-				ArrayDef::PARAM_SCHEMA => ArrayDef::makeMapSchema(
-					ArrayDef::makeObjectSchema( [
+				ArrayDef::PARAM_SCHEMA => ArrayDef::makeMapSchema( ArrayDef::makeObjectSchema(
+					[
 						'revIds' => ArrayDef::makeListSchema( 'string' ),
 						'logIds' => ArrayDef::makeListSchema( 'string' ),
 						'lastUsedIp' => 'boolean',
-					] )
-				),
+					],
+					$optionalUserProperties
+				) ),
 			]
 		] + parent::getBodyParamSettings();
 	}
--- tests/phpunit/integration/Api/Rest/Handler/BatchTemporaryAccountHandlerTest.php
+++ tests/phpunit/integration/Api/Rest/Handler/BatchTemporaryAccountHandlerTest.php
@@ -9,6 +9,14 @@ use MediaWiki\CheckUser\CheckUserPermissionStatus;
 use MediaWiki\CheckUser\HookHandler\Preferences;
 use MediaWiki\CheckUser\Services\CheckUserPermissionManager;
 use MediaWiki\CheckUser\Services\CheckUserTemporaryAccountAutoRevealLookup;
+use MediaWiki\Context\RequestContext;
+use MediaWiki\Extension\AbuseFilter\AbuseFilterServices;
+use MediaWiki\Extension\AbuseFilter\Filter\Flags;
+use MediaWiki\Extension\AbuseFilter\Filter\MutableFilter;
+use MediaWiki\Extension\AbuseFilter\Tests\Integration\FilterFromSpecsTestTrait;
+use MediaWiki\Extension\AbuseFilter\Variables\VariableHolder;
+use MediaWiki\Registration\ExtensionRegistry;
+use MediaWiki\Request\FauxRequest;
 use MediaWiki\Rest\RequestData;
 use MediaWiki\Tests\Rest\Handler\HandlerTestTrait;
 use MediaWiki\Tests\Unit\MockServiceDependenciesTrait;
@@ -25,6 +33,7 @@ use Psr\Log\LoggerInterface;
  */
 class BatchTemporaryAccountHandlerTest extends MediaWikiIntegrationTestCase {
 
+	use FilterFromSpecsTestTrait;
 	use HandlerTestTrait;
 	use MockServiceDependenciesTrait;
 	use TempUserTestTrait;
@@ -36,8 +45,13 @@ class BatchTemporaryAccountHandlerTest extends MediaWikiIntegrationTestCase {
 		int $jobQueueGroupExpects,
 		int $loggerExpects,
 		bool $autoRevealAvailable,
-		bool $autoRevealEnabled
+		bool $autoRevealEnabled,
+		bool $abuseFilterLoaded
 	) {
+		if ( $abuseFilterLoaded ) {
+			$this->markTestSkippedIfExtensionNotLoaded( 'Abuse Filter' );
+		}
+
 		$this->enableAutoCreateTempUser();
 
 		$checkUserPermissionManager = $this->createMock( CheckUserPermissionManager::class );
@@ -69,10 +83,15 @@ class BatchTemporaryAccountHandlerTest extends MediaWikiIntegrationTestCase {
 			);
 		}
 
+		$extensionRegistry = $this->createMock( ExtensionRegistry::class );
+		$extensionRegistry->method( 'isLoaded' )
+			->willReturn( $abuseFilterLoaded );
+
 		$jobQueueGroup = $this->createMock( JobQueueGroup::class );
 		$jobQueueGroup->expects( $this->exactly( $jobQueueGroupExpects ) )
 			->method( 'push' );
 
+		// Set a mock logger for the test and then reset the services as we need services to use this mock logger.
 		$logger = $this->createMock( LoggerInterface::class );
 		$logger->expects( $this->exactly( $loggerExpects ) )
 			->method( 'info' )
@@ -83,6 +102,7 @@ class BatchTemporaryAccountHandlerTest extends MediaWikiIntegrationTestCase {
 				} )
 			);
 		$this->setLogger( 'CheckUser', $logger );
+		$this->resetServices();
 
 		$services = $this->getServiceContainer();
 		$handler = $this->getMockBuilder( BatchTemporaryAccountHandler::class )
@@ -99,7 +119,8 @@ class BatchTemporaryAccountHandlerTest extends MediaWikiIntegrationTestCase {
 				$checkUserPermissionManager,
 				$autoRevealLookup,
 				$services->get( 'CheckUserTemporaryAccountLoggerFactory' ),
-				$services->getReadOnlyMode()
+				$services->getReadOnlyMode(),
+				$extensionRegistry
 			] )
 			->getMock();
 		$handler->method( 'getRevisionsIps' )
@@ -137,6 +158,9 @@ class BatchTemporaryAccountHandlerTest extends MediaWikiIntegrationTestCase {
 				'lastUsedIp' => '9.8.7.6',
 			]
 		];
+		if ( $abuseFilterLoaded ) {
+			$expectedData['~12345']['abuseLogIps'] = null;
+		}
 		if ( $autoRevealAvailable ) {
 			$expectedData['autoReveal'] = $autoRevealEnabled;
 		}
@@ -151,19 +175,138 @@ class BatchTemporaryAccountHandlerTest extends MediaWikiIntegrationTestCase {
 				'loggerExpects' => 1,
 				'autoRevealAvailable' => true,
 				'autoRevealEnabled' => true,
+				'abuseFilterLoaded' => true,
 			],
 			'The correct logger is called when auto-reveal is off' => [
 				'jobQueueGroupExpects' => 1,
 				'loggerExpects' => 0,
 				'autoRevealAvailable' => true,
 				'autoRevealEnabled' => false,
+				'abuseFilterLoaded' => true,
 			],
 			'The correct logger is called when auto-reveal is unavailable' => [
 				'jobQueueGroupExpects' => 1,
 				'loggerExpects' => 0,
 				'autoRevealAvailable' => false,
 				'autoRevealEnabled' => true,
+				'abuseFilterLoaded' => true,
+			],
+			'abuseLogIps key is not added if AbuseFilter is not installed' => [
+				'jobQueueGroupExpects' => 1,
+				'loggerExpects' => 0,
+				'autoRevealAvailable' => false,
+				'autoRevealEnabled' => true,
+				'abuseFilterLoaded' => false,
 			],
 		];
+	}
+
+	public function testExecuteWhenAbuseFilterLogIdsSpecified() {
+		$this->markTestSkippedIfExtensionNotLoaded( 'Abuse Filter' );
+
+		$checkUserPermissionManager = $this->createMock( CheckUserPermissionManager::class );
+		$checkUserPermissionManager->method( 'canAccessTemporaryAccountIPAddresses' )
+			->willReturn( CheckUserPermissionStatus::newGood() );
+
+		$actorStore = $this->createMock( ActorStore::class );
+		$actorStore->method( 'findActorIdByName' )
+			->willReturn( 12345 );
+		$actorStore->method( 'getUserIdentityByName' )
+			->willReturn( new UserIdentityValue( 12345, '~2025-1' ) );
+
+		$autoRevealLookup = $this->createMock( CheckUserTemporaryAccountAutoRevealLookup::class );
+
+		$jobQueueGroup = $this->createMock( JobQueueGroup::class );
+		$jobQueueGroup->expects( $this->once() )
+			->method( 'push' );
+
+		$this->setLogger( 'CheckUser', $this->createNoOpMock( LoggerInterface::class ) );
+
+		$services = $this->getServiceContainer();
+		$handler = $this->getMockBuilder( BatchTemporaryAccountHandler::class )
+			->onlyMethods( [ 'getRevisionsIps', 'getLogIps', 'getActorIps' ] )
+			->setConstructorArgs( [
+				$services->getMainConfig(),
+				$jobQueueGroup,
+				$services->getPermissionManager(),
+				$services->getUserNameUtils(),
+				$services->getConnectionProvider(),
+				$actorStore,
+				$services->getBlockManager(),
+				$services->getRevisionStore(),
+				$checkUserPermissionManager,
+				$autoRevealLookup,
+				$services->get( 'CheckUserTemporaryAccountLoggerFactory' ),
+				$services->getReadOnlyMode(),
+				$services->getExtensionRegistry()
+			] )
+			->getMock();
+		$handler->expects( $this->never() )
+			->method( 'getRevisionsIps' );
+		$handler->expects( $this->never() )
+			->method( 'getLogIps' );
+		$handler->method( 'getActorIps' )
+			->with( 12345, 1 )
+			->willReturn( [ '9.8.7.6' ] );
+
+		$data = $this->executeHandlerAndGetBodyData(
+			$handler, new RequestData(), [], [], [],
+			[
+				'users' => [
+					'~2025-1' => [ 'revIds' => [], 'logIds' => [], 'lastUsedIp' => true, 'abuseLogIds' => [ 1, 2 ] ],
+				],
+			],
+			$this->mockRegisteredUltimateAuthority()
+		);
+
+		$this->assertArrayEquals(
+			[ '~2025-1' => [
+				'revIps' => null,
+				'logIps' => null,
+				'lastUsedIp' => '9.8.7.6',
+				'abuseLogIps' => [ 1 => '1.2.3.4' ],
+			] ],
+			$data,
+			false, true
+		);
+	}
+
+	public function addDBDataOnce(): void {
+		$performer = $this->getTestSysop()->getUser();
+		$this->assertStatusGood( AbuseFilterServices::getFilterStore()->saveFilter(
+			$performer, null,
+			$this->getFilterFromSpecs( [
+				'id' => '1',
+				'name' => 'Test filter',
+				'privacy' => Flags::FILTER_HIDDEN,
+				'rules' => 'old_wikitext = "abc"',
+			] ),
+			MutableFilter::newDefault()
+		) );
+
+		// Insert two hits on the filter performed by different users but on the same IP
+		$this->enableAutoCreateTempUser();
+		RequestContext::getMain()->getRequest()->setIP( '1.2.3.4' );
+		$abuseFilterLoggerFactory = AbuseFilterServices::getAbuseLoggerFactory();
+		$abuseFilterLoggerFactory->newLogger(
+			$this->getExistingTestPage()->getTitle(),
+			$this->getServiceContainer()->getTempUserCreator()->create( '~2025-1', new FauxRequest() )->getUser(),
+			VariableHolder::newFromArray( [
+				'action' => 'edit',
+				'user_name' => '~2025-1',
+				'old_wikitext' => 'abc',
+			] )
+		)->addLogEntries( [ 1 => [] ] );
+
+		$abuseFilterLoggerFactory = AbuseFilterServices::getAbuseLoggerFactory();
+		$abuseFilterLoggerFactory->newLogger(
+			$this->getExistingTestPage()->getTitle(),
+			$this->getServiceContainer()->getTempUserCreator()->create( '~2025-2', new FauxRequest() )->getUser(),
+			VariableHolder::newFromArray( [
+				'action' => 'edit',
+				'user_name' => '~2025-2',
+				'old_wikitext' => 'abc',
+			] )
+		)->addLogEntries( [ 1 => [] ] );
 	}
 }
