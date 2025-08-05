43a7771059efe5cbc73a734b169864ee4915046b
STran <stran@wikimedia.org>
2025-08-05 05:44:05 -0700

    Auto-remove IP reveal rights from inactive users via maintenance script
    Why:
    Inactive users need to have their IP reveal rights revoked automatically.
    
    What:
    - Add RevokeTemporaryAccountViewerGroup script
      + Check if users have been inactive, per the definition, after the
        specified range
      + If so, remove them from the 'temporary-account-viewer' group
    
    Bug: T375115
    Change-Id: I11ff6ddc1b697ddac2b2b1528ed90fd6fc72b2f2

---
+++ maintenance/revokeTemporaryAccountViewerGroup.php
@@ -0,0 +1,230 @@
+<?php
+
+namespace MediaWiki\CheckUser\Maintenance;
+
+use MediaWiki\Context\DerivativeContext;
+use MediaWiki\Context\RequestContext;
+use MediaWiki\Logger\LoggerFactory;
+use MediaWiki\Maintenance\Maintenance;
+use MediaWiki\Permissions\UltimateAuthority;
+use MediaWiki\Specials\SpecialUserRights;
+use MediaWiki\User\User;
+use MediaWiki\User\UserGroupManager;
+use MediaWiki\User\UserIdentity;
+use MediaWiki\User\UserIdentityLookup;
+use Psr\Log\LoggerInterface;
+use Wikimedia\Timestamp\ConvertibleTimestamp;
+
+// @codeCoverageIgnoreStart
+if ( getenv( 'MW_INSTALL_PATH' ) ) {
+	$IP = getenv( 'MW_INSTALL_PATH' );
+} else {
+	$IP = dirname( __DIR__, 3 );
+}
+require_once "$IP/maintenance/Maintenance.php";
+// @codeCoverageIgnoreEnd
+
+/**
+ * Revokes 'temporary-account-viewer' group membership from users who
+ * have been inactive for a specified amount of time.
+ *
+ * Activity is defined as:
+ *   - an undeleted revision
+ *   - a logged action
+ */
+class RevokeTemporaryAccountViewerGroup extends Maintenance {
+
+	protected LoggerInterface $logger;
+	protected UserGroupManager $userGroupManager;
+	protected UserIdentityLookup $userIdentityLookup;
+
+	protected UltimateAuthority $maintenanceScriptAuthority;
+
+	public function __construct() {
+		parent::__construct();
+
+		$this->addDescription(
+			'Revoke the temporary-account-viewer group membership ' .
+			'of users who have been inactive for over N days'
+		);
+		$this->addOption(
+			'expiry',
+			'Revoke the rights of accounts that haven\'t been active in N days. Use 0 to revoke everyone\'s right.',
+			true,
+			true
+		);
+		$this->addOption( 'verbose', 'Verbose logging output' );
+		$this->setBatchSize( 100 );
+	}
+
+	/**
+	 * Construct services the script needs to use
+	 */
+	private function initServices(): void {
+		$services = $this->getServiceContainer();
+
+		$this->logger = LoggerFactory::getInstance( 'CheckUser' );
+		$this->userGroupManager = $services->getUserGroupManager();
+		$this->userIdentityLookup = $services->getUserIdentityLookup();
+	}
+
+	/**
+	 * If --verbose is passed, log to output
+	 *
+	 * @param string $log
+	 * @return void
+	 */
+	private function verboseLog( string $log ) {
+		if ( $this->hasOption( 'verbose' ) ) {
+			$this->output( $log );
+		}
+	}
+
+	/**
+	 * @return int|bool true if success, user id to be marked if failure
+	 */
+	private function revokeTempAccountViewerGroupMembership( UserIdentity $userIdentity ) {
+		// Instantiate Special:UserRights which is the access point to logged user group management
+		$specialUserRights = new SpecialUserRights();
+
+		// Remove user from group
+		$authority = $this->maintenanceScriptAuthority;
+		$context = new DerivativeContext( RequestContext::getMain() );
+		$context->setAuthority( $authority );
+		$specialUserRights->setContext( $context );
+		$revokedMembership = $specialUserRights->doSaveUserGroups(
+			$userIdentity,
+			[],
+			[ 'temporary-account-viewer' ],
+			$context->msg( 'checkuser-temporary-account-autorevoke-userright-reason' )
+		);
+
+		// doSaveUserGroups() will return the removed groups in the second array. If it's empty, there was a problem.
+		if (
+			!count( $revokedMembership[1] ) ||
+			!in_array( 'temporary-account-viewer', $revokedMembership[1] )
+		) {
+			$this->error( 'Problem revoking membership for ' . $userIdentity->getName() );
+			$this->logger->error( 'Problem revoking temporary-account-viewer membership for {user}', [
+				'user' => $userIdentity->getName(),
+			] );
+			return $userIdentity->getId();
+		}
+
+		return true;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function execute() {
+		$this->initServices();
+
+		// Prepare for revocations:
+		// Get the expiry timestamp to search against
+		$dbr = $this->getReplicaDB();
+		$expiryAfterDays = $this->getOption( 'expiry' );
+		if ( !is_numeric( $expiryAfterDays ) ) {
+			$this->fatalError( 'Expiry value must be a number' );
+		}
+		$expiryAfterDays = (int)$expiryAfterDays;
+		$expiryTimestamp = $dbr->timestamp( ConvertibleTimestamp::time() - ( 86_400 * $expiryAfterDays ) );
+
+		// Get the system user to use as the performer for SpecialUserRights. It needs to be
+		// an UltimateAuthority so that it has the right to remove group membership.
+		$maintUser = User::newSystemUser( User::MAINTENANCE_SCRIPT_USER, [ 'steal' => true ] );
+		if ( !$maintUser ) {
+			$this->fatalError( 'Maintenance script user not acquired but required to continue' );
+		}
+		$this->maintenanceScriptAuthority = new UltimateAuthority( $maintUser );
+
+		$maxUserId = 0;
+		$revokeCount = 0;
+		$skippedUsers = [];
+		do {
+			$userQuerySelector = $dbr->newSelectQueryBuilder()
+				->select( 'ug_user' )
+				->distinct()
+				->from( 'user_groups' )
+				->where( $dbr->expr( 'ug_user', '>', $maxUserId ) )
+				->join( 'actor', null, 'ug_user=actor_user' )
+				->orderBy( 'ug_user' )
+				->limit( $this->getBatchSize() );
+
+			// Ignore users that have been attempted but for whatever reason were marked as failures
+			if ( count( $skippedUsers ) ) {
+				$userQuerySelector->andWhere( $dbr->expr( 'ug_user', '!=', $skippedUsers ) );
+			}
+
+			// Get users with 'temporary-account-viewer' membership that should be revoked
+			if ( $expiryAfterDays === 0 ) {
+				// If the expiry is set to 0, all users with an active membership in the group
+				// should have it revoked regardless of activity
+				$userQuerySelector->where( [
+					'ug_group' => 'temporary-account-viewer',
+					$dbr->expr( 'ug_expiry', '>', wfTimestamp( TS_MW ) )
+						->or( 'ug_expiry', '=', null )
+				] );
+			} else {
+				// Otherwise, left join on revision and logging with the expiry range
+				// This works by setting null on all rows in the revision/logging tables
+				// that don't match the range which can then be filtered for. Any row
+				// with a valid timestamp would indicate activity from the user.
+				$userQuerySelector
+					->leftJoin( 'revision', null, [
+						'actor_id=rev_actor',
+						$dbr->expr( 'rev_timestamp', '>', $expiryTimestamp ),
+					] )
+					->leftJoin( 'logging', null, [
+						'actor_id=log_actor',
+						$dbr->expr( 'log_timestamp', '>', $expiryTimestamp ),
+					] )
+					->where( [
+						'ug_group' => 'temporary-account-viewer',
+						$dbr->expr( 'ug_expiry', '>', wfTimestamp( TS_MW ) )
+							->or( 'ug_expiry', '=', null ),
+						'rev_timestamp' => null,
+						'log_timestamp' => null,
+					] );
+			}
+			$usersToRevokeGroupMembershipFrom = $userQuerySelector
+				->caller( __METHOD__ )
+				->fetchResultSet();
+
+			foreach ( $usersToRevokeGroupMembershipFrom as $userRow ) {
+				$userIdentity = $this->userIdentityLookup->getUserIdentityByUserId( (int)$userRow->ug_user );
+				if ( !$userIdentity ) {
+					$this->error( 'Actor row found but no user identity obtainable for ' . $userRow->ug_user );
+					$this->logger->error( 'Actor row found but no user identity obtainable for {userId}', [
+						'userId' => $userRow->ug_user,
+					] );
+					$skippedUsers[] = $userRow->ug_user;
+					continue;
+				}
+				$revokeStatus = $this->revokeTempAccountViewerGroupMembership( $userIdentity );
+				if ( $revokeStatus === true ) {
+					$revokeCount++;
+				} else {
+					$skippedUsers[] = $revokeStatus;
+				}
+				$maxUserId = (int)$userRow->ug_user;
+
+				$this->verboseLog(
+					'Removed ' . $userIdentity->getName() . ' from temporary-account-viewer group' . PHP_EOL
+				);
+			}
+			$this->waitForReplication();
+		} while ( $usersToRevokeGroupMembershipFrom->numRows() );
+
+		$this->output( "Removed $revokeCount user(s) from temporary-account-viewer group." . PHP_EOL );
+		if ( count( $skippedUsers ) ) {
+			$this->output( "Attempted and failed to remove users " . implode( ',', $skippedUsers ) .
+			'from temporary-account-viewer group.' . PHP_EOL );
+		}
+	}
+}
+
+// @codeCoverageIgnoreStart
+$maintClass = RevokeTemporaryAccountViewerGroup::class;
+require_once RUN_MAINTENANCE_IF_MAIN;
+// @codeCoverageIgnoreEnd
