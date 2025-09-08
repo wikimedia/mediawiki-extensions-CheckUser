5a458e6db75bb6e824817f2f100cdf1eb6a0b573
Héctor Arroyo <hector.arroyo@wikimedia.org>
2025-09-08 11:09:34 +0200

    SuggestedInvestigations: Add constants file with status names
    Why:
    - The new dialog to change the status of a given investigation will
      require listing the different statuses a case may be in
    - The list of statuses and their names are currently not defined in
      the frontend code, but having them defined as constants would make
      it easier to extend the dialog later, since it would be able to
      receive the status names as a parameter.
    
    What:
    - Add a new file holding constants for the Suggested Investigations
      dialog, and make each point to the i18n key holting its user-facing
      name.
    
    Bug: T402981
    Change-Id: I079a9ba8cb822b443aa5fea9f250fa370b553982

---
+++ modules/ext.checkUser.suggestedInvestigations/Constants.js
@@ -0,0 +1,12 @@
+const Constants = {
+	caseStatuses: {
+		NOT_STARTED: 'checkuser-suggestedinvestigations-status-open',
+		RESOLVED: 'checkuser-suggestedinvestigations-status-resolved',
+		INVALID: 'checkuser-suggestedinvestigations-status-invalid'
+	},
+	statusDescriptions: {
+		INVALID: 'checkuser-suggestedinvestigations-status-description-invalid'
+	}
+};
+
+module.exports = Constants;
