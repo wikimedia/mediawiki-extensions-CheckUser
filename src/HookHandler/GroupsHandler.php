10131ba7992d9951f11e5757e22505546c0602d8
Sam Reed <sam@reedyboy.net>
2025-11-26 13:36:45 +0000

    Remove CheckUserGroupRequirements
    Effectively reverts Iadcb98b599f05ed4676d68f244bdd42d2c8c3296,
    though leaves GroupsHandler (and tests) behind as
    Idbf3069585318105c64cbb1ce705baa83ad9e9e7 also introduced usages.
    
    Bug: T409718
    Change-Id: Ib7d1c8a543748b1c601ee66f33052bf188225f35
    (cherry picked from commit 2cb62e2f8957551fe76110d0191c1af1abaf1237)

--- src/HookHandler/GroupsHandler.php
+++ src/HookHandler/GroupsHandler.php
@@ -1,128 +1,20 @@
 <?php
 namespace MediaWiki\CheckUser\HookHandler;
 
-use MediaWiki\Config\Config;
-use MediaWiki\Extension\CentralAuth\User\CentralAuthUser;
-use MediaWiki\Hook\SpecialUserRightsChangeableGroupsHook;
-use MediaWiki\Permissions\Authority;
-use MediaWiki\Registration\ExtensionRegistry;
 use MediaWiki\SpecialPage\SpecialPageFactory;
 use MediaWiki\User\CentralId\CentralIdLookup;
 use MediaWiki\User\Hook\UserGroupsChangedHook;
-use MediaWiki\User\Registration\UserRegistrationLookup;
-use MediaWiki\User\UserEditTracker;
-use MediaWiki\User\UserGroupManager;
-use MediaWiki\User\UserIdentity;
 use Wikimedia\ObjectCache\WANObjectCache;
-use Wikimedia\Rdbms\IConnectionProvider;
-use Wikimedia\Timestamp\ConvertibleTimestamp;
 
 class GroupsHandler implements
-	SpecialUserRightsChangeableGroupsHook,
 	UserGroupsChangedHook
-	{
-	private Config $config;
-	private IConnectionProvider $dbProvider;
-	private ExtensionRegistry $extensionRegistry;
-	private UserGroupManager $userGroupManager;
-	private UserEditTracker $userEditTracker;
-	private UserRegistrationLookup $userRegistrationLookup;
-	private CentralIdLookup $centralIdLookup;
-	private WANObjectCache $wanCache;
-	private SpecialPageFactory $specialPageFactory;
+{
 
 	public function __construct(
-		Config $config,
-		IConnectionProvider $dbProvider,
-		ExtensionRegistry $extensionRegistry,
-		UserGroupManager $userGroupManager,
-		UserEditTracker $userEditTracker,
-		UserRegistrationLookup $userRegistrationLookup,
-		CentralIdLookup $centralIdLookup,
-		WANObjectCache $wanCache,
-		SpecialPageFactory $specialPageFactory
+		private readonly CentralIdLookup $centralIdLookup,
+		private readonly WANObjectCache $wanCache,
+		private readonly SpecialPageFactory $specialPageFactory
 	) {
-		$this->config = $config;
-		$this->dbProvider = $dbProvider;
-		$this->extensionRegistry = $extensionRegistry;
-		$this->userGroupManager = $userGroupManager;
-		$this->userEditTracker = $userEditTracker;
-		$this->userRegistrationLookup = $userRegistrationLookup;
-		$this->centralIdLookup = $centralIdLookup;
-		$this->wanCache = $wanCache;
-		$this->specialPageFactory = $specialPageFactory;
-	}
-
-	/**
-	 * @inheritDoc
-	 */
-	public function onSpecialUserRightsChangeableGroups(
-		Authority $authority,
-		UserIdentity $target,
-		array $addableGroups,
-		array &$restrictedGroups
-	): void {
-		$spec = $this->config->get( 'CheckUserGroupRequirements' );
-		$groupsToCheck = array_intersect( array_keys( $spec ), $addableGroups );
-
-		if ( count( $groupsToCheck ) === 0 ) {
-			return;
-		}
-
-		$performerGroups = $this->userGroupManager->getUserGroups( $authority->getUser() );
-		if ( $this->extensionRegistry->isLoaded( 'CentralAuth' ) ) {
-			$centralUser = CentralAuthUser::getInstance( $authority->getUser() );
-			if ( $centralUser->exists() && $centralUser->isAttached() ) {
-				$performerGroups = array_merge( $performerGroups, $centralUser->getGlobalGroups() );
-			}
-		}
-
-		// Get the target user's edit count and registration date. The target user may be local
-		// or external. Note that the requirements are configured on the local wiki.
-		if ( $target->getWikiId() === UserIdentity::LOCAL ) {
-			$editCount = (int)$this->userEditTracker->getUserEditCount( $target );
-			$registration = $this->userRegistrationLookup->getRegistration( $target );
-		} else {
-			$dbr = $this->dbProvider->getReplicaDatabase( $target->getWikiId() );
-			$row = $dbr->newSelectQueryBuilder()
-				->select( [ 'user_editcount', 'user_registration' ] )
-				->from( 'user' )
-				->where( [ 'user_name' => $target->getName() ] )
-				->caller( __METHOD__ )
-				->fetchRow();
-			$editCount = $row->user_editcount;
-			$registration = $row->user_registration;
-		}
-
-		foreach ( $groupsToCheck as $group ) {
-			$groupSpec = $spec[$group];
-
-			// Treat the edit count as 0 if null is returned
-			$editCountPasses = !isset( $groupSpec['edits'] ) || $editCount >= $groupSpec['edits'];
-
-			// Treat the account age as meeting requirements if null is returned for the
-			// registration, because that would imply a very old account.
-			if ( $registration !== null ) {
-				$now = (int)ConvertibleTimestamp::now( TS_UNIX );
-				$accountAge = $now - (int)ConvertibleTimestamp::convert(
-					TS_UNIX,
-					$registration
-				);
-			}
-			$accountAgePasses = !isset( $groupSpec['age'] ) ||
-				!isset( $accountAge ) ||
-				$accountAge >= $groupSpec['age'];
-
-			$conditionMet = $editCountPasses && $accountAgePasses;
-			$ignoreCondition = isset( $groupSpec['exemptGroups'] ) &&
-				count( array_intersect( $performerGroups, $groupSpec['exemptGroups'] ) ) > 0;
-
-			$restrictedGroups[$group] = [
-				'condition-met' => $conditionMet,
-				'ignore-condition' => $ignoreCondition,
-				'message' => $groupSpec['reason'] ?? 'checkuser-group-requirements',
-			];
-		}
 	}
 
 	/**
