c13a3625bfff0926f36dce4a1d8b12513a84d6c0
Fomafix <fomafix@googlemail.com>
2026-02-13 15:19:48 +0000

    Inject services into LogTemporaryAccountAccessJob
    Change-Id: Ic1326eb3ad486000f5e04a52704a0953a9816b64

--- tests/phpunit/integration/Jobs/LogTemporaryAccountAccessJobTest.php
+++ tests/phpunit/integration/Jobs/LogTemporaryAccountAccessJobTest.php
@@ -12,19 +12,31 @@ use MediaWikiIntegrationTestCase;
  */
 class LogTemporaryAccountAccessJobTest extends MediaWikiIntegrationTestCase {
 	public function testRunOnInvalidPerformer() {
-		$job = new LogTemporaryAccountAccessJob( 'unused', [
-			'performer' => 'Template:InvalidUser#test',
-			'target' => 'test', 'timestamp' => 0, 'type' => 'view-ips',
-		] );
+		$services = $this->getServiceContainer();
+		$job = new LogTemporaryAccountAccessJob(
+			'unused',
+			[
+				'performer' => 'Template:InvalidUser#test',
+				'target' => 'test', 'timestamp' => 0, 'type' => 'view-ips',
+			],
+			$services->getService( 'CheckUserTemporaryAccountLoggerFactory' ),
+			$services->getUserIdentityLookup()
+		);
 		$this->assertFalse( $job->run() );
 		$this->assertSame( 'Invalid performer', $job->getLastError() );
 	}
 
 	public function testRunOnInvalidType() {
-		$job = new LogTemporaryAccountAccessJob( 'unused', [
-			'performer' => $this->getTestUser()->getUserIdentity()->getName(),
-			'type' => 'invalidtype', 'target' => 'test', 'timestamp' => 0,
-		] );
+		$services = $this->getServiceContainer();
+		$job = new LogTemporaryAccountAccessJob(
+			'unused',
+			[
+				'performer' => $this->getTestUser()->getUserIdentity()->getName(),
+				'type' => 'invalidtype', 'target' => 'test', 'timestamp' => 0,
+			],
+			$services->getService( 'CheckUserTemporaryAccountLoggerFactory' ),
+			$services->getUserIdentityLookup()
+		);
 		$this->assertFalse( $job->run() );
 		$this->assertSame( "Invalid type 'invalidtype'", $job->getLastError() );
 	}
